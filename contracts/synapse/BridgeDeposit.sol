573e9986777fa03730ba0f9df37c88acb0c95ee5
aureliusbtc <cryptoaurelius@protonmail.com>
2021-05-26 22:42:34 -0400

    test + bridge deployment on ropsten

--- contracts/synapse/BridgeDeposit.sol
+++ contracts/synapse/BridgeDeposit.sol
@@ -1,30 +1,31 @@
 // contracts/ERC20Deposit.sol
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.0 <0.8.0;
+
 
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 
-contract ERC20Deposit is Initializable, OwnableUpgradeable {
+contract BridgeDeposit is Initializable, OwnableUpgradeable {
     using SafeERC20 for IERC20;
 
     function initialize(
     ) public initializer {
-        Ownable.initialize(msg.sender);
+        __Ownable_init_unchained();
     }
     
     event TokenDeposit(address from, address to, IERC20 token, uint256 amount);
     event TokenWithdraw(address to, IERC20 token, uint256 amount);
 
-    function deposit(address to, IERC20 token, uint256 amount) {
+    function deposit(address to, IERC20 token, uint256 amount) public {
        token.safeTransferFrom(msg.sender, address(this), amount);
        emit TokenDeposit(msg.sender, to, token, amount);
     }
 
-    function withdraw(address to, IERC20 token, uint256 amount) onlyOwner() {
+    function withdraw(address to, IERC20 token, uint256 amount) onlyOwner() public {
         token.safeTransferFrom(address(this), to, amount);
         emit TokenWithdraw(to, token, amount);
     }
