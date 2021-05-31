c8209da7f002a2cbbd3ff961e96178121b7e07c5
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-05-30 21:02:59 -0400

    V1 Synapse ERC20 (Syn assets) (Equivalent to anyAssets)

--- contracts/synapse/interfaces/IBridgeDeposit.sol
+++ contracts/synapse/interfaces/IBridgeDeposit.sol
@@ -8,5 +8,5 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 interface IBridgeDeposit {
     using SafeERC20 for IERC20;
     
-    function deposit(address to, IERC20 token, uint256 amount) external;
+    function deposit(address to, uint256 chainId, IERC20 token, uint256 amount) external;
 }
---
+++ contracts/synapse/interfaces/ISynapseERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity >=0.6.0 <0.8.0;
+
+interface ISynapseERC20 {    
+    function initialize(
+        string memory _name, string memory _symbol, uint8 _decimals, address owner) external; 
+}
+
