0724ffe37e4ec55f36a68ab93f0ff852036d12bb
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-05-30 23:16:19 -0400

    underlyign token info

--- contracts/synapse/SynapseERC20.sol
+++ contracts/synapse/SynapseERC20.sol
@@ -11,15 +11,18 @@ import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 
 contract SynapseERC20 is Initializable, ContextUpgradeable, AccessControlUpgradeable, ERC20BurnableUpgradeable, ERC20PermitUpgradeable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
+    uint256 public underlyingChainId;
+    address public underlyingTokenAddress;
     /**
      * @notice Initializes this ERC20 contract with the given parameters.
      * @param _name Token name
      * @param _symbol Token symbol
      * @param _decimals Token name
+     * @param _underlyingChainId Base asset chain ID which SynapseERC20 represents 
+     * @param _underlyingTokenAddress Base asset address which SynapseERC20 represents 
      * @param _owner admin address to be initialized with
      */
-    function initialize(string memory _name, string memory _symbol, uint8 _decimals, address _owner) public initializer {
+    function initialize(string memory _name, string memory _symbol, uint8 _decimals, uint256 _underlyingChainId, address _underlyingTokenAddress, address _owner) public initializer {
         __Context_init_unchained();
         __AccessControl_init_unchained();
         __ERC20_init_unchained(_name, _symbol);
@@ -27,6 +30,8 @@ contract SynapseERC20 is Initializable, ContextUpgradeable, AccessControlUpgrade
         _setupDecimals(_decimals);
         __ERC20Permit_init(_name);
         _setupRole(DEFAULT_ADMIN_ROLE, _owner);
+        underlyingChainId = _underlyingChainId;
+        underlyingTokenAddress = _underlyingTokenAddress;
     }
     
     function mint(address to, uint256 amount) public {
--- contracts/synapse/interfaces/ISynapseERC20.sol
+++ contracts/synapse/interfaces/ISynapseERC20.sol
@@ -5,6 +5,6 @@ pragma solidity >=0.6.0 <0.8.0;
 
 interface ISynapseERC20 {    
     function initialize(
-        string memory _name, string memory _symbol, uint8 _decimals, address owner) external; 
+        string memory _name, string memory _symbol, uint8 _decimals,  uint256 _underlyingChainId, address _underlyingTokenAddress,  address owner) external; 
 }
 
