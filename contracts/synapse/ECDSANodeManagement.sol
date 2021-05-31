151e8490eb9f779f26c857236f3173851805c440
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-05-30 22:21:08 -0400

    utils folder

--- contracts/synapse/ECDSANodeManagement.sol
+++ contracts/synapse/ECDSANodeManagement.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
-import "./AddressArrayUtils.sol";
+import "./utils/AddressArrayUtils.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
