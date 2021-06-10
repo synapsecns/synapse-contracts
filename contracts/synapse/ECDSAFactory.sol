6a450ce54bf8e0805a0b18dadbff6da40e49f0a6
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-06-10 03:10:55 -0400

    OnlyOwner modifier on deploys

--- contracts/synapse/ECDSAFactory.sol
+++ contracts/synapse/ECDSAFactory.sol
@@ -2,36 +2,36 @@
 
 pragma solidity 0.6.12;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/proxy/Clones.sol";
-import "./interfaces/IECDSANodeManagement.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/proxy/Clones.sol';
+import './interfaces/IECDSANodeManagement.sol';
 
 contract ECDSAFactory is Ownable {
-    event ECDSANodeGroupCreated(
-        address indexed keepAddress,
-        address[] members,
-        address indexed owner, 
-        uint256 honestThreshold
-    );
+  event ECDSANodeGroupCreated(
+    address indexed keepAddress,
+    address[] members,
+    address indexed owner,
+    uint256 honestThreshold
+  );
 
-    struct LatestNodeGroup {
-        address keepAddress;
-        address[] members;
-        address owner;
-        uint256 honestThreshold;
-    }
+  struct LatestNodeGroup {
+    address keepAddress;
+    address[] members;
+    address owner;
+    uint256 honestThreshold;
+  }
 
-    LatestNodeGroup public latestNodeGroup;
+  LatestNodeGroup public latestNodeGroup;
 
-    constructor() public Ownable() {}
+  constructor() public Ownable() {}
 
-    /// @notice Returns members of the keep.
-    /// @return List of the keep members' addresses.
-    function getMembers() public view returns (address[] memory) {
-        return latestNodeGroup.members;
-    }
+  /// @notice Returns members of the keep.
+  /// @return List of the keep members' addresses.
+  function getMembers() public view returns (address[] memory) {
+    return latestNodeGroup.members;
+  }
 
-    /**
+  /**
     @notice Deploys a new node 
     @param nodeMgmtAddress address of the ECDSANodeManagement contract to initialize with
     @param owner Owner of the  ECDSANodeManagement contract who can determine if the node group is closed or active
@@ -39,25 +39,21 @@ contract ECDSAFactory is Ownable {
     @param honestThreshold Number of signers to process a transaction 
     @return Address of the newest node management contract created
     **/
-    function deploy(
-        address nodeMgmtAddress,
-        address owner,
-        address[] memory members,
-        uint256 honestThreshold
-    ) external returns (address) {
-        address nodeClone = Clones.clone(nodeMgmtAddress);
-        IECDSANodeManagement(nodeClone).initialize(
-            owner,
-            members,
-            honestThreshold
-        );
-        
-        latestNodeGroup.keepAddress = nodeClone;
-        latestNodeGroup.members = members;
-        latestNodeGroup.owner = owner;
-        latestNodeGroup.honestThreshold = honestThreshold;
+  function deploy(
+    address nodeMgmtAddress,
+    address owner,
+    address[] memory members,
+    uint256 honestThreshold
+  ) external onlyOwner returns (address) {
+    address nodeClone = Clones.clone(nodeMgmtAddress);
+    IECDSANodeManagement(nodeClone).initialize(owner, members, honestThreshold);
 
-        emit ECDSANodeGroupCreated(nodeClone, members, owner, honestThreshold);
-        return nodeClone;
-    }
+    latestNodeGroup.keepAddress = nodeClone;
+    latestNodeGroup.members = members;
+    latestNodeGroup.owner = owner;
+    latestNodeGroup.honestThreshold = honestThreshold;
+
+    emit ECDSANodeGroupCreated(nodeClone, members, owner, honestThreshold);
+    return nodeClone;
+  }
 }
