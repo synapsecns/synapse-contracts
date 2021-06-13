e704c2a2decf02694424430faa6f2c04de9c872f
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-06-13 01:40:38 -0400

    SynERc20 test

--- contracts/synapse/SynapseERC20.sol
+++ contracts/synapse/SynapseERC20.sol
@@ -1,64 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
-import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20BurnableUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/drafts/ERC20PermitUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/proxy/Initializable.sol';
 
-contract SynapseERC20 is
-  Initializable,
-  ContextUpgradeable,
-  AccessControlUpgradeable,
-  ERC20BurnableUpgradeable,
-  ERC20PermitUpgradeable
-{
-  bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
-  uint256 public underlyingChainId;
-  address public underlyingTokenAddress;
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/drafts/ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 
-  /**
-   * @notice Initializes this ERC20 contract with the given parameters.
-   * @param _name Token name
-   * @param _symbol Token symbol
-   * @param _decimals Token name
-   * @param _underlyingChainId Base asset chain ID which SynapseERC20 represents
-   * @param _underlyingTokenAddress Base asset address which SynapseERC20 represents
-   * @param _owner admin address to be initialized with
-   */
-  function initialize(
-    string memory _name,
-    string memory _symbol,
-    uint8 _decimals,
-    uint256 _underlyingChainId,
-    address _underlyingTokenAddress,
-    address _owner
-  ) public initializer {
-    __Context_init_unchained();
-    __AccessControl_init_unchained();
-    __ERC20_init_unchained(_name, _symbol);
-    __ERC20Burnable_init_unchained();
-    _setupDecimals(_decimals);
-    __ERC20Permit_init(_name);
-    _setupRole(DEFAULT_ADMIN_ROLE, _owner);
-    underlyingChainId = _underlyingChainId;
-    underlyingTokenAddress = _underlyingTokenAddress;
-  }
 
-  function mint(address to, uint256 amount) public {
-    require(hasRole(MINTER_ROLE, msg.sender));
-    _mint(to, amount);
-  }
+contract SynapseERC20 is Initializable, ContextUpgradeable, AccessControlUpgradeable, ERC20BurnableUpgradeable, ERC20PermitUpgradeable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    uint256 public underlyingChainId;
+    address public underlyingTokenAddress;
+    /**
+     * @notice Initializes this ERC20 contract with the given parameters.
+     * @param _name Token name
+     * @param _symbol Token symbol
+     * @param _decimals Token name
+     * @param _underlyingChainId Base asset chain ID which SynapseERC20 represents 
+     * @param _underlyingTokenAddress Base asset address which SynapseERC20 represents 
+     * @param _owner admin address to be initialized with
+     */
+    function initialize(string memory _name, string memory _symbol, uint8 _decimals, uint256 _underlyingChainId, address _underlyingTokenAddress, address _owner) public initializer {
+        __Context_init_unchained();
+        __AccessControl_init_unchained();
+        __ERC20_init_unchained(_name, _symbol);
+        __ERC20Burnable_init_unchained();
+        _setupDecimals(_decimals);
+        __ERC20Permit_init(_name);
+        _setupRole(DEFAULT_ADMIN_ROLE, _owner);
+        underlyingChainId = _underlyingChainId;
+        underlyingTokenAddress = _underlyingTokenAddress;
+    }
+    
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Not a minter");
+        _mint(to, amount);
+    }
 
-  function mintMultiple(
-    address to,
-    uint256 amount,
-    address feeAddress,
-    uint256 feeAmount
-  ) public {
-    require(hasRole(MINTER_ROLE, msg.sender));
-    _mint(to, amount);
-    _mint(feeAddress, feeAmount);
-  }
+    function mintMultiple(address to, uint256 amount, address feeAddress, uint256 feeAmount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Not a minter");
+        _mint(to, amount);
+        _mint (feeAddress, feeAmount);
+    }
 }
--- contracts/synapse/SynapseERC20Factory.sol
+++ contracts/synapse/SynapseERC20Factory.sol
@@ -6,26 +6,30 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./interfaces/ISynapseERC20.sol";
 
-contract SynapseERC20Factory  {
+contract SynapseERC20Factory {
     constructor() public {}
 
-    event SynapseERC20Created(
-        address contractAddress
-    );
+    event SynapseERC20Created(address contractAddress);
 
     /**
-    * @notice Deploys a new node 
-    * @param synapseERC20Address address of the synapseERC20Address contract to initialize with
-    * @param name Token name
-    * @param symbol Token symbol
-    * @param decimals Token name
-    * @param underlyingChainId Base asset chain ID which SynapseERC20 represents 
-    * @param underlyingTokenAddress Base asset address which SynapseERC20 represents 
-    * @param owner admin address to be initialized with
-    * @return Address of the newest node management contract created
-    **/
+     * @notice Deploys a new node
+     * @param synapseERC20Address address of the synapseERC20Address contract to initialize with
+     * @param name Token name
+     * @param symbol Token symbol
+     * @param decimals Token name
+     * @param underlyingChainId Base asset chain ID which SynapseERC20 represents
+     * @param underlyingTokenAddress Base asset address which SynapseERC20 represents
+     * @param owner admin address to be initialized with
+     * @return Address of the newest node management contract created
+     **/
     function deploy(
-    address synapseERC20Address, string memory name, string memory symbol, uint256 underlyingChainId, address underlyingTokenAddress, uint8 decimals, address owner
+        address synapseERC20Address,
+        string memory name,
+        string memory symbol,   
+        uint8 decimals,
+        uint256 underlyingChainId,
+        address underlyingTokenAddress,
+        address owner
     ) external returns (address) {
         address synERC20Clone = Clones.clone(synapseERC20Address);
         ISynapseERC20(synERC20Clone).initialize(
