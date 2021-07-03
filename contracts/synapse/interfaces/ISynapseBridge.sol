ebcb2a0bc20e30adf95561070aab6931529af949
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-07-03 03:34:31 -0500

    Integrate all Nerve pool swaps for the bridge

---
+++ contracts/synapse/interfaces/ISynapseBridge.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol';
+
+interface ISynapseBridge {
+  using SafeERC20 for IERC20;
+
+  function deposit(
+    address to,
+    uint256 chainId,
+    IERC20 token,
+    uint256 amount
+  ) external;
+
+  function depositAndSwap(
+    address to,
+    uint256 chainId,
+    IERC20 token,
+    uint256 amount,
+    uint8 tokenIndexFrom,
+    uint8 tokenIndexTo,
+    uint256 minDy,
+    uint256 deadline
+  ) external;
+
+  function redeem(
+    address to,
+    uint256 chainId,
+    IERC20 token,
+    uint256 amount
+  ) external;
+
+  function redeemAndSwap(
+    address to,
+    uint256 chainId,
+    IERC20 token,
+    uint256 amount,
+    uint256 swapTokenAmount,
+    uint8 swapTokenIndex,
+    uint256 swapMinAmount,
+    uint256 swapDeadline
+  ) external;
+}
