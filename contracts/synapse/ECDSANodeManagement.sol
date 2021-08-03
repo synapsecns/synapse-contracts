646e828b63831fb177564d2b3e3a7bb8482760cf
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-08-03 18:38:44 +0300

    standardize sol version

--- contracts/synapse/ECDSANodeManagement.sol
+++ contracts/synapse/ECDSANodeManagement.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity 0.6.12;
 
 import "./utils/AddressArrayUtils.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
