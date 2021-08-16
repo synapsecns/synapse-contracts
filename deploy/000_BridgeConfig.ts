e96344ece86ec945ad33e618acf247f40c462b9b
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-08-15 20:01:45 -0400

    New Bridge Config

--- deploy/000_BridgeConfig.ts
+++ deploy/000_BridgeConfig.ts
@@ -3,7 +3,7 @@ import { DeployFunction } from 'hardhat-deploy/types'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
-  const { deploy, get } = deployments
+  const { execute, deploy, get } = deployments
   const { deployer } = await getNamedAccounts()
 
   await deploy('BridgeConfig', {
@@ -11,6 +11,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     log: true,
     skipIfAlreadyDeployed: true,
   })
+
+  await execute(
+    "BridgeConfig",
+    { from: deployer, log: true },
+    "grantRole",
+    "0x4370dcf3e42e4d5b773a451bb8390ee8e7308f47681d1414cff87c2ad0512c85",
+    (await get("DevMultisig")).address,
+  )
 }
+
 export default func
 func.tags = ['BridgeConfig']
