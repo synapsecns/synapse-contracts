7e062130d45ef63fc594cdecdaf496e07162cef5
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-08-15 23:27:34 -0400

    clean up deployments

--- deploy/002_ECDSANodeManagement.ts
+++ deploy/002_ECDSANodeManagement.ts
@@ -2,15 +2,17 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
+  const { deployments, getNamedAccounts, getChainId } = hre
   const { deploy, get } = deployments
   const { deployer } = await getNamedAccounts()
 
-  await deploy('ECDSANodeManagement', {
-    from: deployer,
-    log: true,
-    skipIfAlreadyDeployed: true,
-  })
+  if ((await getChainId()) === '1') { 
+    await deploy('ECDSANodeManagement', {
+      from: deployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+    })
+  }
 }
 export default func
 func.tags = ['ECDSANodeManagement']
