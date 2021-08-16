3355237df77829265faf0343c61e24ced55d6030
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-08-15 20:03:46 -0400

    Bridgeconfig slight change to deploy script

--- deploy/000_BridgeConfig.ts
+++ deploy/000_BridgeConfig.ts
@@ -2,23 +2,25 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
+  const { deployments, getNamedAccounts, getChainId} = hre
   const { execute, deploy, get } = deployments
   const { deployer } = await getNamedAccounts()
 
-  await deploy('BridgeConfig', {
-    from: deployer,
-    log: true,
-    skipIfAlreadyDeployed: true,
-  })
+  if ((await getChainId()) === '1') { 
+    await deploy('BridgeConfig', {
+      from: deployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+    })
 
-  await execute(
-    "BridgeConfig",
-    { from: deployer, log: true },
-    "grantRole",
-    "0x4370dcf3e42e4d5b773a451bb8390ee8e7308f47681d1414cff87c2ad0512c85",
-    (await get("DevMultisig")).address,
-  )
+    await execute(
+      "BridgeConfig",
+      { from: deployer, log: true },
+      "grantRole",
+      "0x4370dcf3e42e4d5b773a451bb8390ee8e7308f47681d1414cff87c2ad0512c85",
+      (await get("DevMultisig")).address,
+    )
+  }
 }
 
 export default func
