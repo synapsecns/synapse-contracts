4f2d6761d435f5ff880f91e689446040afd9344e
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-08-15 20:47:12 -0400

    add Pausable to Bridge, prepare for deploy

--- deploy/001_SynapseBridge.ts
+++ deploy/001_SynapseBridge.ts
@@ -2,16 +2,31 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
+  const { deployments, getNamedAccounts, getChainId } = hre
   const { deploy, get } = deployments
-  const { deployer } = await getNamedAccounts()
+  const { deployer, bscMultisig, ethMultisig, polyMultisig } = await getNamedAccounts()
+  let multisig;
+
+
+  if ((await getChainId()) === '1') {
+    multisig = ethMultisig;
+  }
+
+  if ((await getChainId()) === '56') {
+    multisig = bscMultisig;
+  }
+
+  if ((await getChainId()) === '137') {
+    multisig = polyMultisig;
+  }
+
 
   await deploy('SynapseBridge', {
     from: deployer,
     log: true,
     skipIfAlreadyDeployed: true,
     proxy: {
-      owner: deployer,
+      owner: multisig,
       proxyContract: 'OpenZeppelinTransparentProxy',
     },
   })
