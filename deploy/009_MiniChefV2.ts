4768beae81382095a45fe3d62540d1c7054f7c70
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-08-25 01:07:36 -0400

    deploy MiniChefV2

---
+++ deploy/009_MiniChefV2.ts
@@ -0,0 +1,31 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, getChainId } = hre
+  const { deploy, get, execute } = deployments
+  const { deployer } = await getNamedAccounts()
+
+    const deployResult = await deploy('MiniChefV2', {
+        from: deployer,
+        log: true,
+        skipIfAlreadyDeployed: true,
+        args: [
+        (await get('SynapseToken')).address
+        ],
+    })
+
+    if (deployResult.newlyDeployed) {
+      await execute(
+        "MiniChefV2",
+        { from: deployer, log: true },
+        "transferOwnership",
+        (await get("DevMultisig")).address,
+        true,
+        false
+      )
+    }
+}
+
+export default func
+func.tags = ['MiniChefV2']
