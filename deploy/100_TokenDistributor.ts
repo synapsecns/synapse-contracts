6ab7dcf2b4efbb656acff3ba22a1e6bf58bf98bd
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-11-14 10:37:59 -0500

    nusd token dist

--- deploy/100_TokenDistributor.ts
+++ deploy/100_TokenDistributor.ts
@@ -14,6 +14,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       args: ["0x42F6f551ae042cBe50C739158b4f0CAC0Edb9096"],
     })
   }
+
+  if ((await getChainId()) === '43114') {
+    await deploy('nUSDTokenDistributor', {
+      contract: "TokenDistributor",
+      from: deployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+      args: ["0xCFc37A6AB183dd4aED08C204D1c2773c0b1BDf46"],
+    })
+  }
 }
 export default func
 func.tags = ['PoolConfig']
