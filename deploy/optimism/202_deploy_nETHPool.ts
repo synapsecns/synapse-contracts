2063f51b8bc17ecb0348ffe2789eb8325810fdb0
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-11-17 18:02:53 -0500

    Deploy custom WETH for optimism

--- deploy/optimism/202_deploy_nETHPool.ts
+++ deploy/optimism/202_deploy_nETHPool.ts
@@ -7,63 +7,63 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy, execute, get, getOrNull, log, read, save } = deployments
   const { deployer } = await getNamedAccounts()
 
-  // Manually check if the pool is already deployed
-  let ETHPool = await getOrNull("ETHPool")
-  if (ETHPool) {
-    log(`reusing "ETHPool" at ${ETHPool.address}`)
-  } else if (
-    (await getChainId()) != CHAIN_ID.OPTIMISM &&
-    (await getChainId()) != CHAIN_ID.HARDHAT
-  ) {
-    log(`Not Optimism or Hardhat`)
-  } else {
-    // Constructor arguments
-    const TOKEN_ADDRESSES = [
-      (await get("nETH")).address,
-      (await get("WETH")).address
-    ]
-    const TOKEN_DECIMALS = [18, 18]
-    const LP_TOKEN_NAME = "nETH-LP"
-    const LP_TOKEN_SYMBOL = "nETH-LP"
-    const INITIAL_A = 100
-    const SWAP_FEE = 4e6 // 4bps
-    const ADMIN_FEE = 6000000000
+  // // Manually check if the pool is already deployed
+  // let ETHPool = await getOrNull("ETHPool")
+  // if (ETHPool) {
+  //   log(`reusing "ETHPool" at ${ETHPool.address}`)
+  // } else if (
+  //   (await getChainId()) != CHAIN_ID.OPTIMISM &&
+  //   (await getChainId()) != CHAIN_ID.HARDHAT
+  // ) {
+  //   log(`Not Optimism or Hardhat`)
+  // } else {
+  //   // Constructor arguments
+  //   const TOKEN_ADDRESSES = [
+  //     (await get("nETH")).address,
+  //     (await get("WETH")).address
+  //   ]
+  //   const TOKEN_DECIMALS = [18, 18]
+  //   const LP_TOKEN_NAME = "nETH-LP"
+  //   const LP_TOKEN_SYMBOL = "nETH-LP"
+  //   const INITIAL_A = 100
+  //   const SWAP_FEE = 4e6 // 4bps
+  //   const ADMIN_FEE = 6000000000
 
-    await deploy('ETHPool', {
-        contract: 'SwapFlashLoan',
-        from: deployer,
-        log: true,
-        libraries: {
-            SwapUtils: (await get("SwapUtils")).address,
-            AmplificationUtils: (await get("AmplificationUtils")).address,
-          },
-        skipIfAlreadyDeployed: true,
-      })
+  //   await deploy('ETHPool', {
+  //       contract: 'SwapFlashLoan',
+  //       from: deployer,
+  //       log: true,
+  //       libraries: {
+  //           SwapUtils: (await get("SwapUtils")).address,
+  //           AmplificationUtils: (await get("AmplificationUtils")).address,
+  //         },
+  //       skipIfAlreadyDeployed: true,
+  //     })
 
-    const receipt = await execute(
-      "ETHPool",
-      { from: deployer, log: true },
-      "initialize",
-      TOKEN_ADDRESSES,
-      TOKEN_DECIMALS,
-      LP_TOKEN_NAME,
-      LP_TOKEN_SYMBOL,
-      INITIAL_A,
-      SWAP_FEE,
-      ADMIN_FEE,
-      (
-        await get("LPToken")
-      ).address,
-    )
+  //   const receipt = await execute(
+  //     "ETHPool",
+  //     { from: deployer, log: true },
+  //     "initialize",
+  //     TOKEN_ADDRESSES,
+  //     TOKEN_DECIMALS,
+  //     LP_TOKEN_NAME,
+  //     LP_TOKEN_SYMBOL,
+  //     INITIAL_A,
+  //     SWAP_FEE,
+  //     ADMIN_FEE,
+  //     (
+  //       await get("LPToken")
+  //     ).address,
+  //   )
 
-    const lpTokenAddress = (await read("ETHPool", "swapStorage")).lpToken
-    log(`ETH pool LP Token at ${lpTokenAddress}`)
+  //   const lpTokenAddress = (await read("ETHPool", "swapStorage")).lpToken
+  //   log(`ETH pool LP Token at ${lpTokenAddress}`)
 
-    await save("ETHPoolLPToken", {
-      abi: (await get("DAI")).abi, // Generic ERC20 ABI
-      address: lpTokenAddress,
-    })
-  }
+  //   await save("ETHPoolLPToken", {
+  //     abi: (await get("DAI")).abi, // Generic ERC20 ABI
+  //     address: lpTokenAddress,
+  //   })
+  // }
 }
 export default func
 func.tags = ["ETHPool"]
--- deploy/optimism/204_deploy_SYN.ts
+++ deploy/optimism/204_deploy_SYN.ts
@@ -1,29 +1,33 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { DeployFunction } from 'hardhat-deploy/types'
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
-  const { deploy, get, execute } = deployments
+  const { deploy, get,getOrNull, execute, log } = deployments
   const { deployer } = await getNamedAccounts()
 
-  await deploy('SynapseToken', {
-    contract: 'SynapseERC20',
-    from: deployer,
-    log: true,
-    skipIfAlreadyDeployed: true,
-  })
-
-  await execute(
-          "SynapseToken",
-          { from: deployer, log: true },
-          "initialize",
-          "Synapse",
-          "SYN",
-          "18",
-          (
-            await get("DevMultisig")
-          ).address,
-        )
+  let SynapseToken = await getOrNull("SynapseToken")
+  if (SynapseToken) {
+    log(`reusing 'SynapseToken' at ${SynapseToken.address}`)
+  } else {
+    await deploy("SynapseToken", {
+      contract: "SynapseERC20",
+      from: deployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+    })
+    await execute(
+      "SynapseToken",
+      { from: deployer, log: true },
+      "initialize",
+      "Synapse",
+      "SYN",
+      "18",
+      (
+        await get("DevMultisig")
+      ).address,
+    )
+  }
 }
 export default func
-func.tags = ['SynapseERC20']
+func.tags = ["SynapseERC20"]
