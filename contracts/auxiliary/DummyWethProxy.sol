0ebd790ffe8f1697a2bb9052b903d352e9ce30e4
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-11-17 23:02:09 -0500

    Dummy WETH Proxy Mocks and Tests

---
+++ contracts/auxiliary/DummyWethProxy.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import '@openzeppelin/contracts-upgradeable/proxy/Initializable.sol';
+
+interface IWETH9 {
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function decimals() external view returns (uint8);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function allowance(address, address) external view returns (uint256);
+
+    receive() external payable;
+
+    function deposit() external payable;
+
+    function withdraw(uint256 wad) external;
+
+    function totalSupply() external view returns (uint256);
+
+    function approve(address guy, uint256 wad) external returns (bool);
+
+    function transfer(address dst, uint256 wad) external returns (bool);
+
+    function transferFrom(
+        address src,
+        address dst,
+        uint256 wad
+    ) external returns (bool);
+}
+
+contract DummyWethProxy is Initializable, OwnableUpgradeable {
+    
+    function initialize() external initializer {
+        __Ownable_init();
+    }
+
+
+    IWETH9 public  WETH;
+    function setWETHAddress(address payable _weth) onlyOwner external{
+        WETH = IWETH9(_weth);
+    }
+
+    function withdrawToSelf(uint256 amount) external {
+        WETH.withdraw(amount);
+    }
+
+    function rescue(uint256 amount) onlyOwner external {
+        WETH.transfer(owner(), amount);
+    }
+
+    fallback() external payable {}
+}
