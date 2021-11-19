62a555b61eec0a5955a0c38e963af9e68ed65b47
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-11-18 21:11:19 -0500

    Deploy Minichef

--- deploy/009_MiniChefV2.ts
+++ deploy/009_MiniChefV2.ts
@@ -6,25 +6,25 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy, get, execute } = deployments
   const { deployer } = await getNamedAccounts()
 
-    // const deployResult = await deploy('MiniChefV2', {
-    //     from: deployer,
-    //     log: true,
-    //     skipIfAlreadyDeployed: true,
-    //     args: [
-    //     (await get('SynapseToken')).address
-    //     ],
-    // })
+    const deployResult = await deploy('MiniChefV2', {
+        from: deployer,
+        log: true,
+        skipIfAlreadyDeployed: true,
+        args: [
+        (await get('SynapseToken')).address
+        ],
+    })
 
-    // if (deployResult.newlyDeployed) {
-    //   await execute(
-    //     "MiniChefV2",
-    //     { from: deployer, log: true },
-    //     "transferOwnership",
-    //     (await get("DevMultisig")).address,
-    //     true,
-    //     false
-    //   )
-    // }
+    if (deployResult.newlyDeployed) {
+      await execute(
+        "MiniChefV2",
+        { from: deployer, log: true },
+        "transferOwnership",
+        (await get("DevMultisig")).address,
+        true,
+        false
+      )
+    }
 }
 
 export default func
