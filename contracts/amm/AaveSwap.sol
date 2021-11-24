37792a491aa32e62052dac95d27ab1e3d163f259
aureliusbtc <aureliusbtc@users.noreply.github.com>
2021-11-24 13:46:00 -0500

    first v aave rewards manager

--- contracts/amm/AaveSwap.sol
+++ contracts/amm/AaveSwap.sol
@@ -24,5 +24,62 @@ import "./Swap.sol";
  */
 
  contract AaveSwap is Swap {
-     
+     address internal AAVE_REWARDS;
+     address internal AAVE_LENDING_POOL;
+     address internal REWARD_TOKEN;
+     address internal REWARD_RECEIVER;
+     address[] internal AAVE_ASSETS;
+
+         /**
+     * @notice Initializes this Swap contract with the given parameters.
+     * This will also clone a LPToken contract that represents users'
+     * LP positions. The owner of LPToken will be this contract - which means
+     * only this contract is allowed to mint/burn tokens.
+     *
+     * @param _pooledTokens an array of ERC20s this pool will accept
+     * @param decimals the decimals to use for each pooled token,
+     * eg 8 for WBTC. Cannot be larger than POOL_PRECISION_DECIMALS
+     * @param lpTokenName the long-form name of the token to be deployed
+     * @param lpTokenSymbol the short symbol for the token to be deployed
+     * @param _a the amplification coefficient * n * (n - 1). See the
+     * StableSwap paper for details
+     * @param _fee default swap fee to be initialized with
+     * @param _adminFee default adminFee to be initialized with
+     * @param lpTokenTargetAddress the address of an existing LPToken contract to use as a target
+     */
+    function initialize(
+        IERC20[] memory _pooledTokens,
+        uint8[] memory decimals,
+        string memory lpTokenName,
+        string memory lpTokenSymbol,
+        uint256 _a,
+        uint256 _fee,
+        uint256 _adminFee,
+        address lpTokenTargetAddress
+    ) public virtual override initializer {
+        Swap.initialize(
+            _pooledTokens,
+            decimals,
+            lpTokenName,
+            lpTokenSymbol,
+            _a,
+            _fee,
+            _adminFee,
+            lpTokenTargetAddress
+        );
+        AAVE_REWARDS = 0x01d83fe6a10d2f2b7af17034343746188272cac9; 
+        AAVE_LENDING_POOL = 0x4F01AeD16D97E3aB5ab2B501154DC9bb0F1A5A2C;
+        REWARD_TOKEN = 0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7;
+        AAVE_ASSETS = [0x53f7c5869a859f0aec3d334ee8b4cf01e3492f21];
+        REWARD_RECEIVER = msg.sender;
+    }
+
+    function setRewardReceiver(address _reward_receiver) external onlyOwner {
+        REWARD_RECEIVER = _reward_receiver;
+    }
+
+    function claimAaveRewards() external {
+        AAVE_REWARDS.call(abi.encodeWithSignature("claimRewards(address[],uint256,address)", AAVE_ASSETS, type(uint256).max, REWARD_RECEIVER));
+    }
+
  }
