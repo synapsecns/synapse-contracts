f12fbb6ce8d53d416c142c16a86b195d2d32e22f
Maoz Dror <maozdror@protonmail.com>
2022-01-01 23:42:15 -0500

    Refactor large verifySignature functions into dedicated library
    Also make formatting better

---
+++ contracts/bridge/interfaces/IERC20Mintable.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+interface IERC20Mintable is IERC20 {
+    function mint(address to, uint256 amount) external;
+}
