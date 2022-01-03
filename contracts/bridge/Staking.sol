40ff9db7c6c7b51db03ba80a28b01e44a6f27632
Trajan0x <trajan0x@users.noreply.github.com>
2022-01-02 19:45:35 -0500

    backup contract

--- contracts/bridge/Staking.sol
+++ contracts/bridge/Staking.sol
@@ -1,23 +1,25 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.6.12;
-pragma experimental ABIEncoderV2;
 
-import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol';
 
 contract SynStaking {
-    using BoringERC20 for IERC20;
+
+    constructor(ERC20Burnable _synapse) public {
+        SYNAPSE = _synapse;
+    }
 
     /// @notice Address of SYNAPSE contract.
-    IERC20 public immutable SYNAPSE;
+    ERC20Burnable public immutable SYNAPSE;
 
-    event Bond(address indexed user, uint256 indexed amount, uint256 amount, string indexed to);
+    event Bond(address indexed user, uint256 indexed amount,  string indexed to);
 
-    function stake(uint256 _amount, string delegatee) external {
+    function stake(uint256 _amount, string memory delegatee) external {
+        require(_amount != 0);
         // transfer the token from the user to the staking contract
-        synapse.safeTransferFrom(msg.sender, address(this), _amount);
+        SYNAPSE.burnFrom(msg.sender, _amount);
         // burn the synapse in the contract
-        synapse.burn(_amount);
         emit Bond(msg.sender, _amount, delegatee);
     }
 }
