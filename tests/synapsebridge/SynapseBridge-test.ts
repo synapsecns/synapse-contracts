25e4e01dc1627c1c3575556891f22cc05e1d1105
Maoz Dror <maozdror@protonmail.com>
2022-01-13 15:37:19 -0500

    Much test very wow no timelock

--- tests/synapsebridge/SynapseBridge-test.ts
+++ tests/synapsebridge/SynapseBridge-test.ts
@@ -12,209 +12,118 @@ import {step} from "mocha-steps";
 
 import {expect} from "chai";
 
-import type {SynapseBridge, TimelockController, SynapseERC20, ERC20} from "../../build/typechain";
+import type {SynapseBridge, TimelockController, SynapseERC20} from "../../build/typechain";
 
 import {TestUtils, Birdies} from "../util";
 
 import {DeployUtils} from "../../deploy/utils";
 
-import {hexZeroPad} from "@ethersproject/bytes";
 import {Event as ContractEvent} from "@ethersproject/contracts";
-import {BigNumber, BigNumberish} from "@ethersproject/bignumber";
+import {BigNumber} from "@ethersproject/bignumber";
 import {CHAIN_ID} from "../../utils/network";
+import {Deployment} from "hardhat-deploy/types";
 
 
 describe("SynapseBridge", function(this: Mocha.Suite) {
-    const { deployments, getNamedAccounts } = hre;
+    const { getNamedAccounts } = hre;
     let
-        synapseBridge: SynapseBridge,
-        timelockController: TimelockController,
-        deployerAddr: string;
+        synapseBridge:      Deployment,
+        devMultisig:        Deployment,
+        deployerAddr:       string;
 
     step("setup", function(this: Context, done: Done) {
         this.timeout(120 * 1000);
 
-        deployments.fixture([
+        hre.deployments.log("setting up fixtures...")
+
+        hre.deployments.fixture([
             'DevMultisig',
             'Multicall2',
             'TimelockController',
             'SynapseBridge',
-            'SynapseERC20Factory',
-            'SynapseERC20',
-            'SynapseToken',
-            'nUSD',
-            'gOHM'
-        ], {keepExistingDeployments: false})
+            "SynapseERC20",
+            "SynapseERC20Factory",
+            "SynapseToken",
+            "nUSD",
+            "gOHM"
+        ], {keepExistingDeployments: true})
             .then(() => {
                 let preloadTasks = [
-                    TestUtils.contractInstanceFromDeployment('SynapseBridge', hre).then((c) => synapseBridge = c as SynapseBridge),
-                    TestUtils.contractInstanceFromDeployment('TimelockController', hre).then((c) => timelockController = c as TimelockController),
+                    hre.deployments.get("SynapseBridge").then((d) => synapseBridge = d),
+                    hre.deployments.get("DevMultisig").then((d) => devMultisig = d),
                     getNamedAccounts().then(({deployer}) => deployerAddr = deployer)
                 ]
 
                 expect(Promise.resolve(preloadTasks)).to.eventually.be.fulfilled.notify(done);
             })
-    })
-
-    describe("AccessControl fuckery", function(this: Mocha.Suite) {
-        let deployerIsBridgeAdmin: boolean;
-
-        step("check if I have DEFAULT_ADMIN_ROLE on SynapseBridge", function (this: Context, done: Done) {
-            this.timeout(10 * 1000);
-
-            expect(synapseBridge).to.not.be.undefined;
-            expect(deployerAddr).to.not.equal("");
-
-            synapseBridge.hasRole(DeployUtils.Roles.DefaultAdminRole, deployerAddr)
-                .then((res: boolean) => {
-                    deployerIsBridgeAdmin = res;
-                    done();
-                })
-                .catch(TestUtils.catchError(done))
-        })
-
-        if (!deployerIsBridgeAdmin) {
-            let
-                scheduledGrantRoleId: string,
-                encodedFunc: string;
-
-            const
-                schedulerValue: BigNumberish = 0,
-                schedulerSalt = ethers.utils.randomBytes(32),
-                schedulerDelay = 181;
-
-            describe.skip("TimelockController testing I guess", function (this: Mocha.Suite) {
-                step(`schedule up a grantRole call for me`, function (this: Context, done: Done) {
-                    this.timeout(13 * 1000);
-
-                    expect(synapseBridge).to.not.be.undefined;
-                    expect(deployerAddr).to.not.equal("");
-
-                    encodedFunc = synapseBridge.interface.encodeFunctionData('grantRole', [DeployUtils.Roles.DefaultAdminRole, deployerAddr]);
-
-                    let hashCall: Promise<boolean> = timelockController.hashOperation(
-                        deployerAddr,
-                        schedulerValue,
-                        encodedFunc,
-                        hexZeroPad('0x0', 32),
-                        schedulerSalt,
-                        {from: deployerAddr}
-                    ).then((hash: string): boolean => {
-                        scheduledGrantRoleId = hash;
-
-                        return true
-                    });
-
-                    expect(hashCall).to.eventually.be.true;
-
-                    Birdies.expectTxnReceiptSuccess(timelockController.schedule(
-                            deployerAddr,
-                            schedulerValue,
-                            encodedFunc,
-                            hexZeroPad('0x0', 32),
-                            schedulerSalt,
-                            schedulerDelay,
-                            {from: deployerAddr}
-                        ).then(TestUtils.waitForConfirmations(1)).catch(TestUtils.catchError(done))
-                    ).notify(done);
-                })
-
-                step(`guess we're waiting 181 blocks...`, function (this: Context, done: Done) {
-                    this.timeout(240 * 1000);
-
-                    TestUtils.pollContract(timelockController.isOperationReady, 10, done, scheduledGrantRoleId);
-                })
-
-                step("execute grantRole(DEFAULT_ADMIN_ROLE) action", function (this: Context, done: Done) {
-                    this.timeout(13 * 1000);
-
-                    Birdies.expectTxnReceiptSuccess(timelockController.execute(
-                            deployerAddr,
-                            schedulerValue,
-                            encodedFunc,
-                            hexZeroPad('0x0', 32),
-                            schedulerSalt,
-                            {from: deployerAddr}
-                        ).then(TestUtils.waitForConfirmations(5)).catch(TestUtils.catchError(done))
-                    ).notify(done)
-                })
-
-                step("ensure I have DEFAULT_ADMIN_ROLE on the Bridge contract", function (this: Context, done: Done) {
-                    this.timeout(240 * 1000);
-
-                    expect(timelockController.isOperationDone(scheduledGrantRoleId))
-                        .to.eventually.be.true.notify(done);
-
-                    TestUtils.pollContract(synapseBridge.hasRole, 5, done, DeployUtils.Roles.DefaultAdminRole, deployerAddr, {from: deployerAddr});
-                })
-            })
-        }
+            .catch(TestUtils.catchError(done))
     })
 
     describe("Basic read-only function tests", function(this: Mocha.Suite) {
         step("should be able to fetch the chain gas amount", function(this: Context, done: Done) {
-            expect(synapseBridge.chainGasAmount())
-                .to.eventually.be.fulfilled.notify(done);
+            hre.deployments.read(
+                "SynapseBridge",
+                "chainGasAmount"
+            ).then((res: BigNumber) => {
+                expect(res).to.be.gte(0);
+                done();
+            })
         })
     })
 
     describe("do some weird stuff with Tokens", function(this: Mocha.Suite) {
-        const { deployments: {get, execute} } = hre;
+        const { deployments: {execute} } = hre;
 
         const tokenNames = ["SynapseToken", "nUSD", "gOHM"];
 
-        step("give meself mint privileges", async function(this: Context, done: Done) {
+        describe("give meself mint privileges", function(this: Mocha.Suite) {
             this.timeout(30*1000);
-
-            const devMS = await get("DevMultisig");
-
-            const execFunc = async (tokenName: string) => {
-                let txn = await execute(
-                    tokenName,
-                    { from: devMS.address, log: true },
-                    "grantRole",
-                    DeployUtils.Roles.SynapseERC20MinterRole,
-                    devMS.address
-                );
-
-                Birdies.expectTxnReceiptSuccess(txn);
-
-                let txn2 = await (await TestUtils.signer(hre)).provider.getTransaction(txn.transactionHash);
-                await txn2.wait(2);
-
-                return;
-            }
 
             for (const tName of tokenNames) {
-                await execFunc(tName);
+                step(`should make me a minter for ${tName}`, function(this: Context, done: Done) {
+                    execute(
+                        tName,
+                        {from: devMultisig.address, log: true},
+                        "grantRole",
+                        DeployUtils.Roles.SynapseERC20MinterRole,
+                        devMultisig.address
+                    ).then((txn) => {
+                        // Birdies.expectTxnReceiptSuccess(txn);
+                        TestUtils.signer(hre).then((s) => {
+                            s.provider.getTransaction(txn.transactionHash).then((txn2) => {
+                                Birdies.expectTxnReceiptSuccess(txn2.wait(2)).notify(done)
+                            })
+                        }).catch(TestUtils.catchError(done))
+                    })
+                })
             }
-
-            done();
         })
 
-        step("mint some shiz", function(this: Context, done: Done) {
-            this.timeout(30*1000);
-
+        describe("mint some shiz", function(this: Mocha.Suite) {
             const desiredBalance: BigNumber = ethers.utils.parseEther('2000');
 
-            const minterFunc = async (tokenName: string, nonce: number) => {
-                const instance = await TestUtils.SynapseERC20Instance(tokenName, hre);
+            tokenNames.forEach((tName: string) => {
+                step(`should mint some ${tName}`, function(this: Context, done: Done) {
+                    this.timeout(20*1000);
 
-                let populated = await instance.populateTransaction.mint(deployerAddr, desiredBalance);
-                populated.nonce = nonce;
-
-                let txn = await (await TestUtils.signer(hre)).sendTransaction(populated);
-                await txn.wait(2);
-                Birdies.expectBigNumber((await instance.balanceOf(deployerAddr)), desiredBalance);
-            }
-
-            TestUtils.getLastNonce(hre)
-                .then((lastNonce) => {
-                    expect(Promise.resolve(
-                        tokenNames.map((tName, idx): Promise<void> =>
-                            minterFunc(tName, lastNonce++))
-                    )).to.eventually.be.fulfilled.notify(done);
+                    execute(
+                        tName,
+                        {from: devMultisig.address, log: true},
+                        "mint",
+                        deployerAddr,
+                        desiredBalance
+                    ).then(() =>
+                        TestUtils.getSynapseERC20Balance(
+                            hre,
+                            tName,
+                            deployerAddr
+                        ).then((bal: BigNumber) => {
+                            expect(bal).to.equal(desiredBalance);
+                            done()
+                        })
+                    )
                 })
+            })
         })
     })
 
@@ -222,99 +131,92 @@ describe("SynapseBridge", function(this: Mocha.Suite) {
         interface testCase {
             amt:         BigNumber,
             token:       string,
-            deployment?: boolean,
             toChain:     number,
         }
 
-        [
+        const testCases: testCase[] = [
             {
                 amt:       ethers.utils.parseEther('27'),
                 toChain:   parseInt(CHAIN_ID.BSC),
                 token:     "SynapseToken",
-                deployment: true,
             },
             {
                 amt:       ethers.utils.parseEther('522'),
                 toChain:   parseInt(CHAIN_ID.MAINNET),
                 token:     "SynapseToken",
-                deployment: true,
             },
             {
                 amt:       ethers.utils.parseEther('420'),
                 toChain:   parseInt(CHAIN_ID.POLYGON),
                 token:     "nUSD",
-                deployment: true,
             }
-        ].forEach((tc: testCase, idx) => {
-            let
-                instance: SynapseERC20|ERC20,
-                lastNonce: number;
+        ]
 
-            step("setup", function(this: Context, done: Done) {
-                let prom1 = ((tc.deployment ?? false)
-                        ? TestUtils.SynapseERC20Instance(tc.token, hre)
-                        : TestUtils.ERC20Instance(tc.token, hre))
-                    .then((c) => instance = c);
+        describe(`should deposit tokens into the Bridge`, function(this: Mocha.Suite) {
+            testCases.forEach((tc) => {
+                step(`should approve ${tc.amt} of ${tc.token} for spending`, function(this: Context, done: Done) {
+                    this.timeout(10*1000)
+                    hre.deployments.execute(
+                        tc.token,
+                        {from: deployerAddr, log: true},
+                        "approve",
+                        synapseBridge.address,
+                        tc.amt
+                    )
+                        .then((txn) => Birdies.expectTxnReceiptSuccess(txn).notify(done))
+                        .catch(TestUtils.catchError(done))
+                })
 
+                step(`should deposit ${tc.amt} of ${tc.token}`, function(this: Context, done: Done) {
+                    this.timeout(30*1000);
 
-                let prom2 = TestUtils.getLastNonce(hre).then((n) => lastNonce = n);
+                    let startBalProm: Promise<BigNumber> = Promise.resolve(
+                        hre.deployments.read(
+                            tc.token,
+                            "balanceOf",
+                            deployerAddr
+                        ).then((bal: BigNumber) => bal)
+                    )
 
-                expect(Promise.resolve([prom1, prom2])).to.eventually.be.fulfilled.notify(done);
-            })
+                    hre.deployments.get(tc.token).then(({address: tokenAddress}) => {
+                        hre.deployments.execute(
+                            "SynapseBridge",
+                            {from:deployerAddr, log:true},
+                            "deposit",
+                            deployerAddr,
+                            tc.toChain,
+                            tokenAddress,
+                            tc.amt,
+                        ).then(txnReceipt=> {
+                            Birdies.expectArrayObject(txnReceipt.events, "event", "TokenDeposit");
+                            const eventLog: ContractEvent = txnReceipt.events.find((e) => e.event === "TokenDeposit");
+                            expect(eventLog.args).to.have.a.lengthOf(4);
 
-            step("approve transfer", async function(this: Context, done: Done) {
-                this.timeout(10*1000);
+                            const testVals: any[] = [deployerAddr, tc.toChain, tokenAddress, tc.amt];
 
-                const f = txnReceipt => Birdies.expectTxnReceiptSuccess(txnReceipt);
+                            eventLog.args.forEach((arg: any, idx: number) => {
+                                if (arg instanceof BigNumber) {
+                                    Birdies.expectBigNumber(arg, testVals[idx]);
+                                } else if (typeof arg === 'string') {
+                                    Birdies.expectString(arg, testVals[idx]);
+                                }
+                            })
 
-                let populated = await instance.populateTransaction.approve(synapseBridge.address, tc.amt, {from:deployerAddr});
-                populated.nonce = lastNonce++;
+                            TestUtils.getSynapseERC20Balance(
+                                hre,
+                                tc.token,
+                                deployerAddr
+                            ).then((bal) => {
+                                startBalProm.then((startBal) => {
+                                    let diff: BigNumber = startBal.sub(tc.amt);
+                                    expect(bal).to.equal(diff);
 
-                let r = await (await TestUtils.signer(hre)).sendTransaction(populated);
-                await f(r.wait(2));
-
-                done();
-            })
-
-            step(`should deposit ${ethers.utils.formatEther(tc.amt)} of token into the Bridge`, async function(this: Context, done: Done) {
-                this.timeout(20*1000);
-
-                let startBal: BigNumber = await instance.balanceOf(deployerAddr).then(b => b.sub(tc.amt));
-
-                const f = async txnReceipt => {
-                    Birdies.expectTxnReceiptSuccess(txnReceipt);
-
-                    txnReceipt = await txnReceipt;
-                    Birdies.expectArrayObject(txnReceipt.events, "event", "TokenDeposit");
-
-                    const eventLog: ContractEvent = txnReceipt.events.find((e) => e.event === "TokenDeposit");
-
-                    expect(eventLog.args).to.have.a.lengthOf(4);
-
-                    Birdies.expectString(eventLog.args[0], deployerAddr)
-
-                    Birdies.expectBigNumber(eventLog.args[1], tc.toChain)
-
-                    Birdies.expectString(eventLog.args[2], instance.address)
-
-                    Birdies.expectBigNumber(eventLog.args[3], tc.amt)
-
-                    Birdies.expectBigNumber(instance.balanceOf(deployerAddr), startBal);
-                }
-
-                let populated = await synapseBridge.populateTransaction.deposit(
-                    deployerAddr,
-                    tc.toChain,
-                    instance.address,
-                    tc.amt,
-                    { from: deployerAddr }
-                );
-                populated.nonce = lastNonce++;
-
-                let txn = await (await TestUtils.signer(hre)).sendTransaction(populated);
-                await f(txn.wait(2));
-
-                done();
+                                    done();
+                                })
+                            })
+                        })
+                    })
+                })
             })
         })
     })
--- tests/util/util.ts
+++ tests/util/util.ts
@@ -1,3 +1,5 @@
+// noinspection JSUnusedGlobalSymbols
+
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-deploy";
 
@@ -9,14 +11,12 @@ import {Done} from "mocha";
 
 import {Contract, ContractReceipt, ContractTransaction} from "@ethersproject/contracts";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
-import {BigNumber} from "ethers";
+import {BigNumber, PopulatedTransaction} from "ethers";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {Deployment} from "hardhat-deploy/types";
 import {ERC20, SynapseERC20} from "../../build/typechain";
-import {expect} from "chai";
 import {Birdies} from "./birdies";
-
-export const ZeroAddress: string = "0x0000000000000000000000000000000000000000";
+import {TransactionResponse} from "@ethersproject/abstract-provider";
 
 export namespace TestUtils {
     export const
@@ -24,22 +24,6 @@ export namespace TestUtils {
         SYNAPSE_ERC20_MINTER_ROLE:      string = ethers.utils.hexlify("0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6"),
         SYNAPSEBRIDGE_NODEGROUP_ROLE:   string = ethers.utils.hexlify("0xb5c00e6706c3d213edd70ff33717fac657eacc5fe161f07180cf1fcab13cc4cd"),
         SYNAPSEBRIDGE_GOVERNANCE_ROLE:  string = ethers.utils.hexlify("0x71840dc4906352362b0cdaf79870196c8e42acafade72d5d5a6d59291253ceb1");
-
-    export async function getDeployment(name: string, hre: HardhatRuntimeEnvironment): Promise<Deployment> {
-        let {deployments: {get}} = hre;
-
-        return get(name);
-    }
-
-    export async function deploymentAddress(name: string, hre: HardhatRuntimeEnvironment): Promise<string|null> {
-        return Promise.resolve(getDeployment(name, hre)
-            .then(({address}) => address)
-            .catch((err: any): any => {
-                console.error(err instanceof Error ? err : new Error(err));
-                return null
-            })
-        )
-    }
 
     export function doneWithError(err: any, done: Done) {
         let e = err instanceof Error ? err : new Error(err);
@@ -51,59 +35,20 @@ export namespace TestUtils {
 
 
     export async function signer(hre: HardhatRuntimeEnvironment): Promise<SignerWithAddress> {
-        return (await ethers.getSigner((await hre.getNamedAccounts()).deployer))
+        return hre.getNamedAccounts().then(({deployer}) => hre.ethers.getSigner(deployer));
     }
 
-    export async function contractInstanceFromDeployment(name: string, hre: HardhatRuntimeEnvironment): Promise<Contract> {
-        const
-            {deployments: {get}, getNamedAccounts} = hre,
-            {deployer}     = await getNamedAccounts(),
-            {address, abi} = await get(name),
-            signer         = await ethers.getSigner(deployer);
-
-        return await ethers.getContractAt(abi, address, signer)
-    }
-
-    export function waitForConfirmations(
-        confs?: number,
-        fn?: (txReceipt: ContractReceipt) => any|void
-    ): (txn: ContractTransaction) => Promise<any|void> {
-        fn = fn ?? (txReceipt => txReceipt)
-        return (txn: ContractTransaction): Promise<any|void> => txn.wait(confs ?? 1).then(fn)
-    }
-
-    export async function pollContract(f: (...args: any) => Promise<boolean>, seconds: number, done: Done, ...args: any) {
-        let isReady = await f(...args);
-
-        let interval = setInterval(async () => {
-            isReady = await f(...args);
-            if (isReady) {
-                clearInterval(interval);
-                done();
-            }
-        }, seconds*1000);
-    }
-
-    export async function getLastNonce(hre: HardhatRuntimeEnvironment): Promise<number> {
-        const
-            {deployer} = await hre.getNamedAccounts(),
-            signer = await ethers.getSigner(deployer);
-
-        return signer.getTransactionCount('pending');
-    }
-
-    export async function checkWalletBalance(hre: HardhatRuntimeEnvironment): Promise<BigNumber> {
-        return (await signer(hre)).getBalance()
-    }
-
-    export async function ERC20Instance(address: Birdies.KindaPromise<string>, hre: HardhatRuntimeEnvironment): Promise<ERC20> {
-        return (await hre.ethers.getContractAt("ERC20", await address)) as ERC20;
-    }
-
-    export async function SynapseERC20Instance(
-        name: string,
-        hre:  HardhatRuntimeEnvironment
-    ): Promise<SynapseERC20> {
-        return (await contractInstanceFromDeployment(name, hre)) as SynapseERC20
+    export function getSynapseERC20Balance(
+        hre: HardhatRuntimeEnvironment,
+        deploymentName: string,
+        addr: string
+    ): Promise<BigNumber> {
+        return Promise.resolve(
+            hre.deployments.read(
+                deploymentName,
+                "balanceOf",
+                addr
+            ).then((bal: BigNumber) => bal)
+        )
     }
 }
