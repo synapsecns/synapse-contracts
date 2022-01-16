27e57da7d96ea4337c49cc60de8a8834101637d9
aureliusbtc <aureliusbtc@users.noreply.github.com>
2022-01-16 17:01:44 -0500

    GMXTokenWrapper

--- contracts/bridge/wrappers/MoonriverBridgeZap.sol
+++ contracts/bridge/wrappers/MoonriverBridgeZap.sol
@@ -36,7 +36,7 @@ contract MoonriverBridgeZap {
         synapseBridge = _synapseBridge;
         swapMap[tokenOne] = _swapOne;
         swapMap[tokenTwo] = _swapTwo;
-
+        IERC20(_wethAddress).safeIncreaseAllowance(address(_synapseBridge), MAX_UINT256);
         if (address(_swapOne) != address(0)) {
             {
                 uint8 i;
@@ -274,6 +274,24 @@ contract MoonriverBridgeZap {
         }
         synapseBridge.deposit(to, chainId, token, amount);
     }
+
+    /**
+   * @notice Wraps SynapseBridge deposit() function to make it compatible w/ ETH -> WETH conversions
+   * @param to address on other chain to bridge assets to
+   * @param chainId which chain to bridge assets onto
+   * @param amount Amount in native token decimals to transfer cross-chain pre-fees
+   **/
+  function depositETH(
+    address to,
+    uint256 chainId,
+    uint256 amount
+    ) external payable {
+      require(msg.value > 0 && msg.value == amount, 'INCORRECT MSG VALUE');
+      IWETH9(WETH_ADDRESS).deposit{value: msg.value}();
+      synapseBridge.deposit(to, chainId, IERC20(WETH_ADDRESS), amount);
+    }
+
+
 
     function swapETHAndRedeem(
         address to,
