b2c7740a83d7a09cddff4302c64aa5ed2c399a99
aureliusbtc <aureliusbtc@users.noreply.github.com>
2022-01-17 00:45:08 -0500

    gmx redeploy

--- contracts/bridge/wrappers/GMXWrapper.sol
+++ contracts/bridge/wrappers/GMXWrapper.sol
@@ -7,13 +7,35 @@ pragma solidity 0.6.12;
 interface IGMX {
     function burn(address _account, uint256 _amount) external;
     function balanceOf(address account) external view returns (uint256);
+    function mint(address _account, uint256 _amount) external;
 }
 
 contract GMXWrapper {
     using SafeMath for uint256;
 
-    address constant gmx = 0x62edc0692BD897D2295872a9FFCac5425011c661;
-    address constant bridge = 0xC05e61d0E7a63D27546389B7aD62FdFf5A91aACE;
+    address constant public gmx = 0x62edc0692BD897D2295872a9FFCac5425011c661;
+    address constant public bridge = 0xC05e61d0E7a63D27546389B7aD62FdFf5A91aACE;
+
+    function transfer(address _recipient, uint256 _amount) external returns (bool) {
+        require(msg.sender == bridge);
+        _transfer(msg.sender, _recipient, _amount);
+        return true;
+    }
+
+    function _transfer(address _sender, address _recipient, uint256 _amount) private {
+        require(_sender != address(0), "BaseToken: transfer from the zero address");
+        require(_recipient != address(0), "BaseToken: transfer to the zero address");
+        IGMX(gmx).burn(_sender, _amount);
+        IGMX(gmx).mint(_recipient, _amount);
+    }
+
+    function mint(address _addr, uint256 _amount) external {
+        require(msg.sender == bridge);
+        uint256 preMint = IGMX(gmx).balanceOf(_addr);
+        IGMX(gmx).mint(_addr, _amount);
+        uint256 postMint = IGMX(gmx).balanceOf(_addr);
+        require(preMint.add(_amount) == postMint, "Mint incomplete");
+    }
 
     function burnFrom(address _addr, uint256 _amount) external {
         require(msg.sender == bridge);
