d22d0a985ea8a7baa6b59e07240cb6ac33af592d
aureliusbtc <aureliusbtc@users.noreply.github.com>
2022-01-16 23:36:05 -0500

    Complete GMX Wrapper

--- contracts/bridge/wrappers/GMXWrapper.sol
+++ contracts/bridge/wrappers/GMXWrapper.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/math/SafeMath.sol';
 
 pragma solidity 0.6.12;
 
-interface IGMX is IERC20 {
+interface IGMX {
     function burn(address _account, uint256 _amount) external;
+    function balanceOf(address account) external view returns (uint256);
 }
 
-contract GMXTokenWraper {
+contract GMXWrapper {
     using SafeMath for uint256;
 
     address constant gmx = 0x62edc0692BD897D2295872a9FFCac5425011c661;
