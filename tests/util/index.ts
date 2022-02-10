9f439dbbb7540a4f484ed25419807554d3e5b4e0
Trajan0x <trajan0x@users.noreply.github.com>
2022-02-09 22:20:46 -0500

    add minichef test

--- tests/util/index.ts
+++ tests/util/index.ts
@@ -1,4 +1,8 @@
 import "./chaisetup";
+import "./time";
+import "./deploy"
 
 export * from "./birdies";
 export * from "./util";
+export * from "./time";
+export * from "./deploy";
--- tests/util/util.ts
+++ tests/util/util.ts
@@ -23,7 +23,8 @@ export namespace TestUtils {
         DEFAULT_ADMIN_ROLE:             string = ethers.utils.hexZeroPad("0x0", 32),
         SYNAPSE_ERC20_MINTER_ROLE:      string = ethers.utils.hexlify("0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6"),
         SYNAPSEBRIDGE_NODEGROUP_ROLE:   string = ethers.utils.hexlify("0xb5c00e6706c3d213edd70ff33717fac657eacc5fe161f07180cf1fcab13cc4cd"),
-        SYNAPSEBRIDGE_GOVERNANCE_ROLE:  string = ethers.utils.hexlify("0x71840dc4906352362b0cdaf79870196c8e42acafade72d5d5a6d59291253ceb1");
+        SYNAPSEBRIDGE_GOVERNANCE_ROLE:  string = ethers.utils.hexlify("0x71840dc4906352362b0cdaf79870196c8e42acafade72d5d5a6d59291253ceb1"),
+        ADDRESS_ZERO:                   string = "0x0000000000000000000000000000000000000000"
 
     export function doneWithError(err: any, done: Done) {
         let e = err instanceof Error ? err : new Error(err);
