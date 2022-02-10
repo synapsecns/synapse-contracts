492cf1d68e4220290cdf2011fdc5a143833c859c
Trajan0x <trajan0x@users.noreply.github.com>
2022-02-09 20:11:57 -0500

    wagmi first pass

--- contracts/bridge/interfaces/IMiniChefV2.sol
+++ contracts/bridge/interfaces/IMiniChefV2.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
 interface IMiniChefV2 {
     struct UserInfo {
         uint256 amount;
@@ -14,6 +16,8 @@ interface IMiniChefV2 {
         uint64 allocPoint;
     }
 
+    function lpToken(uint) external view returns(IERC20);
+    function poolInfo(uint) external view returns (IMiniChefV2.PoolInfo memory);
     function poolLength() external view returns (uint256);
     function updatePool(uint256 pid) external returns (IMiniChefV2.PoolInfo memory);
     function userInfo(uint256 _pid, address _user) external view returns (uint256, uint256);
