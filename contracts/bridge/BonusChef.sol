909ce9c5d92f491369ae2f47af9f8573880b5b58
Trajan0x <trajan0x@users.noreply.github.com>
2022-02-10 19:17:30 -0500

    switch to experimental decoder

--- contracts/bridge/BonusChef.sol
+++ contracts/bridge/BonusChef.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: ISC
 
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/math/Math.sol";
--- contracts/bridge/BonusChefFactory.sol
+++ contracts/bridge/BonusChefFactory.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: ISC
 
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import {IMiniChefV2} from "./interfaces/IMiniChefV2.sol";
