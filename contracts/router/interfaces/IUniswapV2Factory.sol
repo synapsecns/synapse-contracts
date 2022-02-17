ced77eaf58706ae7ce89702456694ac7a360f2af
chisquared <ChiTimesChi@protonmail.com>
2022-02-17 22:55:11 +0300

    uniswap adapter

---
+++ contracts/router/interfaces/IUniswapV2Factory.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IUniswapV2Factory {
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+}
