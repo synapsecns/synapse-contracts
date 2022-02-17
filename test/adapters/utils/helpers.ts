31733651456047140fe9275d07c4bc9426097f03
chisquared <ChiTimesChi@protonmail.com>
2022-02-17 22:55:56 +0300

    tests for uniswap adapter

--- test/adapters/utils/helpers.ts
+++ test/adapters/utils/helpers.ts
@@ -2,13 +2,13 @@
 
 import {ethers, network } from "hardhat"
 
-export async function setBalance(userAddress, tokenAddress, amount) {
+export async function setBalance(userAddress, tokenAddress, amount, storage = 0) {
 	const encode = (types, values) =>   
 	  ethers.utils.defaultAbiCoder.encode(types, values);
   
 	const index = ethers.utils.solidityKeccak256(
 	  ["uint256", "uint256"],
-	  [userAddress, 0] // slot = 0 for bridged tokens on Avalanche
+	  [userAddress, storage] // slot = 0 for bridged tokens on Avalanche
 	);
   
 	await network.provider.send("hardhat_setStorageAt", [
