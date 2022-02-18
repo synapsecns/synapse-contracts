263eb463608664c4c02169c9e27c19ffa6f3406f
chisquared <ChiTimesChi@protonmail.com>
2022-02-18 20:46:39 +0300

    Curve adapters

---
+++ contracts/router/interfaces/ICurvePool.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface ICurvePool {
+    // Base pool functions
+
+    function coins(uint256 arg0) external view returns (address);
+
+    function get_dy(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+
+    // Meta pool functions
+
+    function get_dy_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function exchange_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external returns (uint256);
+
+    function underlying_coins(uint256 arg0) external view returns (address);
+
+    function base_coins(uint256 arg0) external view returns (address);
+}
