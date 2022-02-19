1ec561106bf4603704f2968c6804746e33ca6e68
chisquared <ChiTimesChi@protonmail.com>
2022-02-19 23:02:40 +0300

    add more functions to Curve interface

--- contracts/router/interfaces/ICurvePool.sol
+++ contracts/router/interfaces/ICurvePool.sol
@@ -3,43 +3,58 @@
 pragma solidity ^0.8.0;
 
 interface ICurvePool {
-    // Base pool functions
+    // Coin getters
 
     function coins(uint256 arg0) external view returns (address);
 
+    function base_coins(uint256 arg0) external view returns (address);
+
+    function underlying_coins(uint256 arg0) external view returns (address);
+
+    // Quote functions (int128)
+
     function get_dy(
         int128 i,
         int128 j,
         uint256 dx
     ) external view returns (uint256);
-
-    function get_dy(
-        uint256 i,
-        uint256 j,
-        uint256 dx
-    ) external view returns (uint256);
-
-    function exchange(
-        int128 i,
-        int128 j,
-        uint256 dx,
-        uint256 min_dy
-    ) external;
-
-    function exchange(
-        uint256 i,
-        uint256 j,
-        uint256 dx,
-        uint256 min_dy
-    ) external;
-
-    // Meta pool functions
 
     function get_dy_underlying(
         int128 i,
         int128 j,
         uint256 dx
     ) external view returns (uint256);
+
+    // Quote functions (uint256)
+
+    function get_dy(
+        uint256 i,
+        uint256 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function get_dy_underlying(
+        uint256 i,
+        uint256 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    // Swap functions (int128)
+
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy,
+        address receiver
+    ) external;
 
     function exchange_underlying(
         int128 i,
@@ -48,7 +63,43 @@ interface ICurvePool {
         uint256 min_dy
     ) external returns (uint256);
 
-    function underlying_coins(uint256 arg0) external view returns (address);
+    function exchange_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy,
+        address receiver
+    ) external returns (uint256);
 
-    function base_coins(uint256 arg0) external view returns (address);
+    // Swap functions (uint256)
+
+    function exchange(
+        uint256 i,
+        uint256 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+
+    function exchange(
+        uint256 i,
+        uint256 j,
+        uint256 dx,
+        uint256 min_dy,
+        address receiver
+    ) external;
+
+    function exchange_underlying(
+        uint256 i,
+        uint256 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+
+    function exchange_underlying(
+        uint256 i,
+        uint256 j,
+        uint256 dx,
+        uint256 min_dy,
+        address receiver
+    ) external;
 }
