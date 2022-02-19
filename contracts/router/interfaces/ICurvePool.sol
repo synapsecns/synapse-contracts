a29c2e3b29e0977a026d4e5d4b0a3394531b8c6a
chisquared <ChiTimesChi@protonmail.com>
2022-02-19 15:19:35 +0300

    add triCrypto adapter + tests

--- contracts/router/interfaces/ICurvePool.sol
+++ contracts/router/interfaces/ICurvePool.sol
@@ -13,9 +13,22 @@ interface ICurvePool {
         uint256 dx
     ) external view returns (uint256);
 
+    function get_dy(
+        uint256 i,
+        uint256 j,
+        uint256 dx
+    ) external view returns (uint256);
+
     function exchange(
         int128 i,
         int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+
+    function exchange(
+        uint256 i,
+        uint256 j,
         uint256 dx,
         uint256 min_dy
     ) external;
