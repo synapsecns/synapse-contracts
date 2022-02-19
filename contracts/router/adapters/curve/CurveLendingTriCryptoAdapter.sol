29138ad76d65bcc4c99df31b62d7f9520691a9bc
chisquared <ChiTimesChi@protonmail.com>
2022-02-19 23:06:28 +0300

    adapter for Curve triCrypto pool on AVAX

---
+++ contracts/router/adapters/curve/CurveLendingTriCryptoAdapter.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Adapter} from "../../Adapter.sol";
+
+import {IERC20} from "@synapseprotocol/sol-lib/contracts/solc8/erc20/IERC20.sol";
+import {ICurvePool} from "../../interfaces/ICurvePool.sol";
+
+import {CurveAbstractAdapter} from "./CurveAbstractAdapter.sol";
+import {SafeCast} from "@openzeppelin/contracts-4.4.2/utils/math/SafeCast.sol";
+
+contract CurveLendingTriCryptoAdapter is CurveAbstractAdapter {
+    uint256 private immutable numberStablecoins;
+
+    mapping(address => uint256) public tokenIndex;
+
+    constructor(
+        string memory _name,
+        address _pool,
+        uint256 _swapGasEstimate,
+        bool _directSwapSupported,
+        address _basePool
+    )
+        CurveAbstractAdapter(
+            _name,
+            _pool,
+            _swapGasEstimate,
+            _directSwapSupported
+        )
+    {
+        numberStablecoins = _getBasePoolSize(_basePool);
+    }
+
+    function _setPoolTokens() internal virtual override {
+        for (uint8 i = 0; true; i++) {
+            try pool.underlying_coins(i) returns (address _tokenAddress) {
+                _addPoolToken(_tokenAddress, i);
+            } catch {
+                break;
+            }
+        }
+    }
+
+    function _addPoolToken(address _tokenAddress, uint8 _index)
+        internal
+        virtual
+        override
+    {
+        isPoolToken[_tokenAddress] = true;
+        tokenIndex[_tokenAddress] = _index;
+    }
+
+    function _getBasePoolSize(address _basePoolAddress)
+        internal
+        view
+        returns (uint256 _numCoins)
+    {
+        ICurvePool _basePool = ICurvePool(_basePoolAddress);
+        _numCoins = 0;
+        for (;;) {
+            try _basePool.coins(_numCoins) {
+                _numCoins++;
+            } catch {
+                break;
+            }
+        }
+    }
+
+    function _doDirectSwap(
+        uint256 _amountIn,
+        address _tokenIn,
+        address _tokenOut,
+        address _to
+    ) internal virtual override {
+        pool.exchange_underlying(
+            tokenIndex[_tokenIn],
+            tokenIndex[_tokenOut],
+            _amountIn,
+            0,
+            _to
+        );
+    }
+
+    function _doIndirectSwap(
+        uint256 _amountIn,
+        address _tokenIn,
+        address _tokenOut
+    ) internal virtual override returns (uint256 _amountOut) {
+        pool.exchange_underlying(
+            tokenIndex[_tokenIn],
+            tokenIndex[_tokenOut],
+            _amountIn,
+            0
+        );
+        // Imagine not returning amount of swapped tokens
+        _amountOut = IERC20(_tokenOut).balanceOf(address(this));
+    }
+
+    function _query(
+        uint256 _amountIn,
+        address _tokenIn,
+        address _tokenOut
+    ) internal view virtual override returns (uint256 _amountOut) {
+        // -1 to account for rounding errors.
+        // This will underquote by 1 wei sometimes, but that's life
+        _amountOut =
+            pool.get_dy_underlying(
+                tokenIndex[_tokenIn],
+                tokenIndex[_tokenOut],
+                _amountIn
+            ) -
+            1;
+
+        // quote for swaps from [base pool token] to [meta pool token] is
+        // sometimes overly optimistic. Subtracting 4 bp should give
+        // a more accurate lower bound for actual amount of tokens swapped
+        if (tokenIndex[_tokenIn] < numberStablecoins) {
+            _amountOut = (_amountOut * 9996) / 10000;
+        }
+    }
+}
