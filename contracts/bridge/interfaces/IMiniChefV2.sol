b5395e5113e9b1cc79e28047e86cd732a16f1f3e
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-02-22 02:14:16 +0300

    BonusChef should check if MiniChef is set up

--- contracts/bridge/interfaces/IMiniChefV2.sol
+++ contracts/bridge/interfaces/IMiniChefV2.sol
@@ -3,6 +3,7 @@ pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
 import {IERC20} from "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
+import {IRewarder} from "./IRewarder.sol";
 
 interface IMiniChefV2 {
     struct UserInfo {
@@ -19,6 +20,7 @@ interface IMiniChefV2 {
     function lpToken(uint) external view returns(IERC20);
     function poolInfo(uint) external view returns (IMiniChefV2.PoolInfo memory);
     function poolLength() external view returns (uint256);
+    function rewarder(uint) external view returns (IRewarder);
     function updatePool(uint256 pid) external returns (IMiniChefV2.PoolInfo memory);
     function userInfo(uint256 _pid, address _user) external view returns (uint256, uint256);
     function deposit(uint256 pid, uint256 amount, address to) external;
