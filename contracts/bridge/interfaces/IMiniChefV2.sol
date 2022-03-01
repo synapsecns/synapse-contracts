86a96fd522e8a1d49ae5bc78cff6d9c63462cec7
Bleaker <maozdror@protonmail.com>
2022-03-01 16:49:55 -0500

    Make MiniChefV21 implement IMiniChefV2

--- contracts/bridge/interfaces/IMiniChefV2.sol
+++ contracts/bridge/interfaces/IMiniChefV2.sol
@@ -8,7 +8,7 @@ import {IRewarder} from "./IRewarder.sol";
 interface IMiniChefV2 {
     struct UserInfo {
         uint256 amount;
-        uint256 rewardDebt;
+        int256 rewardDebt;
     }
 
     struct PoolInfo {
@@ -17,25 +17,24 @@ interface IMiniChefV2 {
         uint64 allocPoint;
     }
 
-    function lpToken(uint256) external view returns (IERC20);
-
     function poolInfo(uint256)
         external
         view
-        returns (IMiniChefV2.PoolInfo memory);
+        returns (uint128, uint64, uint64);
 
+    function updatePool(uint256 pid)
+        external
+        returns (PoolInfo memory);
+
+    function lpToken(uint256) external view returns (IERC20);
     function poolLength() external view returns (uint256);
 
     function rewarder(uint256) external view returns (IRewarder);
 
-    function updatePool(uint256 pid)
-        external
-        returns (IMiniChefV2.PoolInfo memory);
-
     function userInfo(uint256 _pid, address _user)
         external
         view
-        returns (uint256, uint256);
+        returns (uint256, int256);
 
     function deposit(
         uint256 pid,
