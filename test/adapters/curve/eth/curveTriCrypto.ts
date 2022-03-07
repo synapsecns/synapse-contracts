53122fbd444349dd228dcb13ea14f63c0c559645
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-03-07 17:58:12 +0300

    Curve ETH TriCrypto test

--- test/adapters/curve/eth/curveTriCrypto.ts
+++ test/adapters/curve/eth/curveTriCrypto.ts
@@ -1,26 +1,25 @@
 //@ts-nocheck
 import { Signer } from "ethers"
-import { MAX_UINT256, getUserTokenBalance } from "../../../utils"
+import { getUserTokenBalance } from "../../../utils"
 import { solidity } from "ethereum-waffle"
-import { deployments } from "hardhat"
 
-import { TestAdapterSwap } from "../../../../build/typechain/TestAdapterSwap"
-import { IAdapter } from "../../../../build/typechain/IAdapter"
 import chai from "chai"
 import { getBigNumber } from "../../../bridge/utilities"
 import {
-  deployAdapter,
-  setupTokens,
   testRunAdapter,
-  range, forkChain, prepareAdapterFactories, setupAdapterTests, getAmounts,
+  range,
+  forkChain,
+  prepareAdapterFactories,
+  setupAdapterTests,
+  getAmounts,
+  getSwapsAmount,
 } from "../../utils/helpers"
 
 import config from "../../../config.json"
 import adapters from "../adapters.json"
-import {CurveBasePoolAdapter} from "../../../../build/typechain";
+import { CurveBasePoolAdapter } from "../../../../build/typechain"
 
 chai.use(solidity)
-
 
 const { expect } = chai
 const CHAIN = 1
@@ -32,8 +31,6 @@ const ADAPTER = adapters[CHAIN][POOL]
 const ADAPTER_NAME = String(ADAPTER.params[0])
 
 describe.only(ADAPTER_NAME, async () => {
-  let signers: Array<Signer>
-
   let owner: Signer
   let ownerAddress: string
   let dude: Signer
@@ -41,11 +38,10 @@ describe.only(ADAPTER_NAME, async () => {
 
   let adapter: CurveBasePoolAdapter
 
-  let testAdapterSwap: TestAdapterSwap
-
   const TOKENS_DECIMALS = []
   const TOKENS = []
-  // const TOKENS_DECIMALS: Array<Number> = [6, 4, 14]
+
+  const tokenSymbols: Array<string> = ["USDT", "WBTC", "WETH"]
   const poolTokenSymbols: Array<String> = ["USDT", "WBTC", "WETH"]
 
   const ALL_TOKENS: Array<Number> = range(poolTokenSymbols.length)
@@ -53,46 +49,54 @@ describe.only(ADAPTER_NAME, async () => {
   const SHARE_SMALL: Array<Number> = [1, 12, 29, 42]
   const SHARE_BIG: Array<Number> = [66, 121]
 
+  let swapsPerTime: Number =
+    SHARE_SMALL.length * getSwapsAmount(tokenSymbols.length)
+  const timesSmall: Number = Math.floor(125 / swapsPerTime) + 1
+  const swapsAmountSmall: Number = timesSmall * swapsPerTime
+
+  swapsPerTime = SHARE_BIG.length * getSwapsAmount(tokenSymbols.length)
+  const timesBig: Number = Math.floor(50 / swapsPerTime) + 1
+  const swapsAmountBig: Number = timesBig * swapsPerTime
+
   const AMOUNTS: Array<Number> = []
-  const AMOUNTS_BIG: Array<Number> =  []
+  const AMOUNTS_BIG: Array<Number> = []
 
   const CHECK_UNDERQUOTING = true
   const MAX_UNDERQUOTE = 1
   const MINT_AMOUNT = getBigNumber("1000000000000000000")
-
 
   before(async function () {
     await forkChain(process.env.ALCHEMY_API, 14000000)
     await prepareAdapterFactories(this, ADAPTER)
   })
 
-  beforeEach(async function ()  {
+  beforeEach(async function () {
     await setupAdapterTests(
-        this,
-        config[CHAIN],
-        ADAPTER,
-        poolTokenSymbols,
-        MAX_UNDERQUOTE,
-        MINT_AMOUNT
+      this,
+      config[CHAIN],
+      ADAPTER,
+      tokenSymbols,
+      MAX_UNDERQUOTE,
+      MINT_AMOUNT,
     )
 
     for (let token of this.tokens) {
       expect(await getUserTokenBalance(this.ownerAddress, token)).to.eq(
-          MINT_AMOUNT,
+        MINT_AMOUNT,
       )
     }
 
     AMOUNTS = await getAmounts(
-        config[CHAIN],
-        config[CHAIN][DEX][STORAGE],
-        poolTokenSymbols,
-        SHARE_SMALL,
+      config[CHAIN],
+      config[CHAIN][DEX][STORAGE],
+      poolTokenSymbols,
+      SHARE_SMALL,
     )
     AMOUNTS_BIG = await getAmounts(
-        config[CHAIN],
-        config[CHAIN][DEX][STORAGE],
-        poolTokenSymbols,
-        SHARE_BIG,
+      config[CHAIN],
+      config[CHAIN][DEX][STORAGE],
+      poolTokenSymbols,
+      SHARE_BIG,
     )
 
     adapter = this.adapter
@@ -177,12 +181,12 @@ describe.only(ADAPTER_NAME, async () => {
         TOKENS[1].address,
       )
 
-      // .add(1) to reflect underquoting by 1
+      // .add(MAX_UNDERQUOTE) to reflect underquoting
       await expect(() =>
         adapter
           .connect(dude)
           .swap(extra, TOKENS[0].address, TOKENS[1].address, dudeAddress),
-      ).to.changeTokenBalance(TOKENS[1], dude, swapQuote.add(1))
+      ).to.changeTokenBalance(TOKENS[1], dude, swapQuote.add(MAX_UNDERQUOTE))
     })
 
     it("Only Owner can rescue GAS from Adapter", async () => {
@@ -205,27 +209,33 @@ describe.only(ADAPTER_NAME, async () => {
     })
   })
 
-  describe("Adapter Swaps", () => {
-    it("Swaps between tokens [120 small-medium swaps]", async function() {
-      await testRunAdapter(
+  describe("Adapter Swaps", function () {
+    it(
+      "Swaps between tokens [" + swapsAmountSmall + " small-medium swaps]",
+      async function () {
+        await testRunAdapter(
           this,
           ALL_TOKENS,
           ALL_TOKENS,
-          5,
+          timesSmall,
           AMOUNTS,
           CHECK_UNDERQUOTING,
-      )
-    })
+        )
+      },
+    )
 
-    it("Swaps between tokens [90 big-ass swaps]", async function() {
-      await testRunAdapter(
+    it(
+      "Swaps between tokens [" + swapsAmountBig + " big-ass swaps]",
+      async function () {
+        await testRunAdapter(
           this,
           ALL_TOKENS,
           ALL_TOKENS,
-          5,
+          timesBig,
           AMOUNTS_BIG,
           CHECK_UNDERQUOTING,
-      )
-    })
+        )
+      },
+    )
   })
 })
