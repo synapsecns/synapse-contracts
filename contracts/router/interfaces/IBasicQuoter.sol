7f2a964c73aa06f2403d749af1d538f8513a4000
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-03-09 17:51:48 +0300

    Basic Quoter

---
+++ contracts/router/interfaces/IBasicQuoter.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IBasicQuoter {
+    event UpdatedTrustedTokens(address[] _newTrustedTokens);
+
+    event AddedTrustedToken(address _newTrustedTokens);
+
+    event RemovedToken(address _removedTokens);
+
+    struct Query {
+        address adapter;
+        address tokenIn;
+        address tokenOut;
+        uint256 amountOut;
+    }
+
+    struct OfferWithGas {
+        bytes amounts;
+        bytes adapters;
+        bytes path;
+        uint256 gasEstimate;
+    }
+
+    struct FormattedOfferWithGas {
+        uint256[] amounts;
+        address[] adapters;
+        address[] path;
+        uint256 gasEstimate;
+    }
+
+    struct Trade {
+        uint256 amountIn;
+        uint256 amountOut;
+        address[] path;
+        address[] adapters;
+    }
+
+    function getTrustedToken(uint256 _index) external view returns (address);
+
+    function trustedTokensCount() external view returns (uint256);
+
+    function addTrustedToken(address _token) external;
+
+    function setTokens(address[] memory _tokens) external;
+
+    function removeToken(address _token) external;
+
+    function removeTokenByIndex(uint256 _index) external;
+}
