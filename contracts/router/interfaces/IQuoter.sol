abd6cea9018ed31e36bf919054bd8fee987f533d
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-03-12 14:03:06 +0300

    add missing functions to interface

--- contracts/router/interfaces/IQuoter.sol
+++ contracts/router/interfaces/IQuoter.sol
@@ -4,11 +4,19 @@ pragma solidity ^0.8.0;
 import {IBasicQuoter} from "./IBasicQuoter.sol";
 
 interface IQuoter is IBasicQuoter {
+
+    // solhint-disable-next-line
+    function WGAS() external view returns (address payable);
+
+    // -- DIRECT SWAP QUERIES --
+
     function queryDirectSwap(
         uint256 _amountIn,
         address _tokenIn,
         address _tokenOut
     ) external view returns (Query memory);
+
+    // -- FIND BEST PATH --
 
     function findBestPathWithGas(
         uint256 _amountIn,
@@ -17,4 +25,24 @@ interface IQuoter is IBasicQuoter {
         uint8 _maxSwaps,
         uint256 _gasPrice
     ) external view returns (FormattedOfferWithGas memory);
+
+    function getBridgeDataAmountOut(
+        bytes4 _selector,
+        address _to,
+        address _bridgeToken,
+        uint256 _amountIn,
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _gasPrice,
+        uint256 _maxSwapSlippage
+    ) external view returns (bytes memory, uint256);
+
+    function getTradeDataAmountOut(
+        uint256 _amountIn,
+        address _tokenIn,
+        address _tokenOut,
+        uint8 _maxSwaps,
+        uint256 _gasPrice,
+        uint256 _maxSwapSlippage
+    ) external view returns (Trade memory, uint256);
 }
