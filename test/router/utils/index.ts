a3e8c857997bad4bb746f0c5d096f6cb67fe50e0
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-03-15 23:03:30 +0300

    very tests much complex swaps

--- test/router/utils/index.ts
+++ test/router/utils/index.ts
@@ -1,5 +1,14 @@
 import { ethers } from "hardhat"
-import { BigNumber } from "ethers"
+import { BigNumber, ContractFactory } from "ethers"
+import { Context } from "mocha"
+import {
+  IERC20,
+  Swap,
+  UniswapV2Factory,
+  SynapseBasePoolAdapter,
+  IUniswapV2Pair,
+} from "../../../build/typechain"
+import { MAX_UINT256 } from "../../utils"
 
 export const BASE_TEN = 10
 
@@ -11,6 +20,9 @@ export async function prepare(thisObject, contracts) {
   thisObject.signers = await ethers.getSigners()
   thisObject.owner = thisObject.signers[0]
   thisObject.dude = thisObject.signers[1]
+
+  thisObject.ownerAddress = await thisObject.signers[0].getAddress()
+  thisObject.dudeAddress = await thisObject.signers[1].getAddress()
 }
 
 export async function deploy(thisObject, contracts) {
@@ -23,4 +35,91 @@ export async function deploy(thisObject, contracts) {
 
 export function getBigNumber(amount, decimals = 18) {
   return BigNumber.from(amount).mul(BigNumber.from(BASE_TEN).pow(decimals))
+}
+
+export async function setupSynapsePool(
+  thisObject: Context,
+  swapFactory: ContractFactory,
+  adapterFactory: ContractFactory,
+  lpTokenAddress: string,
+  pool: string,
+  adapter: string,
+  tokens: Array<IERC20>,
+  decimals: Array<number>,
+  a: number,
+  fee: number,
+  amount: number,
+) {
+  let swap = (await swapFactory.deploy()) as Swap
+  await swap.initialize(
+    tokens.map(function (token) {
+      return token.address
+    }),
+    decimals,
+    "LP Token",
+    "LP",
+    a,
+    fee,
+    6 * 10 ** 9,
+    lpTokenAddress,
+  )
+  thisObject[pool] = swap
+
+  let basePoolAdapter = (await adapterFactory.deploy(
+    "BasePoolAdapter",
+    swap.address,
+    160000,
+  )) as SynapseBasePoolAdapter
+  thisObject[adapter] = basePoolAdapter
+
+  let amounts = []
+
+  for (let index in tokens) {
+    await tokens[index].approve(swap.address, MAX_UINT256)
+    amounts.push(getBigNumber(amount, decimals[index]))
+  }
+
+  await swap.addLiquidity(amounts, 0, MAX_UINT256)
+}
+
+export async function setupUniswapPool(
+  thisObject: Context,
+  uniswapFactory: UniswapV2Factory,
+  tokenA: IERC20,
+  amountA: number,
+  decimalsA: number,
+  tokenB: IERC20,
+  amountB: number,
+  decimalsB: number,
+) {
+  await uniswapFactory.createPair(tokenA.address, tokenB.address)
+  let pairAddress = await uniswapFactory.getPair(tokenA.address, tokenB.address)
+
+  // provide liquidity
+  await tokenA.transfer(pairAddress, getBigNumber(amountA, decimalsA))
+  await tokenB.transfer(pairAddress, getBigNumber(amountB, decimalsB))
+  let pair = (await ethers.getContractAt(
+    "contracts/router/helper/uniswap/interfaces/IUniswapV2Pair.sol:IUniswapV2Pair",
+    pairAddress,
+  )) as IUniswapV2Pair
+  await pair.mint(thisObject.ownerAddress)
+}
+
+export async function setupUniswapAdapters(
+  thisObject: Context,
+  uniswapAdapterFactory: ContractFactory,
+  factories: Array<string>,
+  adapters: Array<string>,
+  swapGasEstimate: number = 100000,
+  fee: number = 30,
+) {
+  for (let index in factories) {
+    let factoryName = factories[index]
+    thisObject[adapters[index]] = await uniswapAdapterFactory.deploy(
+      factoryName,
+      thisObject[factoryName].address,
+      swapGasEstimate,
+      fee,
+    )
+  }
 }
