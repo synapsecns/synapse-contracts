bb36187af0f28c29c2b71542d269c09eebd05a4a
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-03-18 13:49:58 +0300

    add factory for BonusChef + adjust deployment

--- deploy/010_deploy_BonusChef.ts
+++ deploy/010_deploy_BonusChef.ts
@@ -1,103 +1,56 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { DeployFunction } from 'hardhat-deploy/types'
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
 import { CHAIN_ID } from "../utils/network"
-import {includes} from "lodash";
+import { includes } from "lodash"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId } = hre
   const { deploy, get, execute } = deployments
   const { deployer } = await getNamedAccounts()
 
-  if ((includes([CHAIN_ID.BOBA], await getChainId()))) {
-    const deployResultZero = await deploy('BonusChef0', {
-        contract: "BonusChef",
-        from: deployer,
-        log: true,
-        skipIfAlreadyDeployed: true,
-        args: [
-        (await get('MiniChefV21')).address,
-        0,
-        (await get('DevMultisig')).address,
-        ],
-    })
-
-
-    if (deployResultZero.newlyDeployed) {
-      await execute(
-        "BonusChef0",
-        { from: deployer, log: true },
-        "grantRole",
-        "0x0000000000000000000000000000000000000000000000000000000000000000",
-        (
-          await get("DevMultisig")
-        ).address,
-      )
-
-      await execute(
-        "BonusChef0",
-        { from: deployer, log: true },
-        "grantRole",
-        "0x0000000000000000000000000000000000000000000000000000000000000000",
-        (
-          await get("DevMultisig")
-        ).address,
-      )
-
-      await execute(
-        "BonusChef0",
-        { from: deployer, log: true },
-        "renounceRole",
-        "0x0000000000000000000000000000000000000000000000000000000000000000",
-        deployer,
-      )
-    }
-
-    const deployResultOne = await deploy('BonusChef1', {
-      contract: "BonusChef",
+  if (includes([CHAIN_ID.BOBA], await getChainId())) {
+    const deployResultFactory = await deploy("BonusChefFactory", {
       from: deployer,
       log: true,
       skipIfAlreadyDeployed: true,
-      args: [
-      (await get('MiniChefV21')).address,
-      0,
-      (await get('DevMultisig')).address,
-      ],
-  })
+    })
 
+    if (deployResultFactory.newlyDeployed) {
+      // args are [MiniChef, poolId, rewardsDistribution, governance]
+      await execute(
+        "BonusChefFactory",
+        { from: deployer, log: true },
+        "deploy",
+        (
+          await get("MiniChefV21")
+        ).address,
+        0,
+        (
+          await get("DevMultisig")
+        ).address,
+        (
+          await get("DevMultisig")
+        ).address,
+      )
 
-  if (deployResultOne.newlyDeployed) {
-    await execute(
-      "BonusChef1",
-      { from: deployer, log: true },
-      "grantRole",
-      "0x0000000000000000000000000000000000000000000000000000000000000000",
-      (
-        await get("DevMultisig")
-      ).address,
-    )
-
-    await execute(
-      "BonusChef0",
-      { from: deployer, log: true },
-      "grantRole",
-      "0x0000000000000000000000000000000000000000000000000000000000000000",
-      (
-        await get("DevMultisig")
-      ).address,
-    )
-
-    await execute(
-      "BonusChef0",
-      { from: deployer, log: true },
-      "renounceRole",
-      "0x0000000000000000000000000000000000000000000000000000000000000000",
-      deployer,
-    )
-  }
-
-
+      await execute(
+        "BonusChefFactory",
+        { from: deployer, log: true },
+        "deploy",
+        (
+          await get("MiniChefV21")
+        ).address,
+        1,
+        (
+          await get("DevMultisig")
+        ).address,
+        (
+          await get("DevMultisig")
+        ).address,
+      )
     }
+  }
 }
 
 export default func
-func.tags = ['MiniChefV21']
+func.tags = ["MiniChefV21"]
