415986fc8d56531a977f94bd01c0a95cca918abe
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-03-23 23:05:23 +0300

    Synapse adapters rework + speedup

--- contracts/router/adapters/synapse/SynapseAaveAdapter.sol
+++ contracts/router/adapters/synapse/SynapseAaveAdapter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import {ILendingPool} from "./interfaces/ILendingPool.sol";
 import {ISynapse} from "./interfaces/ISynapse.sol";
-import {SynapseBasePoolAdapter} from "./SynapseBasePoolAdapter.sol";
+import {SynapseBaseAdapter} from "./SynapseBaseAdapter.sol";
 
 import {IERC20} from "@synapseprotocol/sol-lib/contracts/solc8/erc20/IERC20.sol";
 
@@ -11,7 +11,7 @@ interface AaveToken is IERC20 {
     function scaledBalanceOf(address user) external view returns (uint256);
 }
 
-contract SynapseAavePoolAdapter is SynapseBasePoolAdapter {
+contract SynapseAaveAdapter is SynapseBaseAdapter {
     ILendingPool public immutable lendingPool;
 
     mapping(address => address) public aaveToken;
@@ -22,41 +22,45 @@ contract SynapseAavePoolAdapter is SynapseBasePoolAdapter {
 
     /**
      * @param _name Adapter name
-     * @param _pool Pool address
      * @param _swapGasEstimate Estimated gas usage for this.swap()
+     * @param _pool Pool address
      * @param _lendingPool Aave lending pool address
      * @param _underlyingTokens underlying pool tokens, that will be traded
      */
     constructor(
         string memory _name,
-        address _pool,
         uint256 _swapGasEstimate,
+        address _pool,
         address _lendingPool,
         address[] memory _underlyingTokens
-    ) SynapseBasePoolAdapter(_name, _pool, _swapGasEstimate) {
+    ) SynapseBaseAdapter(_name, _swapGasEstimate, _pool) {
         require(_underlyingTokens.length == numTokens, "Wrong tokens amount");
         lendingPool = ILendingPool(_lendingPool);
         for (uint8 i = 0; i < _underlyingTokens.length; i++) {
             address _poolToken = address(poolTokens[i]);
             if (_poolToken != _underlyingTokens[i]) {
-                aaveToken[_underlyingTokens[i]] = _poolToken;
-                isUnderlying[_underlyingTokens[i]] = true;
+                address _underlying = _underlyingTokens[i];
+                aaveToken[_underlying] = _poolToken;
+                isUnderlying[_underlying] = true;
+
+                _checkAllowance(
+                    IERC20(_underlying),
+                    UINT_MAX,
+                    address(_lendingPool)
+                );
             }
         }
     }
 
-    function _approveIfNeeded(address _tokenIn, uint256 _amount)
-        internal
-        virtual
-        override
-    {
-        if (isUnderlying[_tokenIn]) {
-            // Lending Pool needs to have approval to spend underlying
-            _checkAllowance(IERC20(_tokenIn), _amount, address(lendingPool));
-            // Swap pool needs to have approval to spend aToken
-            _tokenIn = aaveToken[_tokenIn];
-        }
-        SynapseBasePoolAdapter._approveIfNeeded(_tokenIn, _amount);
+    function _approveIfNeeded(address, uint256) internal virtual override {
+        this;
+        // if (isUnderlying[_tokenIn]) {
+        //     // Lending Pool needs to have approval to spend underlying
+        //     _checkAllowance(IERC20(_tokenIn), _amount, address(lendingPool));
+        //     // Swap pool needs to have approval to spend aToken
+        //     _tokenIn = aaveToken[_tokenIn];
+        // }
+        // SynapseBaseAdapter._approveIfNeeded(_tokenIn, _amount);
     }
 
     function _swap(
@@ -73,7 +77,7 @@ contract SynapseAavePoolAdapter is SynapseBasePoolAdapter {
         }
         if (isUnderlying[_tokenOut]) {
             // User needs to receive underlying token, so we ask the aToken to be sent to this contract
-            SynapseBasePoolAdapter._swap(
+            SynapseBaseAdapter._swap(
                 _amountIn,
                 _tokenIn,
                 aaveToken[_tokenOut],
@@ -83,7 +87,7 @@ contract SynapseAavePoolAdapter is SynapseBasePoolAdapter {
             _amountOut = _aaveWithdraw(_tokenOut, UINT_MAX, _to);
         } else {
             // User needs to receive pool token, so we can use parent's logic
-            _amountOut = SynapseBasePoolAdapter._swap(
+            _amountOut = SynapseBaseAdapter._swap(
                 _amountIn,
                 _tokenIn,
                 _tokenOut,
@@ -109,7 +113,7 @@ contract SynapseAavePoolAdapter is SynapseBasePoolAdapter {
             uint256 _index = lendingPool.getReserveNormalizedIncome(_tokenOut);
             // replace _tokenOut with actual pool token
             _tokenOut = aaveToken[_tokenOut];
-            _amountOut = SynapseBasePoolAdapter._query(
+            _amountOut = SynapseBaseAdapter._query(
                 _amountIn,
                 _tokenIn,
                 _tokenOut
@@ -117,7 +121,7 @@ contract SynapseAavePoolAdapter is SynapseBasePoolAdapter {
 
             _amountOut = _calcTransferredOut(_amountOut, _index);
         } else {
-            _amountOut = SynapseBasePoolAdapter._query(
+            _amountOut = SynapseBaseAdapter._query(
                 _amountIn,
                 _tokenIn,
                 _tokenOut
@@ -146,7 +150,7 @@ contract SynapseAavePoolAdapter is SynapseBasePoolAdapter {
 
         uint256 _delta = (_amount * RAY + _index / 2) / _index;
         uint256 _oldScaledBalance = AaveToken(aaveToken[_token])
-        .scaledBalanceOf(address(pool));
+            .scaledBalanceOf(address(pool));
 
         uint256 _newScaledBalance = _oldScaledBalance + _delta;
 
---
+++ contracts/router/adapters/synapse/SynapseBaseAdapter.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ISynapse} from "./interfaces/ISynapse.sol";
+import {Adapter} from "../../Adapter.sol";
+import {SwapCalculator} from "../../helper/SwapCalculator.sol";
+
+import {IERC20} from "@synapseprotocol/sol-lib/contracts/solc8/erc20/IERC20.sol";
+
+//solhint-disable not-rely-on-time
+
+contract SynapseBaseAdapter is SwapCalculator, Adapter {
+    mapping(address => bool) public isPoolToken;
+    mapping(address => uint256) public tokenIndex;
+
+    constructor(
+        string memory _name,
+        uint256 _swapGasEstimate,
+        address _pool
+    ) SwapCalculator(ISynapse(_pool)) Adapter(_name, _swapGasEstimate) {
+        this;
+    }
+
+    function _addPoolToken(IERC20 token, uint256 index)
+        internal
+        virtual
+        override
+    {
+        SwapCalculator._addPoolToken(token, index);
+        isPoolToken[address(token)] = true;
+        tokenIndex[address(token)] = index;
+
+        _checkAllowance(token, UINT_MAX, address(pool));
+    }
+
+    function _approveIfNeeded(address, uint256) internal virtual override {
+        this;
+    }
+
+    function _depositAddress(address, address)
+        internal
+        view
+        override
+        returns (address)
+    {
+        return address(this);
+    }
+
+    function _swap(
+        uint256 _amountIn,
+        address _tokenIn,
+        address _tokenOut,
+        address _to
+    ) internal virtual override returns (uint256 _amountOut) {
+        _amountOut = pool.swap(
+            uint8(tokenIndex[_tokenIn]),
+            uint8(tokenIndex[_tokenOut]),
+            _amountIn,
+            0,
+            block.timestamp
+        );
+
+        _returnTo(_tokenOut, _amountOut, _to);
+    }
+
+    function _checkTokens(address _tokenIn, address _tokenOut)
+        internal
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        return isPoolToken[_tokenIn] && isPoolToken[_tokenOut];
+    }
+
+    function _query(
+        uint256 _amountIn,
+        address _tokenIn,
+        address _tokenOut
+    ) internal view virtual override returns (uint256 _amountOut) {
+        if (pool.paused()) {
+            return 0;
+        }
+        try
+            pool.calculateSwap(
+                uint8(tokenIndex[_tokenIn]),
+                uint8(tokenIndex[_tokenOut]),
+                _amountIn
+            )
+        returns (uint256 amountOut) {
+            _amountOut = amountOut;
+        } catch {
+            return 0;
+        }
+    }
+}
