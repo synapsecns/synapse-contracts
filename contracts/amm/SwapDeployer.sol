e2e24965f5be8b8e71f58d22178e2def8dccb60d
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-03-26 23:02:19 +0300

    add deterministic deployment

--- contracts/amm/SwapDeployer.sol
+++ contracts/amm/SwapDeployer.sol
@@ -25,8 +25,66 @@ contract SwapDeployer is Ownable {
         uint256 _fee,
         uint256 _adminFee,
         address lpTokenTargetAddress
-    ) external returns (address) {
-        address swapClone = Clones.clone(swapAddress);
+    ) external returns (address swapClone) {
+        swapClone = Clones.clone(swapAddress);
+        _initializeSwap(
+            swapClone,
+            _pooledTokens,
+            decimals,
+            lpTokenName,
+            lpTokenSymbol,
+            _a,
+            _fee,
+            _adminFee,
+            lpTokenTargetAddress
+        );
+    }
+
+    function deployDeterministic(
+        address swapAddress,
+        bytes32 salt,
+        IERC20[] memory _pooledTokens,
+        uint8[] memory decimals,
+        string memory lpTokenName,
+        string memory lpTokenSymbol,
+        uint256 _a,
+        uint256 _fee,
+        uint256 _adminFee,
+        address lpTokenTargetAddress
+    ) external returns (address swapClone) {
+        swapClone = Clones.cloneDeterministic(swapAddress, salt);
+        _initializeSwap(
+            swapClone,
+            _pooledTokens,
+            decimals,
+            lpTokenName,
+            lpTokenSymbol,
+            _a,
+            _fee,
+            _adminFee,
+            lpTokenTargetAddress
+        );
+    }
+
+    function predictDeterministicAddress(address swapAddress, bytes32 salt)
+        external
+        view
+        returns (address)
+    {
+        return Clones.predictDeterministicAddress(swapAddress, salt);
+    }
+
+    function _initializeSwap(
+        address swapClone,
+        IERC20[] memory _pooledTokens,
+        uint8[] memory decimals,
+        string memory lpTokenName,
+        string memory lpTokenSymbol,
+        uint256 _a,
+        uint256 _fee,
+        uint256 _adminFee,
+        address lpTokenTargetAddress
+    ) internal {
         ISwap(swapClone).initialize(
             _pooledTokens,
             decimals,
@@ -39,6 +97,5 @@ contract SwapDeployer is Ownable {
         );
         Ownable(swapClone).transferOwnership(owner());
         emit NewSwapPool(msg.sender, swapClone, _pooledTokens);
-        return swapClone;
     }
 }
