5061a29a99e11456f966f4a01c7a4cd6e9ff0f13
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-03-29 16:51:37 +0300

    lint AwaxJewelMigration

--- contracts/bridge/wrappers/AvaxJewelMigration.sol
+++ contracts/bridge/wrappers/AvaxJewelMigration.sol
@@ -2,11 +2,11 @@
 
 pragma solidity 0.6.12;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import '../interfaces/ISynapseBridge.sol';
+import "../interfaces/ISynapseBridge.sol";
 
 interface IERC20Mintable is IERC20 {
     function mint(address to, uint256 amount) external;
@@ -16,30 +16,36 @@ contract AvaxJewelMigration is Ownable {
     using SafeERC20 for IERC20;
     using SafeERC20 for IERC20Mintable;
 
-
-    ISynapseBridge constant synapseBridge = ISynapseBridge(0xC05e61d0E7a63D27546389B7aD62FdFf5A91aACE);
-    // MULTICHAIN JEWEL 
-    IERC20 constant legacyToken = IERC20(0x4f60a160D8C2DDdaAfe16FCC57566dB84D674BD6);
+    ISynapseBridge public constant SYNAPSE_BRIDGE =
+        ISynapseBridge(0xC05e61d0E7a63D27546389B7aD62FdFf5A91aACE);
+    // MULTICHAIN JEWEL
+    IERC20 public constant LEGACY_TOKEN =
+        IERC20(0x4f60a160D8C2DDdaAfe16FCC57566dB84D674BD6);
     // SYNAPSE JEWEL
-    IERC20 constant newToken = IERC20(0x997Ddaa07d716995DE90577C123Db411584E5E46);
-    uint256 constant MAX_UINT256 = 2**256 - 1;
+    IERC20 public constant NEW_TOKEN =
+        IERC20(0x997Ddaa07d716995DE90577C123Db411584E5E46);
+    uint256 private constant MAX_UINT256 = 2**256 - 1;
 
     constructor() public {
-        newToken.safeApprove(address(synapseBridge), MAX_UINT256);
+        NEW_TOKEN.safeApprove(address(SYNAPSE_BRIDGE), MAX_UINT256);
     }
 
     function migrate(uint256 amount) public {
-        legacyToken.safeTransferFrom(msg.sender, address(this), amount);
-        IERC20Mintable(address(newToken)).mint(msg.sender, amount);
+        LEGACY_TOKEN.safeTransferFrom(msg.sender, address(this), amount);
+        IERC20Mintable(address(NEW_TOKEN)).mint(msg.sender, amount);
     }
 
-    function migrateAndBridge(uint256 amount, address to, uint256 chainId) external {
+    function migrateAndBridge(
+        uint256 amount,
+        address to,
+        uint256 chainId
+    ) external {
         migrate(amount);
-        synapseBridge.redeem(to, chainId, newToken, amount);
+        SYNAPSE_BRIDGE.redeem(to, chainId, NEW_TOKEN, amount);
     }
 
     function redeemLegacy() external onlyOwner {
-        uint256 legacyBalance = legacyToken.balanceOf(address(this));
-        legacyToken.safeTransfer(owner(), legacyBalance);
+        uint256 legacyBalance = LEGACY_TOKEN.balanceOf(address(this));
+        LEGACY_TOKEN.safeTransfer(owner(), legacyBalance);
     }
-}  
+}
