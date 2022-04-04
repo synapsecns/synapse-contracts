48676a54fb09afcd205ebbac4e2e4d9e4e234ef0
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-04 17:40:37 +0300

    Revert "prepare for merging into master"
    This reverts commit db3dae7b9b405dfd70fbfa9b4367c42f1e18e321.

---
+++ deprecated/deploys/amm/007_deploy_MetaSwap.ts
@@ -0,0 +1,22 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy, get } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  // await deploy("MetaSwap", {
+  //   from: deployer,
+  //   log: true,
+  //   contract: "MetaSwap",
+  //   skipIfAlreadyDeployed: true,
+  //   libraries: {
+  //     SwapUtils: (await get("SwapUtils")).address,
+  //     MetaSwapUtils: (await get("MetaSwapUtils")).address,
+  //     AmplificationUtils: (await get("AmplificationUtils")).address,
+  //   },
+  // })
+}
+
+export default func
+func.tags = ["MetaSwap"]
