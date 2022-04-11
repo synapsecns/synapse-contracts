3052e84c8a49c15abc429cf3eb130d7dfc521d03
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-11 16:01:03 +0300

    more power to Bridge Quoter

--- contracts/router/interfaces/IBridgeQuoter.sol
+++ contracts/router/interfaces/IBridgeQuoter.sol
@@ -5,18 +5,60 @@ import {IQuoter} from "./IQuoter.sol";
 import {Offers} from "../libraries/LibOffers.sol";
 
 interface IBridgeQuoter is IQuoter {
-    function findBestPathInitialChain(
+    // -- BEST PATH: initial chain --
+
+    function bestPathToBridge(
         address tokenIn,
         uint256 amountIn,
-        address tokenOut
+        address bridgeToken
     ) external view returns (Offers.FormattedOffer memory bestOffer);
 
-    function findBestPathDestinationChain(
+    function bestPathToBridgeEVM(
         address tokenIn,
+        uint256 amountIn,
+        uint256 chainId,
+        address bridgeTokenGlobal
+    ) external view returns (Offers.FormattedOffer memory bestOffer);
+
+    function bestPathToBridgeNonEVM(
+        address tokenIn,
+        uint256 amountIn,
+        uint256 chainId,
+        string calldata bridgeTokenGlobal
+    ) external view returns (Offers.FormattedOffer memory bestOffer);
+
+    // -- BEST PATH: destination chain --
+
+    function bestPathFromBridge(
+        address bridgeToken,
         uint256 amountIn,
         address tokenOut,
         bool gasdropRequested
     ) external view returns (Offers.FormattedOffer memory bestOffer);
+
+    function bestPathFromBridgeEVM(
+        uint256 chainId,
+        address bridgeTokenGlobal,
+        uint256 amountIn,
+        address tokenOut,
+        bool gasdropRequested
+    ) external view returns (Offers.FormattedOffer memory bestOffer);
+
+    function bestPathFromBridgeNonEVM(
+        uint256 chainIdFrom,
+        string calldata bridgeTokenFrom,
+        uint256 amountIn
+    ) external view returns (Offers.FormattedOffer memory bestOffer);
+
+    function bestPathFromBridgeNonEVM(
+        uint256 chainIdFrom,
+        string calldata bridgeTokenFrom,
+        uint256 amountIn,
+        address tokenOut,
+        bool gasdropRequested
+    ) external view returns (Offers.FormattedOffer memory bestOffer);
+
+    // -- BRIDGE CONFIG VIEWS --
 
     function getAllBridgeTokensEVM(uint256 chainTo)
         external
