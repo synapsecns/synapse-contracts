4cac26a98a2efad1dbfa94415bcfabcc79f65f73
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-16 12:51:36 +0300

    Uniswap adapter lint

--- contracts/router/adapters/uniswap/UniswapV2Adapter.sol
+++ contracts/router/adapters/uniswap/UniswapV2Adapter.sol
@@ -24,7 +24,7 @@ contract UniswapV2Adapter is Adapter {
     constructor(
         string memory _name,
         uint256 _swapGasEstimate,
-        address _uniswapV2FactoryAddress,
+        address _uniswapV2Factory,
         bytes32 _initCodeHash,
         uint256 _fee
     ) Adapter(_name, _swapGasEstimate) {
@@ -33,19 +33,19 @@ contract UniswapV2Adapter is Adapter {
             "Fee is too high. Must be less than multiplier"
         );
         MULTIPLIER_WITH_FEE = uint128(MULTIPLIER - _fee);
-        uniswapV2Factory = _uniswapV2FactoryAddress;
+        uniswapV2Factory = _uniswapV2Factory;
         initCodeHash = _initCodeHash;
     }
 
-    function _depositAddress(address _tokenIn, address _tokenOut)
+    function _depositAddress(address tokenIn, address tokenOut)
         internal
         view
         override
         returns (address pair)
     {
-        bytes32 salt = _tokenIn < _tokenOut
-            ? keccak256(abi.encodePacked(_tokenIn, _tokenOut))
-            : keccak256(abi.encodePacked(_tokenOut, _tokenIn));
+        bytes32 salt = tokenIn < tokenOut
+            ? keccak256(abi.encodePacked(tokenIn, tokenOut))
+            : keccak256(abi.encodePacked(tokenOut, tokenIn));
         pair = address(
             uint160(
                 uint256(
@@ -63,57 +63,49 @@ contract UniswapV2Adapter is Adapter {
     }
 
     function _swap(
-        uint256 _amountIn,
-        address _tokenIn,
-        address _tokenOut,
-        address _to
-    ) internal virtual override returns (uint256 _amountOut) {
-        address _pair = _depositAddress(_tokenIn, _tokenOut);
+        uint256 amountIn,
+        address tokenIn,
+        address tokenOut,
+        address to
+    ) internal virtual override returns (uint256 amountOut) {
+        address pair = _depositAddress(tokenIn, tokenOut);
 
-        _amountOut = _getPairAmountOut(_pair, _tokenIn, _tokenOut, _amountIn);
-        require(_amountOut > 0, "Adapter: Insufficient output amount");
+        amountOut = _getPairAmountOut(pair, tokenIn, tokenOut, amountIn);
+        require(amountOut > 0, "Adapter: Insufficient output amount");
 
-        if (_tokenIn < _tokenOut) {
-            IUniswapV2Pair(_pair).swap(0, _amountOut, _to, new bytes(0));
+        if (tokenIn < tokenOut) {
+            IUniswapV2Pair(pair).swap(0, amountOut, to, new bytes(0));
         } else {
-            IUniswapV2Pair(_pair).swap(_amountOut, 0, _to, new bytes(0));
+            IUniswapV2Pair(pair).swap(amountOut, 0, to, new bytes(0));
         }
     }
 
     function _query(
-        uint256 _amountIn,
-        address _tokenIn,
-        address _tokenOut
-    ) internal view virtual override returns (uint256 _amountOut) {
-        address _pair = _depositAddress(_tokenIn, _tokenOut);
+        uint256 amountIn,
+        address tokenIn,
+        address tokenOut
+    ) internal view virtual override returns (uint256 amountOut) {
+        address pair = _depositAddress(tokenIn, tokenOut);
 
-        _amountOut = _getPairAmountOut(_pair, _tokenIn, _tokenOut, _amountIn);
+        amountOut = _getPairAmountOut(pair, tokenIn, tokenOut, amountIn);
     }
 
     function _getPairAmountOut(
-        address _pair,
-        address _tokenIn,
-        address _tokenOut,
-        uint256 _amountIn
-    ) internal view returns (uint256 _amountOut) {
-        if (Address.isContract(_pair)) {
-            try IUniswapV2Pair(_pair).getReserves() returns (
-                uint112 _reserve0,
-                uint112 _reserve1,
+        address pair,
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn
+    ) internal view returns (uint256 amountOut) {
+        if (Address.isContract(pair)) {
+            try IUniswapV2Pair(pair).getReserves() returns (
+                uint112 reserve0,
+                uint112 reserve1,
                 uint32
             ) {
-                if (_tokenIn < _tokenOut) {
-                    _amountOut = _calcAmountOut(
-                        _amountIn,
-                        _reserve0,
-                        _reserve1
-                    );
+                if (tokenIn < tokenOut) {
+                    amountOut = _calcAmountOut(amountIn, reserve0, reserve1);
                 } else {
-                    _amountOut = _calcAmountOut(
-                        _amountIn,
-                        _reserve1,
-                        _reserve0
-                    );
+                    amountOut = _calcAmountOut(amountIn, reserve1, reserve0);
                 }
             } catch {
                 this;
@@ -123,17 +115,17 @@ contract UniswapV2Adapter is Adapter {
 
     // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
     function _calcAmountOut(
-        uint256 _amountIn,
-        uint256 _reserveIn,
-        uint256 _reserveOut
-    ) internal view returns (uint256 _amountOut) {
-        if (_reserveIn == 0 || _reserveOut == 0) {
+        uint256 amountIn,
+        uint256 reserveIn,
+        uint256 reserveOut
+    ) internal view returns (uint256 amountOut) {
+        if (reserveIn == 0 || reserveOut == 0) {
             return 0;
         }
-        uint256 amountInWithFee = _amountIn * MULTIPLIER_WITH_FEE;
+        uint256 amountInWithFee = amountIn * MULTIPLIER_WITH_FEE;
 
-        _amountOut =
-            (amountInWithFee * _reserveOut) /
-            (_reserveIn * MULTIPLIER + amountInWithFee);
+        amountOut =
+            (amountInWithFee * reserveOut) /
+            (reserveIn * MULTIPLIER + amountInWithFee);
     }
 }
