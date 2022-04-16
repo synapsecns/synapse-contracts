c66060379874093a054ea9715779ff55f8981ce2
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-04-16 18:40:18 -0400

    work on gas fee pricing

---
+++ contracts/messaging/GasFeePricing.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts-4.5.0/access/Ownable.sol";
+
+contract GasFeePricing is Ownable {
+    // DstChainId => The estimated current gas price in wei of the destination chain
+    mapping(uint256 => uint256) public dstGasPrice;
+    // DstChainId => USD gas ratio of dstGasToken / srcGasToken
+    mapping(uint256 => uint256) public dstGasTokenRatio; 
+
+    /**
+     * @notice Permissioned method to allow an off-chain party to set what each dstChain's
+     * gas cost is priced in the srcChain's native gas currency. 
+     * Example: call on ETH, setCostPerChain(43114, 30000000000, 25180000000000000)
+     * chain ID 43114
+     * Average of 30 gwei cost to transaction on 43114
+     * AVAX/ETH = 0.02518, scaled to gas in wei = 25180000000000000
+     * @param _dstChainId The destination chain ID - typically, standard EVM chain ID, but differs on nonEVM chains
+     * @param _gasUnitPrice The estimated current gas price in wei of the destination chain
+     * @param _gasTokenPriceRatio USD gas ratio of dstGasToken / srcGasToken
+     */
+    function setCostPerChain(uint256 _dstChainId, uint256 _gasUnitPrice, uint256 _gasTokenPriceRatio) external onlyOwner {
+        dstGasPrice[_dstChainId] = _gasUnitPrice;
+        dstGasTokenRatio[_dstChainId] = _gasTokenPriceRatio;
+    }
+
+    /**
+     * @notice Returns srcGasToken fee to charge in wei for the cross-chain message based on the gas limit
+     * @param _options Versioned struct used to instruct relayer on how to proceed with gas limits. Contains data on gas limit to submit tx with.
+     */
+    function estimateGasFee(uint256 _dstChainId, bytes calldata _options) external view returns (uint256) {
+        // temporary gas limit set
+        uint256 gasLimit = 200000;
+        return (dstGasPrice[_dstChainId] * dstGasTokenRatio[_dstChainId] * gasLimit / 10**18);
+    }
+    
+}
---
+++ contracts/messaging/interfaces/IGasFeePricing.sol
@@ -0,0 +1,25 @@
+// SPDX -License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+interface IGasFeePricing {
+    
+    /**
+     * @notice Permissioned method to allow an off-chain party to set what each dstChain's
+     * gas cost is priced in the srcChain's native gas currency. 
+     * Example: call on ETH, setCostPerChain(43114, 30000000000, 25180000000000000)
+     * chain ID 43114
+     * Average of 30 gwei cost to transaction on 43114
+     * AVAX/ETH = 0.02518, scaled to gas in wei = 25180000000000000
+     * @param _dstChainId The destination chain ID - typically, standard EVM chain ID, but differs on nonEVM chains
+     * @param _gasUnitPrice The estimated current gas price in wei of the destination chain
+     * @param _gasTokenPriceRatio Gas ratio of dstGasToken / srcGasToken
+     */
+    function setCostPerChain(uint256 _dstChainId, uint256 _gasUnitPrice, uint256 _gasTokenPriceRatio) external;
+
+    /**
+     * @notice Returns srcGasToken fee to charge in wei for the cross-chain message based on the gas limit
+     * @param _options Versioned struct used to instruct relayer on how to proceed with gas limits. Contains data on gas limit to submit tx with.
+     */
+    function estimateGasFee(bytes calldata _options) external returns (uint256);
+}  
