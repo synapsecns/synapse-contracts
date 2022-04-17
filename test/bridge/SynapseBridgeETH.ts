9747599115577fde0f7449f81326b97d116b7be1
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-17 13:13:06 +0300

    fix rate limiter tests

--- test/bridge/SynapseBridgeETH.ts
+++ test/bridge/SynapseBridgeETH.ts
@@ -5,11 +5,11 @@ import { deployments, ethers } from "hardhat"
 
 import chai from "chai"
 import { GenericERC20, RateLimiter, SynapseBridge } from "../../build/typechain"
-import epochSeconds from "@stdlib/time-now"
 import { keccak256 } from "ethers/lib/utils"
 import { randomBytes } from "crypto"
-import { forkChain } from "../utils"
+import { forkChain, MAX_UINT256 } from "../utils"
 import { deployRateLimiter, setupForkedBridge } from "./utilities/bridge"
+import { getBigNumber } from "./utilities"
 
 chai.use(solidity)
 const { expect } = chai
@@ -34,8 +34,6 @@ describe("SynapseBridgeETH", async () => {
   const NUSD_POOL = "0x1116898DdA4015eD8dDefb84b6e8Bc24528Af2d8"
   const SYN = "0x0f2D719407FdBeFF09D87557AbB7232601FD9F29"
   const WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
-
-  const decimals = Math.pow(10, 6)
 
   // deploys the bridge, grants role. Rate limiter *must* be deployed first
   const setupTokens = async () => {
@@ -74,92 +72,136 @@ describe("SynapseBridgeETH", async () => {
     await setupTest()
   })
 
-  // ammounts are multiplied by 10^6
   const setupAllowanceTest = async (
     tokenAddress: string,
     allowanceAmount: number,
     intervalMin: number = 60,
   ) => {
     const lastReset = Math.floor((await getCurrentBlockTimestamp()) / 60)
-    allowanceAmount = allowanceAmount * decimals
 
-    await expect(
-      rateLimiter.setAllowance(
-        tokenAddress,
-        allowanceAmount,
-        intervalMin,
-        lastReset,
-      ),
-    ).to.be.not.reverted
+    await rateLimiter.setAllowance(
+      tokenAddress,
+      allowanceAmount,
+      intervalMin,
+      lastReset,
+    )
   }
 
-  it("Withdraw: should add to retry queue if rate limit hit", async () => {
-    const mintAmount = 50
+  it("Withdraw: should add to retry queue only if rate limit hit", async () => {
+    const mintAmount = getBigNumber(1000, 6)
+    const allowanceAmount = getBigNumber(100, 6)
+    const firstAmount = getBigNumber(42, 6)
+    const secondAmount = allowanceAmount.sub(firstAmount).add(1)
 
-    await expect(USDC.mint(bridge.address, mintAmount * decimals))
-    await setupAllowanceTest(USDC.address, 100)
+    await USDC.mint(bridge.address, mintAmount)
+    await setupAllowanceTest(USDC.address, allowanceAmount)
 
-    const kappa = keccak256(randomBytes(32))
+    let kappa = keccak256(randomBytes(32))
 
     await expect(
       bridge
         .connect(nodeGroup)
-        .withdraw(await recipient.getAddress(), USDC.address, 101, 50, kappa),
+        .withdraw(
+          await recipient.getAddress(),
+          USDC.address,
+          firstAmount,
+          0,
+          kappa,
+        ),
+    ).to.be.not.reverted
+    // This should NOT BE rate limited
+    expect(await bridge.kappaExists(kappa)).to.be.true
+
+    kappa = keccak256(randomBytes(32))
+    await expect(
+      bridge
+        .connect(nodeGroup)
+        .withdraw(
+          await recipient.getAddress(),
+          USDC.address,
+          secondAmount,
+          0,
+          kappa,
+        ),
     ).to.be.not.reverted
 
-    // make sure withdraw didn't happen
-    expect(await USDC.balanceOf(bridge.address)).to.be.eq(
-      (mintAmount * decimals).toString(),
-    )
+    // This should BE rate limited
+    expect(await bridge.kappaExists(kappa)).to.be.false
 
     // now retry. This should bypass the rate limiter
     await expect(rateLimiter.retryByKappa(kappa)).to.be.not.reverted
     expect(await bridge.kappaExists(kappa)).to.be.true
   })
 
-  it("WithdrawAndRemove: should add to retry queue if rate limit hit", async () => {
-    const allowanceAmount = 500
-    const withdrawAmount = 1000
+  it("WithdrawAndRemove: should add to retry queue only if rate limit hit", async () => {
+    const allowanceAmount = getBigNumber(100)
+    const firstAmount = getBigNumber(69)
+    const secondAmount = allowanceAmount.sub(firstAmount).add(1)
 
     await setupAllowanceTest(NUSD, allowanceAmount)
-    const kappa = keccak256(randomBytes(32))
+    let kappa = keccak256(randomBytes(32))
 
-    await expect(
-      bridge
-        .connect(nodeGroup)
-        .withdrawAndRemove(
-          await recipient.getAddress(),
-          NUSD,
-          withdrawAmount,
-          10,
-          NUSD_POOL,
-          1,
-          withdrawAmount,
-          epochSeconds(),
-          kappa,
-        ),
-    ).to.be.not.reverted
+    await bridge
+      .connect(nodeGroup)
+      .withdrawAndRemove(
+        await recipient.getAddress(),
+        NUSD,
+        firstAmount,
+        0,
+        NUSD_POOL,
+        1,
+        0,
+        MAX_UINT256,
+        kappa,
+      )
 
+    // This should NOT BE rate limited
+    expect(await bridge.kappaExists(kappa)).to.be.true
+
+    kappa = keccak256(randomBytes(32))
+
+    await bridge
+      .connect(nodeGroup)
+      .withdrawAndRemove(
+        await recipient.getAddress(),
+        NUSD,
+        secondAmount,
+        0,
+        NUSD_POOL,
+        1,
+        0,
+        MAX_UINT256,
+        kappa,
+      )
+    // This should BE rate limited
     expect(await bridge.kappaExists(kappa)).to.be.false
-    await expect(rateLimiter.retryByKappa(kappa)).to.be.not.reverted
+
+    await rateLimiter.retryByKappa(kappa)
     expect(await bridge.kappaExists(kappa)).to.be.true
   })
 
-  it("Mint: should add to retry queue if rate limit hit", async () => {
-    const allowanceAmount = 500
-    const mintAmount = 1000
+  it("Mint: should add to retry queue only if rate limit hit", async () => {
+    const allowanceAmount = getBigNumber(100)
+    const firstAmount = getBigNumber(100)
+    const secondAmount = allowanceAmount.sub(firstAmount).add(1)
 
     await setupAllowanceTest(SYN, allowanceAmount)
-    const kappa = keccak256(randomBytes(32))
+    let kappa = keccak256(randomBytes(32))
 
-    await expect(
-      bridge
-        .connect(nodeGroup)
-        .mint(await recipient.getAddress(), SYN, mintAmount, 10, kappa),
-    ).to.be.not.reverted
+    await bridge
+      .connect(nodeGroup)
+      .mint(await recipient.getAddress(), SYN, firstAmount, 0, kappa)
 
+    // This should NOT BE rate limited
+    expect(await bridge.kappaExists(kappa)).to.be.true
+
+    kappa = keccak256(randomBytes(32))
+    await bridge
+      .connect(nodeGroup)
+      .mint(await recipient.getAddress(), SYN, secondAmount, 0, kappa)
+    // This should BE rate limited
     expect(await bridge.kappaExists(kappa)).to.be.false
-    await expect(rateLimiter.retryByKappa(kappa)).to.be.not.reverted
+    await rateLimiter.retryByKappa(kappa)
     expect(await bridge.kappaExists(kappa)).to.be.true
   })
 
