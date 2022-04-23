42d1b1eb7c2518ffdef978af42bea55e01c4ab3c
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-23 18:09:43 +0300

    frax wrapper

---
+++ contracts/bridge/interfaces/IFrax.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+
+interface IFrax {
+    // -- VIEWS --
+
+    function canSwap(address bridgeTokenAddress) external view returns (bool);
+
+    function exchangesPaused() external view returns (bool);
+
+    // solhint-disable-next-line
+    function fee_exempt_list(address swapper) external view returns (bool);
+
+    // solhint-disable-next-line
+    function swap_fees(address bridgeTokenAddress, uint256 direction)
+        external
+        view
+        returns (uint256);
+
+    // solhint-disable-next-line
+    function mint_cap() external view returns (uint256);
+
+    // -- SWAP --
+    function exchangeCanonicalForOld(
+        address bridgeTokenAddress,
+        uint256 tokenAmount
+    ) external returns (uint256 amountOut);
+
+    function exchangeOldForCanonical(
+        address bridgeTokenAddress,
+        uint256 tokenAmount
+    ) external returns (uint256 amountOut);
+}
