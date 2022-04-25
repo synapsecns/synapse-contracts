e1cb7733093619148ce5d6833278c2a322b2b89c
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-25 12:48:33 +0300

    group foundry tests by forked chain

--- scripts/foundry.sh
+++ scripts/foundry.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-forge test --match-contract "FraxMovrTest" --fork-url https://moonriver.api.onfinality.io/public --fork-block-number 1730000 -vvvv
-forge test --no-match-contract "FraxMovrTest" -vvvv
+forge test --match-contract "TestMovr" --fork-url $MOVR_API --fork-block-number 1730000 -vvvv
+forge test --match-contract "Test$" -vvvv
