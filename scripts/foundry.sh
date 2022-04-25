2bdfeb2035daf5f8c832e96107558f2d2984b9f8
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-25 21:04:46 +0300

    option to narrow testing

--- scripts/foundry.sh
+++ scripts/foundry.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# Test contracts ending with exactly "TestMovr" require Moonriver RPC and block number
-forge test --match-contract "TestMovr$" --fork-url $MOVR_API --fork-block-number 1730000 -vvvv
 # Test contracts ending with exactly "Test" don't require any forking
-forge test --match-contract "Test$" -vvvv
+forge test --match-contract "$1.*Test$" -vvvv
+
+# Test contracts ending with exactly "TestMovr" require Moonriver RPC and block number
+forge test --match-contract "$1.*TestMovr$" --fork-url $MOVR_API --fork-block-number 1730000 -vvvv
