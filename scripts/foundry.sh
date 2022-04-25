dadd01978fb4ffb8277fbd469f204e69609464ae
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-25 14:45:02 +0300

    stricter filtering + comments

--- scripts/foundry.sh
+++ scripts/foundry.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-forge test --match-contract "TestMovr" --fork-url $MOVR_API --fork-block-number 1730000 -vvvv
+# Test contracts ending with exactly "TestMovr" require Moonriver RPC and block number
+forge test --match-contract "TestMovr$" --fork-url $MOVR_API --fork-block-number 1730000 -vvvv
+# Test contracts ending with exactly "Test" don't require any forking
 forge test --match-contract "Test$" -vvvv
