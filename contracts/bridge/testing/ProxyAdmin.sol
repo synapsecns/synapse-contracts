9f8c94b7035be514270c6fd8c59432fc2eb194f3
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-26 22:02:49 +0300

    4.3.1 -> 4.5.0 OZ upgrade

--- contracts/bridge/testing/ProxyAdmin.sol
+++ contracts/bridge/testing/ProxyAdmin.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.11;
 
-import "@openzeppelin/contracts-4.3.1/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts-4.5.0/proxy/transparent/ProxyAdmin.sol";
 
 // used to generate typechain for proxy admin
 contract ProxyAdminInterface is ProxyAdmin {
