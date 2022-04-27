e649de662bf73a9c55caf5fa43e336c16c2d9e14
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-27 22:32:52 +0300

    add arbitrum to foundry workflow

--- scripts/foundry.sh
+++ scripts/foundry.sh
@@ -9,5 +9,8 @@ forge test --match-contract "$1.*TestAvax$" --fork-url $AVAX_API --fork-block-nu
 # Test contracts ending with exactly "TestEth" require Ethereum RPC and block number: 2022-04-24
 forge test --match-contract "$1.*TestEth$" --fork-url $ALCHEMY_API --fork-block-number 14650000 -vvv
 
+# Test contracts ending with exactly "TestArb" require Arbitrum RPC and block number: 2022-04-26  
+forge test --match-contract "$1.*TestArb$" --fork-url $ARBITRUM_API --fork-block-number 10600000 -vvv
+
 # Test contracts ending with exactly "TestMovr" require Moonriver RPC and block number: 2022-04-21
 forge test --match-contract "$1.*TestMovr$" --fork-url $MOVR_API --fork-block-number 1730000 -vvv
