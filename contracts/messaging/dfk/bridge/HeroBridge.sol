aada0c9ad1ccf1ffb2fb5c676ab18ca188d3164f
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-04-27 19:36:50 -0400

    finalize handle logic for mints / withdraws

--- contracts/messaging/dfk/bridge/HeroBridge.sol
+++ contracts/messaging/dfk/bridge/HeroBridge.sol
@@ -24,7 +24,7 @@ contract HeroBridge is SynMessagingReceiver {
         heroes = _heroes;
     }
 
-    function _createMessage(uint256 _heroId, address _dstUserAddress, Hero memory _heroToBridge) internal pure returns (bytes calldata {
+    function _createMessage(uint256 _heroId, address _dstUserAddress, Hero memory _heroToBridge) internal pure returns (bytes memory) {
         // create the message here from the nested struct
         MessageFormat memory msgFormat = MessageFormat({
             dstHeroId: _heroId,
@@ -34,7 +34,7 @@ contract HeroBridge is SynMessagingReceiver {
         return abi.encode(msgFormat);
     }
 
-    function _decodeMessage(bytes calldata _message) internal pure returns (MessageFormat memory) {
+    function _decodeMessage(bytes memory _message) internal pure returns (MessageFormat memory) {
         (MessageFormat memory decodedMessage) = abi.decode(_message, (MessageFormat));
         return decodedMessage;
     }
@@ -48,10 +48,10 @@ contract HeroBridge is SynMessagingReceiver {
         Hero memory heroToBridge = IHeroCoreUpgradeable(heroes).getHero(_heroId);
         bytes32 receiver = trustedRemoteLookup[_dstChainId];
         // _createMessage(heroId, dstUserAddress, Hero);
-        bytes calldata msgToPass = _createMessage(_heroId, msg.sender, heroToBridge);
+        bytes memory msgToPass = _createMessage(_heroId, msg.sender, heroToBridge);
         // Create _options
         // temporarily empty
-        bytes calldata options = bytes("");
+        bytes memory options = bytes("");
 
         IHeroCoreUpgradeable(heroes).safeTransferFrom(msg.sender, address(this), _heroId);
         // Hero now locked, message can be safely emitted
@@ -63,7 +63,7 @@ contract HeroBridge is SynMessagingReceiver {
     // executeMessage() handles permissioning checks
     function _handleMessage(bytes32 _srcAddress,
         uint256 _srcChainId,
-        bytes calldata _message,
+        bytes memory _message,
         address _executor) internal override returns (MsgExecutionStatus) {
             // Decode _message, depending on exactly how the originating message is structured
             /** 
@@ -76,21 +76,28 @@ contract HeroBridge is SynMessagingReceiver {
              
              Hero memory dstHero = passedMsg.dstHero;
              address dstUser = passedMsg.dstUser;
-             uint256 dstHeroId = passedMsg.dstHero;
+             uint256 dstHeroId = passedMsg.dstHeroId;
 
-            /** 
-             If hero ID doesn't exist: 
-             1. Mint a hero to msg.dstUserAddress with most recent attributes from the message, and the correct hero ID
-             2. Tx completed, return Success
-             */
-
-
-            /** 
-             If heroId does exist (which means it should be locked on this contract), as it was bridged before.
-             1. first update the hero attributes based on the attributes in the message (Assumes the message has more recent attributes)
-             2. Then transfer it to message.dstUserAddress
-             3. Tx completed, return Success
-             */ 
+            // will revert if non-existant Hero
+            try IHeroCoreUpgradeable(heroes).ownerOf(dstHeroId) {
+                /** 
+                If heroId does exist (which means it should be locked on this contract), as it was bridged before.
+                1. first update the hero attributes based on the attributes in the message (Assumes the message has more recent attributes)
+                */
+                IHeroCoreUpgradeable(heroes).updateHero(dstHero);
+                // 2. Then transfer it to message.dstUserAddress
+                IHeroCoreUpgradeable(heroes).safeTransferFrom(address(this), dstUser, dstHeroId);
+                // 3. Tx completed, return Success
+                return MsgExecutionStatus.Success;
+            } catch {
+                /** 
+                If hero ID doesn't exist: 
+                1. Mint a hero to msg.dstUserAddress with most recent attributes from the message, and the correct hero ID
+                2. Tx completed, return Success
+                */
+                IHeroCoreUpgradeable(heroes).bridgeMint(dstHero, dstUser);
+                return MsgExecutionStatus.Success;
+            }
         }
 
     
