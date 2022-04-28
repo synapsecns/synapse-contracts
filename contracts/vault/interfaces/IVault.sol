796912ef7e5dee4cd27366ff24c43d6faeb5b5ba
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-04-28 23:05:14 +0300

    more consistency in Vault functions

--- contracts/vault/interfaces/IVault.sol
+++ contracts/vault/interfaces/IVault.sol
@@ -9,7 +9,7 @@ interface IVault {
 
     function chainGasAmount() external returns (uint256);
 
-    function getFeeBalance(address tokenAddress)
+    function getFeeBalance(IERC20 token)
         external
         view
         returns (uint256);
@@ -18,11 +18,39 @@ interface IVault {
 
     function kappaExists(bytes32 kappa) external view returns (bool);
 
+    function startBlockNumber() external view returns (uint256);
+
+    function bridgeVersion() external view returns (uint256);
+
+    // solhint-disable-next-line
+    function NODEGROUP_ROLE() external view returns (bytes32);
+
+    // solhint-disable-next-line
+    function GOVERNANCE_ROLE() external view returns (bytes32);
+
     // solhint-disable-next-line
     function BRIDGE_ROLE() external view returns (bytes32);
 
     // solhint-disable-next-line
     function WETH_ADDRESS() external returns (address payable);
+
+    // -- RESTRICTED ACCESS --
+
+    function initialize() external;
+
+    function setChainGasAmount(uint256 amount) external;
+
+    function setWethAddress(address payable _wethAddress) external;
+
+    function addKappas(bytes32[] calldata kappas) external;
+
+    function recoverGAS(address to) external;
+
+    function withdrawFees(IERC20 token, address to) external;
+
+    function pause() external;
+
+    function unpause() external;
 
     // -- VAULT FUNCTIONS --
 
