a2e460d4a0b8a08b72b72c93834e0e2f6521f261
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-05-01 18:49:55 +0300

    amm: adapt pools for solidity 0.8

--- deploy/amm/004_deploy_SwapUtils.ts
+++ deploy/amm/004_deploy_SwapUtils.ts
@@ -1,8 +1,9 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import { CHAIN_ID } from "../../utils/network"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
+  const { deployments, getNamedAccounts, getChainId } = hre
   const { deploy, get } = deployments
   const { libraryDeployer } = await getNamedAccounts()
 
@@ -11,6 +12,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     log: true,
     skipIfAlreadyDeployed: true,
   })
+
+  if ((await getChainId()) == CHAIN_ID.HARDHAT) {
+    await deploy("SwapUtils08", {
+      from: libraryDeployer,
+      log: true,
+      skipIfAlreadyDeployed: true,
+    })
+  }
 }
 export default func
-func.tags = ["SwapUtils"]
+func.tags = ["SwapUtils", "SwapUtils08"]
