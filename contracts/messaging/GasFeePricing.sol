b38cd83f0d660070409f62c6e408530d2ac12e15
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-05-03 00:30:59 -0400

    _options encoding and decoding

--- contracts/messaging/GasFeePricing.sol
+++ contracts/messaging/GasFeePricing.sol
@@ -4,13 +4,16 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts-4.5.0/access/Ownable.sol";
 
-contract GasFeePricing is Ownable {
+import "forge-std/Test.sol";
+
+contract GasFeePricing is Ownable, Test {
     // DstChainId => The estimated current gas price in wei of the destination chain
     mapping(uint256 => uint256) public dstGasPriceInWei;
     // DstChainId => USD gas ratio of dstGasToken / srcGasToken
     mapping(uint256 => uint256) public dstGasTokenRatio;
 
     constructor() {}
+    
 
     /**
      * @notice Permissioned method to allow an off-chain party to set what each dstChain's
@@ -36,15 +39,53 @@ contract GasFeePricing is Ownable {
      * @notice Returns srcGasToken fee to charge in wei for the cross-chain message based on the gas limit
      * @param _options Versioned struct used to instruct relayer on how to proceed with gas limits. Contains data on gas limit to submit tx with.
      */
-    function estimateGasFee(uint256 _dstChainId, bytes calldata _options)
+    function estimateGasFee(uint256 _dstChainId, bytes memory _options)
         external
         view
         returns (uint256)
     {
+        uint256 gasLimit;
         // temporary gas limit set
-        uint256 gasLimit = 200000;
+        if (_options.length != 0) {
+            (uint16 txType, uint256 gasLimit, uint256 dstAirdrop, bytes32 dstAddress) = decodeOptions(_options);
+        } else {
+            gasLimit = 200000;
+        }
+
         return ((dstGasPriceInWei[_dstChainId] *
             dstGasTokenRatio[_dstChainId] *
             gasLimit) / 10**18);
+    }
+
+    function encodeOptions(uint16 txType, uint256 gasLimit) public pure returns(bytes memory) {
+        return abi.encodePacked(txType, gasLimit);
+    }
+
+    function encodeOptions(uint16 txType, uint256 gasLimit, uint256 dstNativeAmt, bytes32 dstAddress) public pure returns(bytes memory) {
+        return abi.encodePacked(txType, gasLimit, dstNativeAmt, dstAddress);
+    }
+
+    function decodeOptions(bytes memory _options) public pure returns (uint16, uint256, uint256, bytes32) {
+        // decoding the _options - reverts if type 2 and there is no dstNativeAddress
+        require(_options.length == 34 || _options.length > 66, "Wrong _adapterParameters size");
+        uint16 txType;
+        uint256 gasLimit;
+        uint256 dstNativeAmt;
+        bytes32 dstNativeAddress;
+        assembly {
+            txType := mload(add(_options, 2))
+            gasLimit := mload(add(_options, 34))
+        }
+
+        if (txType == 2) {
+            assembly {
+                dstNativeAmt := mload(add(_options, 66))
+                dstNativeAddress := mload(add(_options, 98))
+            }
+            require(dstNativeAmt != 0, "dstNativeAmt empty");
+            require(dstNativeAddress != bytes32(0), "dstNativeAddress empty");
+        }
+
+        return (txType, gasLimit, dstNativeAmt, dstNativeAddress);
     }
 }
