7efa2f29a8641576d1027d251aa166d5a1a52b04
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-05-04 14:09:29 -0400

    add gaps on syn messaging receiver upgradeable

--- contracts/messaging/framework/SynMessagingReceiverUpgradeable.sol
+++ contracts/messaging/framework/SynMessagingReceiverUpgradeable.sol
@@ -69,4 +69,11 @@ abstract contract SynMessagingReceiverUpgradeable is ISynMessagingReceiver, Owna
         return trustedRemoteLookup[_chainId];
     }
 
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[48] private __gap;
+
 }
