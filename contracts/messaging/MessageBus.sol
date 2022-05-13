18dc225709348f813860d86687ce90a10975aa9b
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-05-13 16:48:09 -0400

    remove top level imports frm msgbus

--- contracts/messaging/MessageBus.sol
+++ contracts/messaging/MessageBus.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.13;
-
-import "@openzeppelin/contracts-4.5.0/access/Ownable.sol";
-import "@openzeppelin/contracts-4.5.0/security/Pausable.sol";
 import "./MessageBusSender.sol";
 import "./MessageBusReceiver.sol";
 
