ff7196e6119307fa18e58835b1b810b27fb63dad
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-05-14 21:10:41 +0300

    tests: add more Curve pools, add FRAX

--- test/vault/fork/VaultTestEth.t.sol
+++ test/vault/fork/VaultTestEth.t.sol
@@ -18,6 +18,7 @@ contract VaultTestEth is DefaultVaultForkedTest {
         address usdc;
         address usdt;
         address wbtc;
+        address frax;
     }
 
     TestTokens internal testTokens =
@@ -25,7 +26,8 @@ contract VaultTestEth is DefaultVaultForkedTest {
             dai: 0x6B175474E89094C44Da98b954EedeAC495271d0F,
             usdc: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
             usdt: 0xdAC17F958D2ee523a2206206994597C13D831ec7,
-            wbtc: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599
+            wbtc: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
+            frax: 0x853d955aCEf822Db058eb8505911ED77F175b99e
         });
 
     TestSetup internal ethConfig =
@@ -45,6 +47,9 @@ contract VaultTestEth is DefaultVaultForkedTest {
     string public constant SUSHISWAP = "Sushiswap";
 
     string public constant CURVE_3POOL = "Curve 3pool";
+    string public constant CURVE_AAVE = "Curve Aave 3pool";
+    string public constant CURVE_FRAX = "Curve FRAX";
+    string public constant CURVE_TRICRYPTO = "Curve TriCrypto";
 
     constructor() DefaultVaultForkedTest(ethConfig) {
         kappas = new bytes32[](4);
@@ -61,11 +66,14 @@ contract VaultTestEth is DefaultVaultForkedTest {
     }
 
     function _setupAdapters() internal override {
-        _deployAdapter(
+        // Mark Synapse nUSD Adapter as potentially "underquoting",
+        // until Quoter is fixed to avoid using it twice in path
+        _deployUnderquoteAdapter(
             "SynapseBaseMainnetAdapter",
             SYNAPSE_NUSD,
             abi.encode(SYNAPSE_NUSD, 0, 0x1116898DdA4015eD8dDefb84b6e8Bc24528Af2d8),
-            abi.encode(32, 4, testTokens.dai, testTokens.usdc, testTokens.usdt, basicTokens.nusd)
+            abi.encode(32, 4, testTokens.dai, testTokens.usdc, testTokens.usdt, basicTokens.nusd),
+            true
         );
 
         _deployAdapter(
@@ -78,15 +86,102 @@ contract VaultTestEth is DefaultVaultForkedTest {
                 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
                 30
             ),
-            abi.encode(32, 5, basicTokens.weth, testTokens.dai, testTokens.usdc, testTokens.usdt, testTokens.wbtc)
+            abi.encode(
+                32,
+                6,
+                basicTokens.weth,
+                testTokens.dai,
+                testTokens.usdc,
+                testTokens.usdt,
+                testTokens.wbtc,
+                testTokens.frax
+            )
+        );
+
+        _deployAdapter(
+            "UniswapV2Adapter",
+            SUSHISWAP,
+            abi.encode(
+                SUSHISWAP,
+                0,
+                0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac,
+                0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303,
+                30
+            ),
+            abi.encode(
+                32,
+                7,
+                basicTokens.weth,
+                basicTokens.syn,
+                testTokens.dai,
+                testTokens.usdc,
+                testTokens.usdt,
+                testTokens.wbtc,
+                testTokens.frax
+            )
+        );
+
+        // Curve Adapter can underquote by 1 wei sometimes
+        _deployUnderquoteAdapter(
+            "CurveBaseAdapter",
+            CURVE_3POOL,
+            abi.encode(CURVE_3POOL, 0, 0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7, false),
+            abi.encode(32, 3, testTokens.dai, testTokens.usdc, testTokens.usdt),
+            true
+        );
+
+        _deployUnderquoteAdapter(
+            "CurveLendingAdapter",
+            CURVE_AAVE,
+            abi.encode(CURVE_AAVE, 0, 0xDeBF20617708857ebe4F679508E7b7863a8A8EeE, false),
+            abi.encode(32, 3, testTokens.dai, testTokens.usdc, testTokens.usdt),
+            true
+        );
+
+        _deployUnderquoteAdapter(
+            "CurveMetaAdapter",
+            CURVE_FRAX,
+            abi.encode(
+                CURVE_FRAX,
+                0,
+                0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B,
+                true,
+                0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7
+            ),
+            abi.encode(32, 4, testTokens.dai, testTokens.usdc, testTokens.usdt, testTokens.frax),
+            true
+        );
+
+        _deployUnderquoteAdapter(
+            "CurveTriCryptoAdapter",
+            CURVE_TRICRYPTO,
+            abi.encode(CURVE_TRICRYPTO, 0, 0xD51a44d3FaE010294C616388b506AcdA1bfAAE46, false),
+            abi.encode(32, 3, basicTokens.weth, testTokens.wbtc, testTokens.usdt),
+            true
         );
     }
 
     function _setupTokens() internal override {
-        // Add WGAS as first token, routeToken = true
+        // _addSimpleBridgeToken(token, name, minAmount, maxAmount, isMintBurn,
+        //                              feeBP, minBridgeFee, minGasDropFee, minSwapFee, chainIdNonEVM, isRouteToken)
+
+        // minAmount reflects token decimals, while maxAmount doesn't.
+        // It doesn't make sense right until it does.
+
+        // Always add WGAS as first token
+        // mintBurn = false, routeToken = true
         _addSimpleBridgeToken(basicTokens.weth, "wETH", 10**16, 10**3, false, 10, 2 * 10**15, 0, 6 * 10**15, 0, true);
-        // routeToken = false
+
+        // mintBurn = false, routeToken = false
         _addSimpleBridgeToken(basicTokens.nusd, "nUSD", 10**18, 10**6, false, 10, 2 * 10**17, 0, 6 * 10**17, 0, false);
+
+        // mintBurn = true, routeToken = false
+        _addSimpleBridgeToken(basicTokens.syn, "SYN", 10**18, 10**6, true, 10, 2 * 10**17, 0, 6 * 10**17, 0, false);
+
+        // mintBurn = false, routeToken = true
+        _addSimpleBridgeToken(testTokens.frax, "FRAX", 10**18, 10**6, false, 10, 2 * 10**17, 0, 6 * 10**17, 0, true);
+        // Sorry Sam, gotta casually mint 1B FRAX for testing
+        _addTokenTo(testTokens.frax, address(vault), 10**27);
 
         // routeToken = true
         _addToken(testTokens.dai, "DAI", 10**18, 10**6, true);
