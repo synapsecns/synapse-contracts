b0bc368a12066ff883ea56e08b79b39a460fa51b
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-05-18 19:10:50 +0300

    update foundry CI scripts

--- scripts/foundry.sh
+++ scripts/foundry.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
 # Test contracts ending with exactly "Test" don't require any forking
-forge test --match-contract "$1.*Test$" -vvv
+forge test --match-contract "$1.*Test$" -vvv || exit 1
 
 # Test contracts ending with exactly "TestAvax" require Avalanche RPC and block number: 2022-04-25
-forge test --match-contract "$1.*TestAvax$" --fork-url $AVAX_API --fork-block-number 13897000 -vvv
+forge test --match-contract "$1.*TestAvax$" --fork-url $AVAX_API --fork-block-number 13897000 -vvv || exit 1
 
 # Test contracts ending with exactly "TestEth" require Ethereum RPC and block number: 2022-04-24
-forge test --match-contract "$1.*TestEth$" --fork-url $ALCHEMY_API --fork-block-number 14650000 -vvv
+forge test --match-contract "$1.*TestEth$" --fork-url $ALCHEMY_API --fork-block-number 14650000 -vvv || exit 1
+
+# Test contracts ending with exactly "TestArb" require Arbitrum RPC and block number: 2022-04-26
+forge test --match-contract "$1.*TestArb$" --fork-url $ARBITRUM_API --fork-block-number 10600000 -vvv || exit 1
+
+# Test contracts ending with exactly "TestOpt" require Optimism RPC and block number: 2022-04-26
+forge test --match-contract "$1.*TestOpt$" --fork-url $OPTIMISM_API --fork-block-number 6600000 -vvv || exit 1
 
 # Test contracts ending with exactly "TestMovr" require Moonriver RPC and block number: 2022-04-21
-forge test --match-contract "$1.*TestMovr$" --fork-url $MOVR_API --fork-block-number 1730000 -vvv
+forge test --match-contract "$1.*TestMovr$" --fork-url $MOVR_API --fork-block-number 1730000 -vvv || exit 1
