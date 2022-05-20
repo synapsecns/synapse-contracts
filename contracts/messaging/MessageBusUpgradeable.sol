712ccdb8490d2265bcbea9e42325f433e94ee97c
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-05-20 16:44:29 +0300

    run prettier

--- contracts/messaging/MessageBusUpgradeable.sol
+++ contracts/messaging/MessageBusUpgradeable.sol
@@ -4,14 +4,8 @@ pragma solidity 0.8.13;
 import "./MessageBusSenderUpgradeable.sol";
 import "./MessageBusReceiverUpgradeable.sol";
 
-contract MessageBusUpgradeable is
-    MessageBusSenderUpgradeable,
-    MessageBusReceiverUpgradeable
-{
-    function initialize(address _gasFeePricing, address _authVerifier)
-        external
-        initializer
-    {
+contract MessageBusUpgradeable is MessageBusSenderUpgradeable, MessageBusReceiverUpgradeable {
+    function initialize(address _gasFeePricing, address _authVerifier) external initializer {
         __Ownable_init_unchained();
         __Pausable_init_unchained();
         __MessageBusSender_init_unchained(_gasFeePricing);
