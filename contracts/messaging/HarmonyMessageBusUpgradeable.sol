95d0fdc36b682e2f81827e2d37d4fe014fe08416
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-05-24 21:26:49 +0300

    remove duplicated code from HarmonyBus

--- contracts/messaging/HarmonyMessageBusUpgradeable.sol
+++ contracts/messaging/HarmonyMessageBusUpgradeable.sol
@@ -2,29 +2,12 @@
 
 pragma solidity 0.8.13;
 
-import "./MessageBusSenderUpgradeable.sol";
-import "./MessageBusReceiverUpgradeable.sol";
+import "./MessageBusUpgradeable.sol";
 
-contract HarmonyMessageBusUpgradeable is MessageBusSenderUpgradeable, MessageBusReceiverUpgradeable {
+contract HarmonyMessageBusUpgradeable is MessageBusUpgradeable {
     uint256 private constant CHAIN_ID = 1666600000;
-
-    function initialize(address _gasFeePricing, address _authVerifier) external initializer {
-        __Ownable_init_unchained();
-        __Pausable_init_unchained();
-        __MessageBusSender_init_unchained(_gasFeePricing);
-        __MessageBusReceiver_init_unchained(_authVerifier);
-    }
 
     function _chainId() internal pure override returns (uint256) {
         return CHAIN_ID;
-    }
-
-    // PAUSABLE FUNCTIONS ***/
-    function pause() external onlyOwner {
-        _pause();
-    }
-
-    function unpause() external onlyOwner {
-        _unpause();
     }
 }
