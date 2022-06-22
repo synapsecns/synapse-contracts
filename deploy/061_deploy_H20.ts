f734475ea1aedfe517b8ab511ccec7c166c13526
Defi-Moses <moses@synapseprotocol.com>
2022-06-22 14:34:59 +0100

    H2O file updates + new bridge contract

--- deploy/061_deploy_H20.ts
+++ deploy/061_deploy_H20.ts
@@ -1,15 +1,29 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types"
-import { DeployFunction } from "hardhat-deploy/types"
-import { CHAIN_ID } from "../utils/network"
-import {includes} from "lodash";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { CHAIN_ID } from "../utils/network";
+import { includes } from "lodash";
+import { ChainId } from "@synapseprotocol/sdk";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, getChainId } = hre
-  const { deploy, get, execute, getOrNull, log, save } = deployments
-  const { deployer } = await getNamedAccounts()
+  const { deployments, getNamedAccounts, getChainId } = hre;
+  const { deploy, get, execute, getOrNull, log, save } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-  if ((includes([CHAIN_ID.POLYGON], await getChainId()))) {
-    if ((await getOrNull("H20")) == null) {
+  if (
+    includes(
+      [
+        CHAIN_ID.POLYGON,
+        CHAIN_ID.AVALANCHE,
+        CHAIN_ID.ARBITRUM,
+        CHAIN_ID.OPTIMISM,
+        CHAIN_ID.MOONBEAM,
+        CHAIN_ID.BSC,
+        CHAIN_ID.MOONRIVER,
+      ],
+      await getChainId()
+    )
+  ) {
+    if ((await getOrNull("H2O")) == null) {
       const receipt = await execute(
         "SynapseERC20Factory",
         { from: deployer, log: true },
@@ -17,56 +31,54 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         (
           await get("SynapseERC20")
         ).address,
-        "H20data",
-        "H20",
+        "H2O",
+        "H2O",
         "18",
-        deployer,
+        deployer
         // (
         //   await get("DevMultisig")
         // ).address,
-      )
+      );
 
-      const newTokenEvent = receipt?.events?.find(
-        (e: any) => e["event"] == "SynapseERC20Created",
-      )
-      const tokenAddress = newTokenEvent["args"]["contractAddress"]
-      log(`deployed H20 token at ${tokenAddress}`)
+      const newTokenEvent = receipt?.events?.find((e: any) => e["event"] == "SynapseERC20Created");
+      const tokenAddress = newTokenEvent["args"]["contractAddress"];
+      log(`deployed H2O token at ${tokenAddress}`);
 
-      await save("H20", {
+      await save("H2O", {
         abi: (await get("SynapseERC20")).abi, // Generic ERC20 ABI
         address: tokenAddress,
-      })
+      });
 
       await execute(
-        "H20",
+        "H2O",
         { from: deployer, log: true },
         "grantRole",
         "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6",
         (
           await get("SynapseBridge")
-        ).address,
-      )
+        ).address
+      );
 
       await execute(
-        "H20",
+        "H2O",
         { from: deployer, log: true },
         "grantRole",
         "0x0000000000000000000000000000000000000000000000000000000000000000",
         (
           await get("DevMultisig")
-        ).address,
-      )
+        ).address
+      );
 
       await execute(
-        "H20",
+        "H2O",
         { from: deployer, log: true },
         "renounceRole",
         "0x0000000000000000000000000000000000000000000000000000000000000000",
-        deployer,
-      )
+        deployer
+      );
     }
   }
-}
+};
 
-export default func
-func.tags = ["H20"]
+export default func;
+func.tags = ["H2O"];
