5468edab2038a3a81d4798171e6e19b211a96f3a
Defi-Moses <moses@synapseprotocol.com>
2022-08-15 09:47:07 -0700

    synFRAX Metis deployment

--- deploy/012_deploy_SynFrax.ts
+++ deploy/012_deploy_SynFrax.ts
@@ -9,7 +9,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await getNamedAccounts()
 
 
-  if (includes([CHAIN_ID.FANTOM, CHAIN_ID.POLYGON, CHAIN_ID.ARBITRUM, CHAIN_ID.AVALANCHE, CHAIN_ID.MOONRIVER, CHAIN_ID.HARMONY], await getChainId())) {
+  if (includes([CHAIN_ID.FANTOM, CHAIN_ID.POLYGON, CHAIN_ID.ARBITRUM, CHAIN_ID.AVALANCHE, CHAIN_ID.MOONRIVER, CHAIN_ID.HARMONY, CHAIN_ID.METIS], await getChainId())) {
     if ((await getOrNull("synFRAX")) == null) {
       const receipt = await execute(
         "SynapseERC20Factory",
