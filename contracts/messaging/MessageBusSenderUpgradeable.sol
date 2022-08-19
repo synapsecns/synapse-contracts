6a5cd24a4cbf5ab051294728352200d47f909c12
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-08-19 17:59:59 +0300

    Revert "MessageBus: add custom block.chainid option"
    This reverts commit 42193b9a72c96d504e89d54687c6dea9b562f90f.

--- contracts/messaging/MessageBusSenderUpgradeable.sol
+++ contracts/messaging/MessageBusSenderUpgradeable.sol
@@ -6,9 +6,8 @@ import "@openzeppelin/contracts-upgradeable-4.5.0/access/OwnableUpgradeable.sol"
 import "@openzeppelin/contracts-upgradeable-4.5.0/security/PausableUpgradeable.sol";
 
 import "./interfaces/IGasFeePricing.sol";
-import "./ContextChainIdUpgradeable.sol";
 
-contract MessageBusSenderUpgradeable is OwnableUpgradeable, PausableUpgradeable, ContextChainIdUpgradeable {
+contract MessageBusSenderUpgradeable is OwnableUpgradeable, PausableUpgradeable {
     address public gasFeePricing;
     uint64 public nonce;
     uint256 public fees;
@@ -100,12 +99,11 @@ contract MessageBusSenderUpgradeable is OwnableUpgradeable, PausableUpgradeable,
         bytes calldata _options,
         address payable _refundAddress
     ) internal {
-        uint256 srcChainId = _chainId();
-        require(_dstChainId != srcChainId, "Invalid chainId");
+        require(_dstChainId != block.chainid, "Invalid chainId");
         uint256 fee = estimateFee(_dstChainId, _options);
         require(msg.value >= fee, "Insufficient gas fee");
-        bytes32 msgId = computeMessageId(msg.sender, srcChainId, _receiver, _dstChainId, nonce, _message);
-        emit MessageSent(msg.sender, srcChainId, _receiver, _dstChainId, _message, nonce, _options, fee, msgId);
+        bytes32 msgId = computeMessageId(msg.sender, block.chainid, _receiver, _dstChainId, nonce, _message);
+        emit MessageSent(msg.sender, block.chainid, _receiver, _dstChainId, _message, nonce, _options, fee, msgId);
         fees += fee;
         ++nonce;
         // refund gas fees in case of overpayment
