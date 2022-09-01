a12dc54bfc93df2334bc5828fc8d09536cab84c6
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-09-01 14:06:31 -0400

    new round of dfk tokens

---
+++ deploy/dfk/011_deploy_DFK_BTCb.ts
@@ -0,0 +1,74 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { CHAIN_ID } from "../../utils/network";
+import { includes } from "lodash";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, getChainId } = hre;
+  const { deploy, get, execute, getOrNull, log, save } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  if (
+    includes(
+      [
+        CHAIN_ID.DFK
+      ],
+      await getChainId()
+    )
+  ) {
+    if ((await getOrNull("BTCb")) == null) {
+      const receipt = await execute(
+        "SynapseERC20Factory",
+        { from: deployer, log: true },
+        "deploy",
+        (
+          await get("SynapseERC20")
+        ).address,
+        "Bitcoin",
+        "BTC.b",
+        "8",
+        deployer
+      );
+
+      const newTokenEvent = receipt?.events?.find((e: any) => e["event"] == "SynapseERC20Created");
+      const tokenAddress = newTokenEvent["args"]["contractAddress"];
+      log(`deployed BTCb token at ${tokenAddress}`);
+
+      await save("BTCb", {
+        abi: (await get("SynapseERC20")).abi, // Generic ERC20 ABI
+        address: tokenAddress,
+      });
+
+      await execute(
+        "BTCb",
+        { from: deployer, log: true },
+        "grantRole",
+        "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6",
+        (
+          await get("SynapseBridge")
+        ).address
+      );
+
+      await execute(
+        "BTCb",
+        { from: deployer, log: true },
+        "grantRole",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        (
+          await get("DevMultisig")
+        ).address
+      );
+
+      await execute(
+        "BTCb",
+        { from: deployer, log: true },
+        "renounceRole",
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+        deployer
+      );
+    }
+  }
+};
+
+export default func;
+func.tags = ["DFK_BTCb"];
