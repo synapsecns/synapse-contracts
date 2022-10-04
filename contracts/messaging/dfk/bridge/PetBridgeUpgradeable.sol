21e14a146afb22fc33f88319b7817694757d548e
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-10-04 17:56:34 -0400

    Fix gap # of variables, only 2 storage variables used

--- contracts/messaging/dfk/bridge/PetBridgeUpgradeable.sol
+++ contracts/messaging/dfk/bridge/PetBridgeUpgradeable.sol
@@ -150,5 +150,5 @@ contract PetBridgeUpgradeable is Initializable, SynMessagingReceiverUpgradeable 
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[47] private __gap;
+    uint256[48] private __gap;
 }
