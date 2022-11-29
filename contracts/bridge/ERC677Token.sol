fdc5c593464d328d0357a5da2bff586dffb0031f
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-11-29 12:24:03 -0500

    Initial LINK ERC677 draft

---
+++ contracts/bridge/ERC677Token.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "./SynapseERC20.sol";
+
+contract ERC677 is SynapseERC20 {
+  function transferAndCall(address to, uint value, bytes data) returns (bool success);
+
+  event Transfer(address indexed from, address indexed to, uint value, bytes data);
+}
+
+
+contract ERC677Receiver {
+  function onTokenTransfer(address _sender, uint _value, bytes _data);
+}
+
+contract ERC677Token is ERC677 {
+
+  /**
+  * @dev transfer token to a contract address with additional data if the recipient is a contact.
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  * @param _data The extra data to be passed to the receiving contract.
+  */
+  function transferAndCall(address _to, uint _value, bytes _data)
+    public
+    returns (bool success)
+  {
+    super.transfer(_to, _value);
+    Transfer(msg.sender, _to, _value, _data);
+    if (isContract(_to)) {
+      contractFallback(_to, _value, _data);
+    }
+    return true;
+  }
+
+
+  // PRIVATE
+
+  function contractFallback(address _to, uint _value, bytes _data)
+    private
+  {
+    ERC677Receiver receiver = ERC677Receiver(_to);
+    receiver.onTokenTransfer(msg.sender, _value, _data);
+  }
+
+  function isContract(address _addr)
+    private
+    returns (bool hasCode)
+  {
+    uint length;
+    assembly { length := extcodesize(_addr) }
+    return length > 0;
+  }
+
+}
