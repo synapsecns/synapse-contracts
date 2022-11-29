07cf256550359e57c4b21e4adca8c1dc84dcd93b
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-11-29 12:32:37 -0500

    lint

--- contracts/bridge/ERC677Token.sol
+++ contracts/bridge/ERC677Token.sol
@@ -4,53 +4,59 @@ pragma solidity 0.6.12;
 import "./SynapseERC20.sol";
 
 contract ERC677 is SynapseERC20 {
-  function transferAndCall(address to, uint value, bytes data) returns (bool success);
+    function transferAndCall(
+        address to,
+        uint256 value,
+        bytes data
+    ) returns (bool success);
 
-  event Transfer(address indexed from, address indexed to, uint value, bytes data);
+    event Transfer(address indexed from, address indexed to, uint256 value, bytes data);
 }
 
-
 contract ERC677Receiver {
-  function onTokenTransfer(address _sender, uint _value, bytes _data);
+    function onTokenTransfer(
+        address _sender,
+        uint256 _value,
+        bytes _data
+    );
 }
 
 contract ERC677Token is ERC677 {
-
-  /**
-  * @dev transfer token to a contract address with additional data if the recipient is a contact.
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  * @param _data The extra data to be passed to the receiving contract.
-  */
-  function transferAndCall(address _to, uint _value, bytes _data)
-    public
-    returns (bool success)
-  {
-    super.transfer(_to, _value);
-    Transfer(msg.sender, _to, _value, _data);
-    if (isContract(_to)) {
-      contractFallback(_to, _value, _data);
+    /**
+     * @dev transfer token to a contract address with additional data if the recipient is a contact.
+     * @param _to The address to transfer to.
+     * @param _value The amount to be transferred.
+     * @param _data The extra data to be passed to the receiving contract.
+     */
+    function transferAndCall(
+        address _to,
+        uint256 _value,
+        bytes _data
+    ) public returns (bool success) {
+        super.transfer(_to, _value);
+        Transfer(msg.sender, _to, _value, _data);
+        if (isContract(_to)) {
+            contractFallback(_to, _value, _data);
+        }
+        return true;
     }
-    return true;
-  }
 
+    // PRIVATE
 
-  // PRIVATE
+    function contractFallback(
+        address _to,
+        uint256 _value,
+        bytes _data
+    ) private {
+        ERC677Receiver receiver = ERC677Receiver(_to);
+        receiver.onTokenTransfer(msg.sender, _value, _data);
+    }
 
-  function contractFallback(address _to, uint _value, bytes _data)
-    private
-  {
-    ERC677Receiver receiver = ERC677Receiver(_to);
-    receiver.onTokenTransfer(msg.sender, _value, _data);
-  }
-
-  function isContract(address _addr)
-    private
-    returns (bool hasCode)
-  {
-    uint length;
-    assembly { length := extcodesize(_addr) }
-    return length > 0;
-  }
-
+    function isContract(address _addr) private returns (bool hasCode) {
+        uint256 length;
+        assembly {
+            length := extcodesize(_addr)
+        }
+        return length > 0;
+    }
 }
