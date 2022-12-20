1659775b2dc5d2a134659583b977ece39fa9831e
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-12-20 12:35:30 +0000

    Fix KlaytnBridge script

--- deploy/klaytn/1001_KlaytnBridge.ts
+++ deploy/klaytn/1001_KlaytnBridge.ts
@@ -1,10 +1,15 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { CHAIN_ID } from "../../utils/network";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId } = hre;
   const { deploy, get, catchUnknownSigner } = deployments;
   const { deployer } = await getNamedAccounts();
+
+  if ((await getChainId()) != CHAIN_ID.KLATYN) {
+    return;
+  }
 
   // await deploy("WKlayUnwrapper", {
   //   from: deployer,
