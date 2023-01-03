de1565309c562c77308617daee81d90d0e5fc20f
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-01-03 22:07:14 +0000

    Move SwapQuoter related stuff to SynapseAdapter

--- contracts/bridge/router/SynapseRouter.sol
+++ contracts/bridge/router/SynapseRouter.sol
@@ -8,7 +8,6 @@ import "../interfaces/ISwapQuoter.sol";
 import "./SynapseAdapter.sol";
 
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @notice SynapseRouter contract that can be used together with SynapseBridge on any chain.
@@ -34,7 +33,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * // If tokenIn is WETH, do router.bridge{value: amount} to use native ETH instead of WETH.
  * Note: the transaction will be reverted, if `bridgeTokenO` is not set up in SynapseRouter.
  */
-contract SynapseRouter is SynapseAdapter, Ownable, ISwapQuoter {
+contract SynapseRouter is SynapseAdapter {
     // SynapseRouter is also the Adapter for the Synapse pools (this reduces the amount of token transfers).
     // SynapseRouter address will be used as swapAdapter in SwapQueries returned by a local SwapQuoter.
 
@@ -176,14 +175,6 @@ contract SynapseRouter is SynapseAdapter, Ownable, ISwapQuoter {
         token.safeApprove(spender, amount);
     }
 
-    /**
-     * @notice Sets the Quoter implementation.
-     * @dev Required for the underlying SynapseAdapter to work properly.
-     */
-    function setSwapQuoter(ISwapQuoter _swapQuoter) external onlyOwner {
-        swapQuoter = _swapQuoter;
-    }
-
     /*╔══════════════════════════════════════════════════════════════════════╗*\
     ▏*║                            BRIDGE & SWAP                             ║*▕
     \*╚══════════════════════════════════════════════════════════════════════╝*/
@@ -309,114 +300,8 @@ contract SynapseRouter is SynapseAdapter, Ownable, ISwapQuoter {
         amount = _bridgeTokens.length();
     }
 
-    /**
-     * @notice Returns a list of all supported pools.
-     */
-    function allPools() public view override returns (Pool[] memory pools) {
-        pools = swapQuoter.allPools();
-    }
-
-    /**
-     * @notice Returns the amount of tokens the given pool supports and the pool's LP token.
-     */
-    function poolInfo(address pool) public view override returns (uint256, address) {
-        return swapQuoter.poolInfo(pool);
-    }
-
-    /**
-     * @notice Returns a list of pool tokens for the given pool.
-     */
-    function poolTokens(address pool) public view override returns (address[] memory tokens) {
-        tokens = swapQuoter.poolTokens(pool);
-    }
-
-    /**
-     * @notice Returns the amount of supported pools.
-     */
-    function poolsAmount() public view override returns (uint256 amount) {
-        amount = swapQuoter.poolsAmount();
-    }
-
     /*╔══════════════════════════════════════════════════════════════════════╗*\
-    ▏*║                          VIEWS: SWAP QUOTER                          ║*▕
-    \*╚══════════════════════════════════════════════════════════════════════╝*/
-
-    /**
-     * @notice Finds the best pool for tokenIn -> tokenOut swap from the list of supported pools.
-     * Returns the `SwapQuery` struct, that can be used on SynapseRouter.
-     * minAmountOut and deadline fields will need to be adjusted based on the swap settings.
-     */
-    function getAmountOut(
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn
-    ) external view override returns (SwapQuery memory) {
-        return swapQuoter.getAmountOut(tokenIn, tokenOut, amountIn);
-    }
-
-    /**
-     * @notice Returns the exact quote for adding liquidity to a given pool
-     * in a form of a single token.
-     * @param pool      The pool to add tokens to
-     * @param amounts   An array of token amounts to deposit.
-     *                  The amount should be in each pooled token's native precision.
-     *                  If a token charges a fee on transfers, use the amount that gets transferred after the fee.
-     * @return LP token amount the user will receive
-     */
-    function calculateAddLiquidity(address pool, uint256[] memory amounts) external view override returns (uint256) {
-        return swapQuoter.calculateAddLiquidity(pool, amounts);
-    }
-
-    /**
-     * @notice Returns the exact quote for swapping between two given tokens.
-     * @param pool              The pool to use for the swap
-     * @param tokenIndexFrom    The token the user wants to sell
-     * @param tokenIndexTo      The token the user wants to buy
-     * @param dx                The amount of tokens the user wants to sell. If the token charges a fee on transfers,
-     *                          use the amount that gets transferred after the fee.
-     * @return amountOut        amount of tokens the user will receive
-     */
-    function calculateSwap(
-        address pool,
-        uint8 tokenIndexFrom,
-        uint8 tokenIndexTo,
-        uint256 dx
-    ) external view override returns (uint256 amountOut) {
-        amountOut = swapQuoter.calculateSwap(pool, tokenIndexFrom, tokenIndexTo, dx);
-    }
-
-    /**
-     * @notice Returns the exact quote for withdrawing pools tokens in a balanced way.
-     * @param pool          The pool to withdraw tokens from
-     * @param amount        The amount of LP tokens that would be burned on withdrawal
-     * @return amountsOut   Array of token balances that the user will receive
-     */
-    function calculateRemoveLiquidity(address pool, uint256 amount)
-        external
-        view
-        override
-        returns (uint256[] memory amountsOut)
-    {
-        amountsOut = swapQuoter.calculateRemoveLiquidity(pool, amount);
-    }
-
-    /**
-     * @notice Returns the exact quote for withdrawing a single pool token.
-     * @param pool          The pool to withdraw a token from
-     * @param tokenAmount   The amount of LP token to burn
-     * @param tokenIndex    Index of which token will be withdrawn
-     * @return amountOut    Calculated amount of underlying token available to withdraw
-     */
-    function calculateWithdrawOneToken(
-        address pool,
-        uint256 tokenAmount,
-        uint8 tokenIndex
-    ) external view override returns (uint256 amountOut) {
-        amountOut = swapQuoter.calculateWithdrawOneToken(pool, tokenAmount, tokenIndex);
-    }
-
-    /*╔══════════════════════════════════════════════════════════════════════╗*\
-    ▏*║                       INTERNAL: BRIDGE & SWAP                        ║*▕
+    ▏*║                            INTERNAL: SWAP                            ║*▕
     \*╚══════════════════════════════════════════════════════════════════════╝*/
 
     /**
@@ -481,7 +366,7 @@ contract SynapseRouter is SynapseAdapter, Ownable, ISwapQuoter {
     }
 
     /*╔══════════════════════════════════════════════════════════════════════╗*\
-    ▏*║                    INTERNAL: ADD & REMOVE TOKENS                     ║*▕
+    ▏*║                 INTERNAL: ADD & REMOVE BRIDGE TOKENS                 ║*▕
     \*╚══════════════════════════════════════════════════════════════════════╝*/
 
     /**
