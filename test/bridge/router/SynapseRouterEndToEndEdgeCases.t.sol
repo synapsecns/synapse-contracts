26cf7eff87a40227afded8fb02cec42a863f8137
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-01-17 13:14:05 +0000

    Add some edge case for Klaytn -> Eth bridge txs

--- test/bridge/router/SynapseRouterEndToEndEdgeCases.t.sol
+++ test/bridge/router/SynapseRouterEndToEndEdgeCases.t.sol
@@ -186,4 +186,130 @@ contract SynapseRouterEndToEndEdgeCasesTest is SynapseRouterSuite {
         // Validator completes the transaction on destination chain
         checkCompletedBridgeTx(destination, bridgeTokenDest, originQuery, destQuery);
     }
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                       KLAYTN USDC -> ETH USDT                        ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    function test_klaytnToEthereum_inUSDC_outUSDT() public {
+        ChainSetup memory origin = chains[KLAY_CHAINID];
+        // Step 1: nUSD is available as intermediate token
+        klaytnToEthereum_inUSDC_outUSDT();
+
+        // Step2: no options are available for bridging
+        // Remove nUSD from list of valid origin tokens
+        origin.router.removeToken(address(origin.nusd));
+        vm.expectRevert("No path found on origin");
+        // Doing an external call to catch the revert from the test suite
+        this.klaytnToEthereum_inUSDC_outUSDT();
+    }
+
+    function klaytnToEthereum_inUSDC_outUSDT() public {
+        // Prepare test parameters: Klaytn USDC -> Ethereum USDT
+        ChainSetup memory origin = chains[KLAY_CHAINID];
+        ChainSetup memory destination = chains[ETH_CHAINID];
+        IERC20 tokenIn = origin.usdc;
+        IERC20 tokenOut = destination.usdt;
+        uint256 amountIn = 10**uint256(ERC20(address(tokenIn)).decimals());
+        (SwapQuery memory originQuery, SwapQuery memory destQuery) = performQuoteCalls(
+            origin,
+            destination,
+            tokenIn,
+            tokenOut,
+            amountIn
+        );
+        // Could use only nUSD
+        address bridgeTokenOrigin = address(origin.nusd);
+        address bridgeTokenDest = address(destination.nusd);
+        // Klaytn: USDC -> nUSD; Ethereum: nUSD -> USDT
+        // Expect Bridge event to be emitted
+        vm.expectEmit(true, true, true, true);
+        emit TokenRedeemAndRemove({
+            to: TO,
+            chainId: ETH_CHAINID,
+            token: bridgeTokenOrigin,
+            amount: originQuery.minAmountOut,
+            swapTokenIndex: 2, // USDT index is 2
+            swapMinAmount: destQuery.minAmountOut,
+            swapDeadline: destQuery.deadline
+        });
+        // User interacts with the SynapseRouter on origin chain
+        initiateBridgeTx(origin, destination, tokenIn, tokenOut, amountIn);
+        // Validator completes the transaction on destination chain
+        checkCompletedBridgeTx(destination, bridgeTokenDest, originQuery, destQuery);
+    }
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                       KLAYTN NUSD -> ETH USDC                        ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    function test_klaytnToEthereum_inNUSD_outUSDC() public {
+        ChainSetup memory origin = chains[ETH_CHAINID];
+        ChainSetup memory destination = chains[KLAY_CHAINID];
+
+        // Step 1: both nUSD and USDC are available as intermediate token
+        klaytnToEthereum_inNUSD_outUSDC({checkNUSD: false, checkUSDC: false});
+
+        // Step 2: only USDC is available  as intermediate token
+        // Remove nUSD pool on origin from the list of pools
+        origin.quoter.removePool(origin.nUsdPool);
+        klaytnToEthereum_inNUSD_outUSDC({checkNUSD: false, checkUSDC: true});
+
+        // Step 3: no tokens are available
+        // Remove USDC as bridge asset from destination router
+        destination.router.removeToken(address(destination.usdc));
+        vm.expectRevert("No path found on origin");
+        // Doing an external call to catch the revert from the test suite
+        this.klaytnToEthereum_inNUSD_outUSDC({checkNUSD: false, checkUSDC: false});
+
+        // Step 4: only nUSD is available  as intermediate token
+        // Add nUSD pool on origin back to the list of pools
+        origin.quoter.addPool(origin.nUsdPool);
+        klaytnToEthereum_inNUSD_outUSDC({checkNUSD: true, checkUSDC: false});
+    }
+
+    function klaytnToEthereum_inNUSD_outUSDC(bool checkNUSD, bool checkUSDC) public {
+        // Prepare test parameters: Klaytn nUSD -> Ethereum USDC
+        ChainSetup memory origin = chains[KLAY_CHAINID];
+        ChainSetup memory destination = chains[ETH_CHAINID];
+        IERC20 tokenIn = origin.nusd;
+        IERC20 tokenOut = destination.usdc;
+        uint256 amountIn = 10**uint256(ERC20(address(tokenIn)).decimals());
+        (SwapQuery memory originQuery, SwapQuery memory destQuery) = performQuoteCalls(
+            origin,
+            destination,
+            tokenIn,
+            tokenOut,
+            amountIn
+        );
+        // Could use either nUSD or USDC as the bridge token on Klaytn
+        address bridgeTokenOrigin = originQuery.tokenOut;
+        if (checkUSDC) require(bridgeTokenOrigin == address(origin.usdc), "!USDC");
+        if (checkNUSD) require(bridgeTokenOrigin == address(origin.nusd), "!NUSD");
+        address bridgeTokenDest;
+        if (bridgeTokenOrigin == address(origin.usdc)) {
+            // Klaytn: nUSD -> USDC; Ethereum: USDC -> USDC
+            // Expect Bridge event to be emitted
+            vm.expectEmit(true, true, true, true);
+            emit TokenRedeem(TO, ETH_CHAINID, bridgeTokenOrigin, originQuery.minAmountOut);
+            bridgeTokenDest = address(destination.usdc);
+        } else {
+            // Klaytn: nUSD -> nUSD; Ethereum: nUSD -> USDC
+            vm.expectEmit(true, true, true, true);
+            emit TokenRedeemAndRemove({
+                to: TO,
+                chainId: ETH_CHAINID,
+                token: bridgeTokenOrigin,
+                amount: originQuery.minAmountOut,
+                swapTokenIndex: 1, // USDC index is 1
+                swapMinAmount: destQuery.minAmountOut,
+                swapDeadline: destQuery.deadline
+            });
+            bridgeTokenDest = address(destination.nusd);
+        }
+        // User interacts with the SynapseRouter on origin chain
+        initiateBridgeTx(origin, destination, tokenIn, tokenOut, amountIn);
+        // Validator completes the transaction on destination chain
+        checkCompletedBridgeTx(destination, bridgeTokenDest, originQuery, destQuery);
+    }
 }
