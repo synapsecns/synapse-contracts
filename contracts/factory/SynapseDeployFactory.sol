1b15c385d6d6e16ccf421a1be93ebd9728adc170
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-01-18 11:07:22 +0000

    Deploy Clone logic

--- contracts/factory/SynapseDeployFactory.sol
+++ contracts/factory/SynapseDeployFactory.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.17;
 
 import {ISynapseDeployFactory} from "./interfaces/ISynapseDeployFactory.sol";
 
+import {Address} from "@openzeppelin/contracts-4.5.0/utils/Address.sol";
+import {Clones} from "@openzeppelin/contracts-4.5.0/proxy/Clones.sol";
 import {CREATE3} from "solmate/utils/CREATE3.sol";
 
 /// @title Factory for deploying contracts to deterministic addresses via CREATE3
@@ -11,6 +13,8 @@ import {CREATE3} from "solmate/utils/CREATE3.sol";
 /// its own namespace for deployed addresses.
 /// Modified by the Synapse contributors to enable deploying minimal proxies (clones) in a similar fashion.
 contract SynapseDeployFactory is ISynapseDeployFactory {
+    using Address for address;
+
     function deploy(bytes32 salt, bytes memory creationCode) external payable override returns (address deployed) {
         // Use salt that is unique for every deployer
         salt = _deployerSalt(msg.sender, salt);
@@ -21,7 +25,17 @@ contract SynapseDeployFactory is ISynapseDeployFactory {
         bytes32 salt,
         address master,
         bytes calldata initData
-    ) external returns (address deployed) {}
+    ) external returns (address deployed) {
+        // Use salt that is unique for every deployer
+        salt = _deployerSalt(msg.sender, salt);
+        // Setup a minimal proxy, using the master implementation
+        deployed = Clones.cloneDeterministic(master, salt);
+        // Do the initializer call, if requested
+        if (initData.length != 0) {
+            // This will bubble up the revert, if it happens during the function call
+            deployed.functionCall(initData);
+        }
+    }
 
     /*╔══════════════════════════════════════════════════════════════════════╗*\
     ▏*║                        PREDICT ADDRESS VIEWS                         ║*▕
@@ -37,7 +51,11 @@ contract SynapseDeployFactory is ISynapseDeployFactory {
         address deployer,
         bytes32 salt,
         address master
-    ) external view returns (address deployed) {}
+    ) external view returns (address deployed) {
+        // Use salt that is unique for every deployer
+        salt = _deployerSalt(deployer, salt);
+        return Clones.predictDeterministicAddress(master, salt);
+    }
 
     /*╔══════════════════════════════════════════════════════════════════════╗*\
     ▏*║                           INTERNAL HELPERS                           ║*▕
