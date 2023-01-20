91142978966b3107a76d845da800479f185e485b
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-01-20 14:01:46 +0000

    Make Router deploy script slightly more readable

--- script/router/DeployRouter.s.sol
+++ script/router/DeployRouter.s.sol
@@ -33,68 +33,105 @@ contract DeployRouterScript is BaseScript {
 
     function execute(bool _isBroadcasted) public override {
         string memory config = loadDeployConfig(ROUTER);
-        address bridge = config.readAddress("bridge");
-        address wgas = config.readAddress("wgas");
-        address[] memory pools = config.readAddressArray("pools");
-        string[] memory ids = config.readStringArray("ids");
-        console.log("Bridge: %s", bridge);
-        console.log("WGAS: %s", wgas);
-        console.log("Pools: %s", pools.length);
-        for (uint256 i = 0; i < pools.length; ++i) {
-            console.log(pools[i]);
-        }
-        console.log("Tokens: %s", ids.length);
-
         startBroadcast(_isBroadcasted);
-        SynapseRouter router;
-        address routerDeployment = tryLoadDeployment(ROUTER);
-        if (routerDeployment == address(0)) {
-            router = new SynapseRouter(bridge);
-            saveDeployment(ROUTER, address(router));
-            LocalBridgeConfig.BridgeTokenConfig[] memory tokens = new LocalBridgeConfig.BridgeTokenConfig[](ids.length);
-            for (uint256 i = 0; i < ids.length; ++i) {
-                bytes memory rawConfig = config.parseRaw(_concat("tokens.", ids[i]));
-                TokenConfig memory tokenConfig = abi.decode(rawConfig, (TokenConfig));
-                console.log("Adding %s: %s", ids[i], tokenConfig.tokenType == 0 ? "Redeem" : "Deposit");
-                console.log("Address: %s", tokenConfig.token);
-                if (tokenConfig.token != tokenConfig.bridgeToken) {
-                    console.log("Wrapper: %s", tokenConfig.bridgeToken);
-                }
-                tokens[i] = LocalBridgeConfig.BridgeTokenConfig({
-                    id: ids[i],
-                    token: tokenConfig.token,
-                    tokenType: LocalBridgeConfig.TokenType(tokenConfig.tokenType),
-                    bridgeToken: tokenConfig.bridgeToken,
-                    bridgeFee: tokenConfig.bridgeFee,
-                    minFee: uint256(tokenConfig.minFee),
-                    maxFee: uint256(tokenConfig.maxFee)
-                });
-            }
-            router.addTokens(tokens);
-        } else {
-            console.log("Skipping %s, deployed at %s", ROUTER, routerDeployment);
-            router = SynapseRouter(payable(routerDeployment));
-        }
-
-        SwapQuoter quoter;
-        address quoterDeployment = tryLoadDeployment(QUOTER);
-        if (quoterDeployment == address(0)) {
-            quoter = new SwapQuoter(address(router), address(wgas));
-            saveDeployment(QUOTER, address(quoter));
-            quoter.addPools(pools);
-            console.log("Pools added");
-        } else {
-            console.log("Skipping %s, deployed at %s", QUOTER, quoterDeployment);
-            quoter = SwapQuoter(quoterDeployment);
-        }
-
+        SynapseRouter router = _deploySetupRouter(config);
+        SwapQuoter quoter = _deploySetupQuoter(config, router);
+        // Check if Swap Quoter is setup correctly
         if (router.swapQuoter() != quoter) {
             router.setSwapQuoter(quoter);
             console.log("%s set to %s", QUOTER, address(quoter));
         } else {
             console.log("%s already set up", QUOTER);
         }
-
         stopBroadcast();
+    }
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                           INTERNAL: ROUTER                           ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    /// @dev Deploys and configures SynapseRouter.
+    function _deploySetupRouter(string memory config) internal returns (SynapseRouter router) {
+        // Read deploy params from the config
+        address bridge = config.readAddress("bridge");
+        console.log("Bridge: %s", bridge);
+        // Check if the deployment already exists
+        address routerDeployment = tryLoadDeployment(ROUTER);
+        if (routerDeployment == address(0)) {
+            router = _deployRouter(bridge);
+            _setupRouter(config, router);
+        } else {
+            console.log("Skipping %s, deployed at %s", ROUTER, routerDeployment);
+            router = SynapseRouter(payable(routerDeployment));
+        }
+    }
+
+    /// @dev Deploys SynapseRouter. Function is virtual to allow different deploy workflows.
+    function _deployRouter(address bridge) internal virtual returns (SynapseRouter router) {
+        router = new SynapseRouter(bridge);
+        saveDeployment(ROUTER, address(router));
+    }
+
+    /// @dev Configures SynapseRouter by adding all chain's bridge tokens.
+    function _setupRouter(string memory config, SynapseRouter router) internal {
+        string[] memory ids = config.readStringArray("ids");
+        console.log("Tokens: %s", ids.length);
+        LocalBridgeConfig.BridgeTokenConfig[] memory tokens = new LocalBridgeConfig.BridgeTokenConfig[](ids.length);
+        for (uint256 i = 0; i < ids.length; ++i) {
+            bytes memory rawConfig = config.parseRaw(_concat("tokens.", ids[i]));
+            TokenConfig memory tokenConfig = abi.decode(rawConfig, (TokenConfig));
+            console.log("Adding %s: %s", ids[i], tokenConfig.tokenType == 0 ? "Redeem" : "Deposit");
+            console.log("Address: %s", tokenConfig.token);
+            if (tokenConfig.token != tokenConfig.bridgeToken) {
+                console.log("Wrapper: %s", tokenConfig.bridgeToken);
+            }
+            tokens[i] = LocalBridgeConfig.BridgeTokenConfig({
+                id: ids[i],
+                token: tokenConfig.token,
+                tokenType: LocalBridgeConfig.TokenType(tokenConfig.tokenType),
+                bridgeToken: tokenConfig.bridgeToken,
+                bridgeFee: tokenConfig.bridgeFee,
+                minFee: uint256(tokenConfig.minFee),
+                maxFee: uint256(tokenConfig.maxFee)
+            });
+        }
+        router.addTokens(tokens);
+    }
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                           INTERNAL: QUOTER                           ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    /// @dev Deploys and configures SwapQuoter.
+    function _deploySetupQuoter(string memory config, SynapseRouter router) internal returns (SwapQuoter quoter) {
+        // Read deploy params from the config
+        address wgas = config.readAddress("wgas");
+        console.log("WGAS: %s", wgas);
+        // Check if deployment already exists
+        address quoterDeployment = tryLoadDeployment(QUOTER);
+        if (quoterDeployment == address(0)) {
+            quoter = _deployQuoter(router, wgas);
+            _setupQuoter(config, quoter);
+        } else {
+            console.log("Skipping %s, deployed at %s", QUOTER, quoterDeployment);
+            quoter = SwapQuoter(quoterDeployment);
+        }
+    }
+
+    /// @dev Deploys SwapQuoter. Function is virtual to allow different deploy workflows.
+    function _deployQuoter(SynapseRouter router, address wgas) internal virtual returns (SwapQuoter quoter) {
+        quoter = new SwapQuoter(address(router), address(wgas));
+        saveDeployment(QUOTER, address(quoter));
+    }
+
+    /// @dev Configures SwapQuoter by adding all chain's liquidity pools.
+    function _setupQuoter(string memory config, SwapQuoter quoter) internal {
+        address[] memory pools = config.readAddressArray("pools");
+        console.log("Pools: %s", pools.length);
+        for (uint256 i = 0; i < pools.length; ++i) {
+            console.log(pools[i]);
+        }
+        quoter.addPools(pools);
+        console.log("Pools added");
     }
 }
