c5a51443454275abd552a1c57e084f2f1ec747fa
χ² <88190723+ChiTimesChi@users.noreply.github.com>
2023-01-22 15:49:21 +0300

    Synapse CREATE3 factory (#212)
    * forge install: solmate
    
    * initial factory commit
    
    * Deploy Clone logic
    
    * Add docs
    
    * Tests for Synapse Deploy Factory
    
    * Checkout with recursive submodules in CI
    
    * install hardhat-preprocessor
    
    * Add preprocess to hardhat config
    
    * Test for deployClone bubbling revert
    
    * Revert "Add preprocess to hardhat config"
    
    This reverts commit 4b9b869b57dd6eff8026f6aa61e0cfa18ba17e64.
    
    * Revert "install hardhat-preprocessor"
    
    This reverts commit 00a914bcbed1e65240ee47ed3c0faefb836140d1.
    
    * Rm prod remappings to make solidity-coverage happy

---
+++ contracts/factory/SynapseDeployFactory.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.17;
+
+import {ISynapseDeployFactory} from "./interfaces/ISynapseDeployFactory.sol";
+
+import {Address} from "@openzeppelin/contracts-4.5.0/utils/Address.sol";
+import {Clones} from "@openzeppelin/contracts-4.5.0/proxy/Clones.sol";
+import {CREATE3} from "../../lib/solmate/src/utils/CREATE3.sol";
+
+/// @title Factory for deploying contracts to deterministic addresses via CREATE3
+/// @author zefram.eth
+/// @notice Enables deploying contracts using CREATE3. Each deployer (msg.sender) has
+/// its own namespace for deployed addresses.
+/// Modified by the Synapse contributors to enable deploying minimal proxies (clones) in a similar fashion.
+contract SynapseDeployFactory is ISynapseDeployFactory {
+    using Address for address;
+
+    /// @inheritdoc ISynapseDeployFactory
+    function deploy(bytes32 salt, bytes memory creationCode) external payable override returns (address deployed) {
+        // Use salt that is unique for every deployer
+        salt = _deployerSalt(msg.sender, salt);
+        return CREATE3.deploy(salt, creationCode, msg.value);
+    }
+
+    /// @inheritdoc ISynapseDeployFactory
+    function deployClone(
+        bytes32 salt,
+        address master,
+        bytes calldata initData
+    ) external returns (address deployed) {
+        // Use salt that is unique for every deployer
+        salt = _deployerSalt(msg.sender, salt);
+        // Setup a minimal proxy, using the master implementation
+        deployed = Clones.cloneDeterministic(master, salt);
+        // Do the initializer call, if requested
+        if (initData.length != 0) {
+            // This will bubble up the revert, if it happens during the function call
+            deployed.functionCall(initData);
+        }
+    }
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                        PREDICT ADDRESS VIEWS                         ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    /// @inheritdoc ISynapseDeployFactory
+    function predictAddress(address deployer, bytes32 salt) external view override returns (address deployed) {
+        // Use salt that is unique for every deployer
+        salt = _deployerSalt(deployer, salt);
+        return CREATE3.getDeployed(salt);
+    }
+
+    /// @inheritdoc ISynapseDeployFactory
+    function predictCloneAddress(
+        address deployer,
+        bytes32 salt,
+        address master
+    ) external view returns (address deployed) {
+        // Use salt that is unique for every deployer
+        salt = _deployerSalt(deployer, salt);
+        return Clones.predictDeterministicAddress(master, salt);
+    }
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                           INTERNAL HELPERS                           ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    /// @dev Returns a unique salt for every (deployer, salt) tuple.
+    function _deployerSalt(address deployer, bytes32 salt) internal pure returns (bytes32 deployerSalt) {
+        // hash salt with the deployer address to give each deployer its own namespace
+        return keccak256(abi.encodePacked(deployer, salt));
+    }
+}
---
+++ test/factory/SynapseDeployFactory.t.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+
+import {SynapseDeployFactory} from "../../contracts/factory/SynapseDeployFactory.sol";
+
+import "@openzeppelin/contracts-4.5.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-4.5.0/access/AccessControl.sol";
+import "@openzeppelin/contracts-4.5.0/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts-upgradeable-4.5.0/token/ERC20/ERC20Upgradeable.sol";
+
+interface ISynapseERC20 {
+    function initialize(
+        string memory name,
+        string memory symbol,
+        uint8 decimals,
+        address owner
+    ) external;
+}
+
+contract ImplementationMock {
+    function initialize() external pure {
+        revert("Gm. This is a revert.");
+    }
+}
+
+// solhint-disable func-name-mixedcase
+contract SynapseDeployFactoryTest is Test {
+    SynapseDeployFactory internal factory;
+    address internal synapseERC20;
+
+    struct SynapseERC20Params {
+        string name;
+        string symbol;
+        uint8 decimals;
+        address owner;
+    }
+
+    function setUp() public {
+        factory = new SynapseDeployFactory();
+        synapseERC20 = deployCode("SynapseERC20.sol");
+    }
+
+    function test_deploy(
+        address deployer,
+        bytes32 salt,
+        string memory name,
+        string memory symbol
+    ) public {
+        vm.assume(deployer != address(0));
+        // We're deploying ERC20(name, symbol) to a predetermined address
+        address predicted = factory.predictAddress(deployer, salt);
+        bytes memory args = abi.encode(name, symbol);
+        // Simulate a deploy call from the deployer
+        vm.prank(deployer);
+        address deployment = factory.deploy(salt, abi.encodePacked(type(ERC20).creationCode, args));
+        // Check deployment address and correctness of constructor args
+        assertEq(deployment, predicted, "Predicted address wrong");
+        ERC20 token = ERC20(deployment);
+        assertEq(token.name(), name, "Wrong name");
+        assertEq(token.symbol(), symbol, "Wrong symbol");
+    }
+
+    function test_deploySynapseERC20(
+        address deployer,
+        bytes32 salt,
+        SynapseERC20Params memory params
+    ) public {
+        vm.assume(deployer != address(0));
+        // We're deploying SynapseERC20 to a predetermined address
+        // And calling initialize(name, symbol, decimals, owner)
+        address predicted = factory.predictCloneAddress(deployer, salt, synapseERC20);
+        bytes memory initData = abi.encodeWithSignature(
+            "initialize(string,string,uint8,address)",
+            params.name,
+            params.symbol,
+            params.decimals,
+            params.owner
+        );
+        // Simulate a deploy call from the deployer
+        vm.prank(deployer);
+        address deployment = factory.deployClone(salt, synapseERC20, initData);
+        // Check deployment address and correctness of initializer args
+        assertEq(deployment, predicted, "Predicted address wrong");
+        ERC20 token = ERC20(deployment);
+        assertEq(token.name(), params.name, "Wrong name");
+        assertEq(token.symbol(), params.symbol, "Wrong symbol");
+        assertEq(token.decimals(), params.decimals, "Wrong decimals");
+        assertTrue(AccessControl(deployment).hasRole(0x00, params.owner), "Default admin role not setup");
+    }
+
+    function test_deployTransparentUpgradeableProxy(
+        address deployer,
+        bytes32 adminSalt,
+        bytes32 proxySalt,
+        address adminOwner,
+        SynapseERC20Params memory params
+    ) public {
+        vm.assume(deployer != address(0));
+        vm.assume(adminOwner != address(0));
+        vm.assume(params.owner != address(0));
+        vm.assume(adminSalt != proxySalt);
+
+        address predictedAdmin = factory.predictAddress(deployer, adminSalt);
+        bytes memory adminArgs = abi.encode(adminOwner);
+        // Simulate a deploy call from the deployer
+        vm.prank(deployer);
+        address deploymentAdmin = factory.deploy(
+            adminSalt,
+            abi.encodePacked(vm.getCode("FactoryProxyAdmin.sol"), adminArgs)
+        );
+        // Check deployment address and correctness of constructor args
+        assertEq(deploymentAdmin, predictedAdmin, "Predicted admin address wrong");
+        assertEq(Ownable(deploymentAdmin).owner(), adminOwner, "Admin owner wrong");
+
+        address predictedProxy = factory.predictAddress(deployer, proxySalt);
+        bytes memory initData = abi.encodeWithSignature(
+            "initialize(string,string,uint8,address)",
+            params.name,
+            params.symbol,
+            params.decimals,
+            params.owner
+        );
+        bytes memory proxyArgs = abi.encode(synapseERC20, deploymentAdmin, initData);
+        // Simulate a deploy call from the deployer
+        vm.prank(deployer);
+        // address deploymentProxy = address(new TransparentUpgradeableProxy(synapseERC20, deploymentAdmin, initData));
+        address deploymentProxy = factory.deploy(
+            proxySalt,
+            abi.encodePacked(type(TransparentUpgradeableProxy).creationCode, proxyArgs)
+        );
+        // Check deployment address and correctness of initializer args
+        assertEq(deploymentProxy, predictedProxy, "Predicted proxy address wrong");
+        ERC20 token = ERC20(deploymentProxy);
+        assertEq(token.name(), params.name, "Wrong name");
+        assertEq(token.symbol(), params.symbol, "Wrong symbol");
+        assertEq(token.decimals(), params.decimals, "Wrong decimals");
+        assertTrue(AccessControl(deploymentProxy).hasRole(0x00, params.owner), "Default admin role not setup");
+
+        // Check that proxy admin can upgrade the implementation
+        TransparentUpgradeableProxy proxy = TransparentUpgradeableProxy(payable(deploymentProxy));
+        ProxyAdmin admin = ProxyAdmin(deploymentAdmin);
+        assertEq(admin.getProxyImplementation(proxy), synapseERC20, "Wrong initial implementation");
+        address newImpl = deployCode("SynapseERC20.sol");
+        vm.prank(adminOwner);
+        admin.upgrade(proxy, newImpl);
+        assertEq(admin.getProxyImplementation(proxy), newImpl, "Wrong new implementation");
+    }
+
+    function test_deployClone_revert() public {
+        address master = address(new ImplementationMock());
+        bytes memory initData = abi.encodePacked(ImplementationMock.initialize.selector);
+        vm.expectRevert("Gm. This is a revert.");
+        factory.deployClone(bytes32(0), master, initData);
+    }
+}
