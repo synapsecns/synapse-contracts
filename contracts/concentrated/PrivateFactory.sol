d5943b32a658546ca16f97a843acb2142399177a
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2023-05-02 14:46:54 -0400

    fix order tokens on private factory

--- contracts/concentrated/PrivateFactory.sol
+++ contracts/concentrated/PrivateFactory.sol
@@ -1,35 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import {IAccessControl} from "@openzeppelin/contracts-4.8.0/access/IAccessControl.sol";
 import {Ownable} from "@openzeppelin/contracts-4.8.0/access/Ownable.sol";
 import {PrivatePool} from "./PrivatePool.sol";
 
 /// @title Private factory for concentrated liquidity
 /// @notice Deploys individual private pools owned by LPs
 contract PrivateFactory is Ownable {
-    address public immutable registry;
+    bytes32 internal constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+    address public immutable bridge;
     mapping(address => mapping(address => mapping(address => address))) public pool;
-
-    modifier onlySupportedToken(address token) {
-        // TODO: query synX registry
-        // TOD: require();
-        _;
-    }
 
     event Deploy(address indexed lp, address token0, address token1);
 
-    constructor(address _registry) {
-        registry = _registry;
+    constructor(address _bridge) {
+        bridge = _bridge;
     }
 
     function orderTokens(address tokenA, address tokenB) public returns (address token0_, address token1_) {
-        // TODO: token0 (base) should be set as synX and token1 should be set as X
-        token0_ = tokenA;
-        token1_ = tokenB;
+        // token0 (base) should be set as synX and token1 should be set as X
+        // check synapse bridge has minter role on token to determine which is synX
+        try IAccessControl(tokenA).hasRole(MINTER_ROLE, bridge) returns (bool has_) {
+            token0_ = has_ ? tokenA : tokenB;
+            token1_ = has_ ? tokenB : tokenA;
+        } catch {
+            token0_ = tokenB;
+            token1_ = tokenA;
+        }
     }
 
-    function deploy(address tokenA, address tokenB) external onlySupportedToken(tokenA) onlySupportedToken(tokenB) {
+    function deploy(address tokenA, address tokenB) external {
         require(tokenA != tokenB, "same token for base and quote");
         require(pool[msg.sender][tokenA][tokenB] == address(0), "pool already exists");
 
