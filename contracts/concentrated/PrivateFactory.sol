95e4da0b9838569032c09ae927bf9332c87f5e1c
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2023-05-03 09:52:03 -0400

    private factory interface

--- contracts/concentrated/PrivateFactory.sol
+++ contracts/concentrated/PrivateFactory.sol
@@ -3,11 +3,13 @@ pragma solidity 0.8.17;
 
 import {IAccessControl} from "@openzeppelin/contracts-4.8.0/access/IAccessControl.sol";
 import {Ownable} from "@openzeppelin/contracts-4.8.0/access/Ownable.sol";
+
+import {IPrivateFactory} from "./interfaces/IPrivateFactory.sol";
 import {PrivatePool} from "./PrivatePool.sol";
 
 /// @title Private factory for concentrated liquidity
 /// @notice Deploys individual private pools owned by LPs
-contract PrivateFactory is Ownable {
+contract PrivateFactory is IPrivateFactory, Ownable {
     bytes32 internal constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     address public immutable bridge;
@@ -19,7 +21,7 @@ contract PrivateFactory is Ownable {
         bridge = _bridge;
     }
 
-    function orderTokens(address tokenA, address tokenB) public returns (address token0_, address token1_) {
+    function orderTokens(address tokenA, address tokenB) public view returns (address token0_, address token1_) {
         // token0 (base) should be set as synX and token1 should be set as X
         // check synapse bridge has minter role on token to determine which is synX
         try IAccessControl(tokenA).hasRole(MINTER_ROLE, bridge) returns (bool has_) {
---
+++ contracts/concentrated/interfaces/IPrivateFactory.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+/// @title Private factory for concentrated liquidity
+/// @notice Deploys individual private pools owned by LPs
+interface IPrivateFactory {
+    function orderTokens(address tokenA, address tokenB) external view returns (address, address);
+
+    function deploy(address tokenA, address tokenB) external;
+}
