606d87b87769dd53e539ddc83223636af58ebef4
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2023-05-11 08:37:12 -0400

    fix private factory interface

--- contracts/concentrated/interfaces/IPrivateFactory.sol
+++ contracts/concentrated/interfaces/IPrivateFactory.sol
@@ -4,6 +4,14 @@ pragma solidity 0.8.17;
 /// @title Private factory for concentrated liquidity
 /// @notice Deploys individual private pools owned by LPs
 interface IPrivateFactory {
+    function bridge() external view returns (address);
+
+    function pool(
+        address lp,
+        address tokenA,
+        address tokenB
+    ) external view returns (address);
+
     function orderTokens(address tokenA, address tokenB) external view returns (address, address);
 
     function deploy(address tokenA, address tokenB) external returns (address);
