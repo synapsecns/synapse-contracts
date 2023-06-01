c8962d3c126563f9c826d224c5a6bfce7db860f5
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-06-01 21:27:16 +0100

    First draft for Synapse CCTP interface/events

---
+++ contracts/cctp/events/SynapseCCTPEvents.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+abstract contract SynapseCCTPEvents {
+    // TODO: figure out what we need to emit for the Explorer
+
+    /// @notice Emitted when a Circle token is sent with an attached action request.
+    /// @dev To fulfill the request, the validator needs to fetch `message` from `MessageSent` event
+    /// emitted by Circle's MessageTransmitter in the same tx, then fetch `signature` for the message from Circle API.
+    /// All this data will need to be presented to SynapseCCTP on the destination domain.
+    /// @param destinationDomain    Domain of destination chain
+    /// @param request              Request for the action to take on the destination domain
+    /// @param recipient            End recipient of the tokens on the destination domain
+    /// @param kappa                Unique identifier of the request
+    event CircleRequestSent(uint32 destinationDomain, bytes request, address indexed recipient, bytes32 indexed kappa);
+
+    /// @notice Emitted when a Circle token is received with an attached action request.
+    /// @param recipient            End recipient of the tokens on this domain
+    /// @param token                Address of Circle token received on this domain
+    /// @param amount               Amount of tokens received
+    /// @param fee                  Fee paid for fulfilling the request
+    /// @param kappa                Unique identifier of the request
+    event CircleRequestFulfilled(
+        address indexed recipient,
+        address token,
+        uint256 amount,
+        uint256 fee,
+        bytes32 indexed kappa
+    );
+}
---
+++ contracts/cctp/interfaces/ISynapseCCTP.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface ISynapseCCTP {
+    /// @notice Send a Circle token supported by CCTP to a given domain
+    /// with the request for the action to take on the destination domain.
+    /// @dev The request is a bytes array containing information about the end recipient of the tokens,
+    /// as well as an optional swap action to take on the destination domain.
+    /// @param destinationDomain    Domain of destination chain
+    /// @param burnToken            Address of Circle token to burn
+    /// @param amount               Amount of tokens to burn
+    /// @param request              Request for the action to take on the destination domain
+    function sendCircleToken(
+        uint32 destinationDomain,
+        address burnToken,
+        uint256 amount,
+        bytes memory request
+    ) external;
+
+    /// @notice Receive  Circle token supported by CCTP with the request for the action to take.
+    /// @dev The request is a bytes array containing information about the end recipient of the tokens,
+    /// as well as an optional swap action to take on this domain.
+    /// @param message              Message raw bytes emitted by CCTP MessageTransmitter on origin domain
+    /// @param signature            Circle's attestation for the message obtained from Circle's API
+    /// @param request              Request for the action to take on this domain
+    function receiveCircleToken(
+        bytes calldata message,
+        bytes calldata signature,
+        bytes memory request
+    ) external;
+}
