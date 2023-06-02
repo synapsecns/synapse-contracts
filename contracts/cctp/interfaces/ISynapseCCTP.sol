6a7ba8d95919276d08732e8d81a93778bd7f5f06
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-06-02 10:31:22 +0100

    Add request versioning

--- contracts/cctp/interfaces/ISynapseCCTP.sol
+++ contracts/cctp/interfaces/ISynapseCCTP.sol
@@ -9,11 +9,13 @@ interface ISynapseCCTP {
     /// @param destinationDomain    Domain of destination chain
     /// @param burnToken            Address of Circle token to burn
     /// @param amount               Amount of tokens to burn
+    /// @param requestVersion       Version of the request format
     /// @param request              Request for the action to take on the destination domain
     function sendCircleToken(
         uint32 destinationDomain,
         address burnToken,
         uint256 amount,
+        uint32 requestVersion,
         bytes memory request
     ) external;
 
@@ -22,10 +24,12 @@ interface ISynapseCCTP {
     /// as well as an optional swap action to take on this domain.
     /// @param message              Message raw bytes emitted by CCTP MessageTransmitter on origin domain
     /// @param signature            Circle's attestation for the message obtained from Circle's API
+    /// @param requestVersion       Version of the request format
     /// @param request              Request for the action to take on this domain
     function receiveCircleToken(
         bytes calldata message,
         bytes calldata signature,
+        uint32 requestVersion,
         bytes memory request
     ) external;
 }
