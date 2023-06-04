b8463dfb778a4daa3f02522ba32df0b7b9bb21a2
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-06-04 23:21:47 +0100

    Fix: check for empty token

--- contracts/cctp/libs/Errors.sol
+++ contracts/cctp/libs/Errors.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.17;
 error IncorrectRequestLength();
 error UnknownRequestVersion();
 
+error LocalCCTPTokenNotFound();
 error RemoteCCTPDeploymentNotSet();
 error RemoteCCTPTokenNotSet();
 
