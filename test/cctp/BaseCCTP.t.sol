e663aa4579f79a14d215cd982a1372899763b024
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-06-04 22:42:47 +0100

    Getters for expected test data

--- test/cctp/BaseCCTP.t.sol
+++ test/cctp/BaseCCTP.t.sol
@@ -6,11 +6,15 @@ import {MockMintBurnToken} from "./mocks/MockMintBurnToken.sol";
 import {MockTokenMessenger} from "./mocks/MockTokenMessenger.sol";
 import {MockTokenMinter} from "./mocks/MockTokenMinter.sol";
 
-import {SynapseCCTP} from "../../contracts/cctp/SynapseCCTP.sol";
+import {MessageTransmitterEvents} from "../../contracts/cctp/events/MessageTransmitterEvents.sol";
+import {TokenMessengerEvents} from "../../contracts/cctp/events/TokenMessengerEvents.sol";
+import {MinimalForwarderLib} from "../../contracts/cctp/libs/MinimalForwarder.sol";
+import {RequestLib} from "../../contracts/cctp/libs/Request.sol";
+import {SynapseCCTPEvents, SynapseCCTP} from "../../contracts/cctp/SynapseCCTP.sol";
 
 import {Test} from "forge-std/Test.sol";
 
-abstract contract BaseCCTPTest is Test {
+abstract contract BaseCCTPTest is MessageTransmitterEvents, TokenMessengerEvents, SynapseCCTPEvents, Test {
     struct CCTPSetup {
         MockMessageTransmitter messageTransmitter;
         MockMintBurnToken mintBurnToken;
@@ -24,12 +28,17 @@ abstract contract BaseCCTPTest is Test {
     mapping(uint32 => CCTPSetup) public cctpSetups;
     mapping(uint32 => SynapseCCTP) public synapseCCTPs;
 
+    address public user;
+    address public recipient;
+
     function setUp() public virtual {
         deployCCTP(DOMAIN_ETH);
         deployCCTP(DOMAIN_AVAX);
         linkDomains(DOMAIN_ETH, DOMAIN_AVAX);
         deploySynapseCCTP(DOMAIN_ETH);
         deploySynapseCCTP(DOMAIN_AVAX);
+        user = makeAddr("User");
+        recipient = makeAddr("Recipient");
     }
 
     function deployCCTP(uint32 domain) public returns (CCTPSetup memory setup) {
@@ -70,6 +79,86 @@ abstract contract BaseCCTPTest is Test {
             remoteDomain: domainA,
             remoteToken: bytes32(uint256(uint160(address(setupA.mintBurnToken)))),
             localToken: address(setupB.mintBurnToken)
+        });
+    }
+
+    function getExpectedMessage(
+        uint32 originDomain,
+        uint32 destinationDomain,
+        address finalRecipient,
+        address originBurnToken,
+        uint256 amount,
+        bytes32 destinationCaller
+    ) public view returns (bytes memory expectedMessage) {
+        bytes32 remoteSynapseCCTP = bytes32(uint256(uint160(address(synapseCCTPs[destinationDomain]))));
+        bytes memory messageBody = cctpSetups[originDomain].tokenMessenger.formatTokenMessage({
+            amount: amount,
+            mintRecipient: remoteSynapseCCTP,
+            burnToken: originBurnToken
+        });
+        expectedMessage = cctpSetups[originDomain].messageTransmitter.formatMessage({
+            remoteDomain: originDomain,
+            sender: address(cctpSetups[originDomain].tokenMessenger),
+            recipient: finalRecipient,
+            destinationCaller: destinationCaller,
+            messageBody: messageBody
+        });
+    }
+
+    function getExpectedKappa(
+        uint32 originDomain,
+        uint32 destinationDomain,
+        address finalRecipient,
+        address originBurnToken,
+        uint256 amount,
+        uint32 requestVersion,
+        bytes memory swapParams
+    ) public view returns (bytes32 kappa) {
+        uint64 nonce = cctpSetups[originDomain].messageTransmitter.nextAvailableNonce();
+        bytes memory formattedRequest = RequestLib.formatRequest({
+            requestVersion: requestVersion,
+            baseRequest: RequestLib.formatBaseRequest({
+                originDomain: originDomain,
+                nonce: nonce,
+                originBurnToken: originBurnToken,
+                amount: amount,
+                recipient: finalRecipient
+            }),
+            swapParams: swapParams
+        });
+        bytes32 requestHash = keccak256(formattedRequest);
+        uint256 prefix = uint256(destinationDomain) * 2**32 + requestVersion;
+        kappa = keccak256(abi.encodePacked(prefix, requestHash));
+    }
+
+    function getExpectedDstCaller(uint32 destinationDomain, bytes32 kappa)
+        public
+        view
+        returns (bytes32 destinationCaller)
+    {
+        address dstCaller = MinimalForwarderLib.predictAddress({
+            deployer: address(synapseCCTPs[destinationDomain]),
+            salt: kappa
+        });
+        destinationCaller = bytes32(uint256(uint160(dstCaller)));
+    }
+
+    function getExpectedRequest(
+        uint32 originDomain,
+        uint256 amount,
+        uint32 requestVersion,
+        bytes memory swapParams
+    ) public view returns (bytes memory expectedRequest) {
+        expectedRequest = RequestLib.formatRequest({
+            requestVersion: requestVersion,
+            baseRequest: RequestLib.formatBaseRequest({
+                originDomain: originDomain,
+                nonce: cctpSetups[originDomain].messageTransmitter.nextAvailableNonce(),
+                originBurnToken: address(cctpSetups[DOMAIN_ETH].mintBurnToken),
+                amount: amount,
+                recipient: recipient
+            }),
+            swapParams: swapParams
         });
     }
 }
