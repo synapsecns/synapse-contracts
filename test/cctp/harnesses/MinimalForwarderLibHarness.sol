8f19a1b16e11e8301dbfa2aa2324bff0706392d1
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-06-12 12:34:29 +0100

    Enable coverage for MinimalForwarder

--- test/cctp/harnesses/MinimalForwarderLibHarness.sol
+++ test/cctp/harnesses/MinimalForwarderLibHarness.sol
@@ -4,27 +4,31 @@ pragma solidity 0.8.17;
 import {MinimalForwarderLib} from "../../../contracts/cctp/libs/MinimalForwarder.sol";
 
 contract MinimalForwarderLibHarness {
-    function deploy(bytes32 salt) public returns (address forwarder) {
-        forwarder = MinimalForwarderLib.deploy(salt);
+    function deploy(bytes32 salt) public returns (address) {
+        address forwarder = MinimalForwarderLib.deploy(salt);
+        return forwarder;
     }
 
     function forwardCall(
         address forwarder,
         address target,
         bytes memory payload
-    ) public returns (bytes memory returnData) {
-        returnData = MinimalForwarderLib.forwardCall(forwarder, target, payload);
+    ) public returns (bytes memory) {
+        bytes memory returnData = MinimalForwarderLib.forwardCall(forwarder, target, payload);
+        return returnData;
     }
 
     function forwardCallWithValue(
         address forwarder,
         address target,
         bytes memory payload
-    ) public payable returns (bytes memory returnData) {
-        returnData = MinimalForwarderLib.forwardCallWithValue(forwarder, target, payload, msg.value);
+    ) public payable returns (bytes memory) {
+        bytes memory returnData = MinimalForwarderLib.forwardCallWithValue(forwarder, target, payload, msg.value);
+        return returnData;
     }
 
-    function predictAddress(address deployer, bytes32 salt) public pure returns (address forwarder) {
-        forwarder = MinimalForwarderLib.predictAddress(deployer, salt);
+    function predictAddress(address deployer, bytes32 salt) public pure returns (address) {
+        address forwarder = MinimalForwarderLib.predictAddress(deployer, salt);
+        return forwarder;
     }
 }
