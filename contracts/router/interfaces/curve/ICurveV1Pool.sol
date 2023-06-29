5026091844a523f02fa7c4b4b1ff164fe706900a
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2023-06-29 11:53:15 -0400

    init CurveV1Module

---
+++ contracts/router/interfaces/curve/ICurveV1Pool.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface ICurveV1Pool {
+    function N_COINS() external view returns (int128);
+
+    function coins(uint256 i) external view returns (address);
+
+    function get_dy(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external returns (uint256);
+}
