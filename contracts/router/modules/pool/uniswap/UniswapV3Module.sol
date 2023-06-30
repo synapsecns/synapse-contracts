20fc2949615b00d3441c7e85d9dbe85ac22e4430
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-06-30 13:18:12 +0100

    Chore: add some docs

--- contracts/router/modules/pool/uniswap/UniswapV3Module.sol
+++ contracts/router/modules/pool/uniswap/UniswapV3Module.sol
@@ -15,6 +15,7 @@ import {SafeERC20, IERC20} from "@openzeppelin/contracts-4.5.0/token/ERC20/utils
 contract UniswapV3Module is OnlyDelegateCall, IPoolModule {
     using SafeERC20 for IERC20;
 
+    /// These need to be immutable in order to be accessed via delegatecall
     IUniswapV3Router public immutable uniswapV3Router;
     IUniswapV3StaticQuoter public immutable uniswapV3StaticQuoter;
 
@@ -34,7 +35,10 @@ contract UniswapV3Module is OnlyDelegateCall, IPoolModule {
         assertDelegateCall();
         address tokenIn = tokenFrom.token;
         IERC20(tokenIn).safeApprove(address(uniswapV3Router), amountIn);
-
+        // Prepare Uniswap Router params for the swap, see for reference:
+        // https://docs.uniswap.org/contracts/v3/guides/swaps/single-swaps#swap-input-parameters
+        // We set `amountOutMinimum` to 0, as the slippage checks are done outside of Pool Module
+        // We set `sqrtPriceLimitX96` to 0, as we don't want to limit the price (same reason as above)
         ExactInputSingleParams memory params = ExactInputSingleParams({
             tokenIn: tokenIn,
             tokenOut: tokenTo.token,
@@ -45,7 +49,7 @@ contract UniswapV3Module is OnlyDelegateCall, IPoolModule {
             amountOutMinimum: 0,
             sqrtPriceLimitX96: 0
         });
-
+        // Swap the tokens, we can trust Uniswap Router to return the correct amountOut
         amountOut = uniswapV3Router.exactInputSingle(params);
     }
 
@@ -60,6 +64,7 @@ contract UniswapV3Module is OnlyDelegateCall, IPoolModule {
         bool // probePaused
     ) external view returns (uint256 amountOut) {
         // We are ignoring the probePaused flag because Uniswap pools cannot be paused
+        // See `poolSwap()` for more details on the parameters
         QuoteExactInputSingleParams memory params = QuoteExactInputSingleParams({
             tokenIn: tokenFrom.token,
             tokenOut: tokenTo.token,
@@ -72,6 +77,7 @@ contract UniswapV3Module is OnlyDelegateCall, IPoolModule {
 
     /// @inheritdoc IPoolModule
     function getPoolTokens(address pool) external view returns (address[] memory tokens) {
+        // Uniswap pools always have exactly 2 tokens
         tokens = new address[](2);
         tokens[0] = IUniswapV3Pair(pool).token0();
         tokens[1] = IUniswapV3Pair(pool).token1();
