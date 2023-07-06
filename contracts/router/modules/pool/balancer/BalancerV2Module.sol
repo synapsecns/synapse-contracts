6e5034233588334d1858a26ca12a8d9be7b4d956
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2023-07-06 11:52:46 -0400

    balancer v2 pool module pool swap

--- contracts/router/modules/pool/balancer/BalancerV2Module.sol
+++ contracts/router/modules/pool/balancer/BalancerV2Module.sol
@@ -5,6 +5,7 @@ import {IERC20, SafeERC20} from "@openzeppelin/contracts-4.5.0/token/ERC20/utils
 
 import {IndexedToken, IPoolModule} from "../../../interfaces/IPoolModule.sol";
 import {IBalancerV2Vault} from "../../../interfaces/balancer/IBalancerV2Vault.sol";
+import {IBalancerV2Pool} from "../../../interfaces/balancer/IBalancerV2Pool.sol";
 
 /// @notice PoolModule for Balancer V2 pools
 contract BalancerV2Module is IPoolModule {
@@ -16,14 +17,36 @@ contract BalancerV2Module is IPoolModule {
         vault = IBalancerV2Vault(_vault);
     }
 
-    // TODO: pool address to bytes32 poolId
+    function _poolId(address pool) internal returns (bytes32 poolId) {
+        poolId = IBalancerV2Pool(pool).getPoolId();
+    }
 
     function poolSwap(
         address pool,
         IndexedToken memory tokenFrom,
         IndexedToken memory tokenTo,
         uint256 amountIn
-    ) external returns (uint256 amountOut) {}
+    ) external returns (uint256 amountOut) {
+        SingleSwap memory singleSwap = SingleSwap({
+            poolId: _poolId(pool),
+            kind: SwapKind.GIVEN_IN,
+            assetIn: tokenFrom.token,
+            assetOut: tokenTo.token,
+            amount: amountIn,
+            userData: bytes(0)
+        });
+        FundManagement memory funds = FundManagement({
+            sender: address(this),
+            fromInternalBalance: false,
+            recipient: address(this),
+            toInternalBalance: false
+        });
+        limit = 0;
+        deadline = type(uint256).max;
+
+        IERC20(tokenFrom.token).safeApprove(pool, amountIn);
+        amountOut = vault.swap(singleSwap, funds, limit, deadline);
+    }
 
     function getPoolQuote(
         address pool,
