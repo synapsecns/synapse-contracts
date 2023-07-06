bade0f0bd857e91e19de7c2ab90aac0060aca5ec
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2023-07-06 12:05:40 -0400

    fix balancer module pool swap

--- contracts/router/modules/pool/balancer/BalancerV2Module.sol
+++ contracts/router/modules/pool/balancer/BalancerV2Module.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.17;
 import {IERC20, SafeERC20} from "@openzeppelin/contracts-4.5.0/token/ERC20/utils/SafeERC20.sol";
 
 import {IndexedToken, IPoolModule} from "../../../interfaces/IPoolModule.sol";
+
+import {IBalancerV2Asset} from "../../../interfaces/balancer/IBalancerV2Asset.sol";
 import {IBalancerV2Vault} from "../../../interfaces/balancer/IBalancerV2Vault.sol";
 import {IBalancerV2Pool} from "../../../interfaces/balancer/IBalancerV2Pool.sol";
 
@@ -17,7 +19,7 @@ contract BalancerV2Module is IPoolModule {
         vault = IBalancerV2Vault(_vault);
     }
 
-    function _poolId(address pool) internal returns (bytes32 poolId) {
+    function _poolId(address pool) internal view returns (bytes32 poolId) {
         poolId = IBalancerV2Pool(pool).getPoolId();
     }
 
@@ -27,22 +29,22 @@ contract BalancerV2Module is IPoolModule {
         IndexedToken memory tokenTo,
         uint256 amountIn
     ) external returns (uint256 amountOut) {
-        SingleSwap memory singleSwap = SingleSwap({
+        IBalancerV2Vault.SingleSwap memory singleSwap = IBalancerV2Vault.SingleSwap({
             poolId: _poolId(pool),
-            kind: SwapKind.GIVEN_IN,
-            assetIn: tokenFrom.token,
-            assetOut: tokenTo.token,
+            kind: IBalancerV2Vault.SwapKind.GIVEN_IN,
+            assetIn: IBalancerV2Asset(tokenFrom.token), // TODO: check ok
+            assetOut: IBalancerV2Asset(tokenTo.token), // TODO: check ok
             amount: amountIn,
-            userData: bytes(0)
+            userData: ""
         });
-        FundManagement memory funds = FundManagement({
+        IBalancerV2Vault.FundManagement memory funds = IBalancerV2Vault.FundManagement({
             sender: address(this),
             fromInternalBalance: false,
-            recipient: address(this),
+            recipient: payable(address(this)),
             toInternalBalance: false
         });
-        limit = 0;
-        deadline = type(uint256).max;
+        uint256 limit = 0;
+        uint256 deadline = type(uint256).max;
 
         IERC20(tokenFrom.token).safeApprove(pool, amountIn);
         amountOut = vault.swap(singleSwap, funds, limit, deadline);
