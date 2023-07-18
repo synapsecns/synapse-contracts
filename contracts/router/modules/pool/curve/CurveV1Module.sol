d0db27e0f2ad4be9523ea03c96ca4ed58bc4ed96
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2023-07-18 14:13:45 -0400

    assert delegate call in pool swap for curve module

--- contracts/router/modules/pool/curve/CurveV1Module.sol
+++ contracts/router/modules/pool/curve/CurveV1Module.sol
@@ -6,9 +6,11 @@ import {IERC20, SafeERC20} from "@openzeppelin/contracts-4.5.0/token/ERC20/utils
 import {IndexedToken, IPoolModule} from "../../../interfaces/IPoolModule.sol";
 import {ICurveV1Pool} from "../../../interfaces/curve/ICurveV1Pool.sol";
 
+import {OnlyDelegateCall} from "../OnlyDelegateCall.sol";
+
 /// @notice PoolModule for Curve V1 pools
 /// @dev Implements IPoolModule interface to be used with pools added to LinkedPool router
-contract CurveV1Module is IPoolModule {
+contract CurveV1Module is OnlyDelegateCall, IPoolModule {
     using SafeERC20 for IERC20;
 
     function poolSwap(
@@ -17,7 +19,7 @@ contract CurveV1Module is IPoolModule {
         IndexedToken memory tokenTo,
         uint256 amountIn
     ) external returns (uint256 amountOut) {
-        // TODO: onlyDelegateCall?
+        assertDelegateCall();
         int128 i = int128(uint128(tokenFrom.index));
         int128 j = int128(uint128(tokenTo.index));
         IERC20(tokenFrom.token).safeApprove(pool, amountIn);
