28e77e9000c49111aa88505ab328ef7218b5657e
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2023-08-01 16:17:02 -0400

    clean up gmx interfaces

--- contracts/router/interfaces/gmx/IGMXV1Router.sol
+++ contracts/router/interfaces/gmx/IGMXV1Router.sol
@@ -4,33 +4,6 @@ pragma solidity 0.8.17;
 interface IGMXV1Router {
     function vault() external view returns (address);
 
-    function addPlugin(address _plugin) external;
-
-    function pluginTransfer(
-        address _token,
-        address _account,
-        address _receiver,
-        uint256 _amount
-    ) external;
-
-    function pluginIncreasePosition(
-        address _account,
-        address _collateralToken,
-        address _indexToken,
-        uint256 _sizeDelta,
-        bool _isLong
-    ) external;
-
-    function pluginDecreasePosition(
-        address _account,
-        address _collateralToken,
-        address _indexToken,
-        uint256 _collateralDelta,
-        uint256 _sizeDelta,
-        bool _isLong,
-        address _receiver
-    ) external returns (uint256);
-
     function swap(
         address[] memory _path,
         uint256 _amountIn,
