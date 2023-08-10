64ae33ef5f8fe98f6e262acf3f48a0c70c68561d
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-08-10 10:42:15 +0100

    Deploy script for QuoterV2

---
+++ script/router/BasicRouter.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {BasicSynapseScript} from "../templates/BasicSynapse.s.sol";
+
+/// @notice Common tools for Router-related scripts.
+abstract contract BasicRouterScript is BasicSynapseScript {
+    string public constant DEFAULT_POOL_CALC = "DefaultPoolCalc";
+
+    string public constant ROUTER_V1 = "SynapseRouter";
+    string public constant ROUTER_V2 = "SynapseRouterV2";
+
+    string public constant QUOTER_V2 = "SwapQuoterV2";
+
+    /// @notice Returns deployment of the latest SynapseRouter contract on the active chain.
+    function getLatestRouterDeployment() internal returns (address synapseRouter) {
+        // Check if SynapseRouterV2 is deployed
+        synapseRouter = tryGetDeploymentAddress(ROUTER_V2);
+        // Use SynapseRouter if SynapseRouterV2 is not deployed
+        if (synapseRouter == address(0)) {
+            synapseRouter = getDeploymentAddress(ROUTER_V1);
+        }
+    }
+}
---
+++ script/router/quoter/DeployQuoterV2.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {SwapQuoterV2} from "../../../contracts/router/quoter/SwapQuoterV2.sol";
+
+import {BasicRouterScript} from "../BasicRouter.s.sol";
+
+// solhint-disable no-console
+/// @notice This script deploys the SwapQuoterV2 contract, without any configuration.
+/// Use ConfigureQuoterV2.s.sol to configure the contract after deployment.
+/// Note: ownership will be transferred to the "owner wallet" address read from .env file.
+contract DeployQuoterV2 is BasicRouterScript {
+    function run() external {
+        // Setup the BasicSynapseScript
+        setUp();
+        vm.startBroadcast();
+        // Use `deployQuoterV2` as callback to deploy the contract
+        deployAndSave({contractName: QUOTER_V2, deployContract: deployQuoterV2});
+        vm.stopBroadcast();
+    }
+
+    /// @notice Callback function to deploy the SwapQuoterV2 contract.
+    /// Must follow this signature for the deploy script to work:
+    /// `deployContract() internal returns (address deployedAt, bytes memory constructorArgs)`
+    function deployQuoterV2() internal returns (address deployedAt, bytes memory constructorArgs) {
+        address synapseRouter = getLatestRouterDeployment();
+        address defaultPoolCalc = getDeploymentAddress(DEFAULT_POOL_CALC);
+        // We need specifically WGAS, so that on BNB chain we use WBNB
+        address weth = getDeploymentAddress("WGAS");
+        // Read the owner address from .env
+        address owner = vm.envAddress("OWNER_ADDR");
+        deployedAt = address(new SwapQuoterV2(synapseRouter, defaultPoolCalc, weth, owner));
+        constructorArgs = abi.encode(synapseRouter, defaultPoolCalc, weth, owner);
+    }
+}
