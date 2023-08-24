b5a66b3caf3c5d0ab593013db5534940005d02be
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-08-24 18:53:20 +0100

    Chore: misc clarifications

--- contracts/router/interfaces/IBridgeModule.sol
+++ contracts/router/interfaces/IBridgeModule.sol
@@ -4,8 +4,9 @@ pragma solidity 0.8.17;
 import {BridgeToken, SwapQuery} from "../libs/Structs.sol";
 
 interface IBridgeModule {
-    /// @notice Performs a bridging transaction on behalf of the sender, assuming `token` is already in the contract.
-    /// @dev This will be used via delegatecall from SynapseRouterV2, which will have custody over the initial tokens.
+    /// @notice Performs a bridging transaction on behalf of the sender, assuming they already have `token`.
+    /// @dev This will be used via delegatecall from SynapseRouterV2, which will have custody over the bridge tokens.
+    /// This will revert if delegatecall is not used.
     /// @param to            Address to receive tokens on destination chain
     /// @param chainId       Destination chain id
     /// @param token         Address of the bridge token
@@ -19,12 +20,13 @@ interface IBridgeModule {
         SwapQuery memory destQuery
     ) external payable;
 
-    /// @notice Gets the maximum amount of tokens user can bridge
+    /// @notice Gets the maximum amount of tokens user can bridge from this chain.
     /// @param token        Address of the bridge token
-    /// @return amount      Max amount of tokens user can bridge
+    /// @return amount      Max amount of tokens user can bridge from this chain
     function getMaxBridgedAmount(address token) external view returns (uint256 amount);
 
     /// @notice Calculates the fee amount for bridging a token to this chain.
+    /// @dev Will revert if the token is not supported.
     /// @param token        Address of the bridge token
     /// @param amount       Amount of tokens to be bridged
     /// @param isSwap       Whether the user provided swap details for converting the bridge token
@@ -58,7 +60,10 @@ interface IBridgeModule {
     /// @return symbol      Symbol of the supported bridge token used by the token's bridge
     function tokenToSymbol(address token) external view returns (string memory symbol);
 
-    /// @notice Returns the action mask associated with the given bridge token
+    /// @notice Returns the action mask associated with bridging a token to this chain.
+    /// Action mask is a bitmask of the actions that could be performed with the token atomically with the
+    /// incoming bridge transaction to this chain. See Structs.sol for the list of actions.
+    /// @dev Will return 0 (empty mask) if the token is not supported.
     /// @param token        Address of the bridge token
     /// @return actionMask  Action mask for the bridge token
     function tokenToActionMask(address token) external view returns (uint256 actionMask);
