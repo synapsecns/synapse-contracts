810d33a942eee26d1ae5f82a0e18f6303dbe815b
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-08-25 16:47:47 +0100

    Add bridging tests

--- test/router/modules/bridge/SynapseCCTPModule.t.sol
+++ test/router/modules/bridge/SynapseCCTPModule.t.sol
@@ -11,13 +11,25 @@ import {
 } from "../../../../contracts/router/modules/bridge/SynapseCCTPModule.sol";
 import {SynapseCCTP} from "../../../../contracts/cctp/SynapseCCTP.sol";
 
-import {BaseCCTPTest} from "../../../cctp/BaseCCTP.t.sol";
+import {BaseCCTPTest, MockERC20, RequestLib} from "../../../cctp/BaseCCTP.t.sol";
 import {DelegateCaller} from "./DelegateCaller.sol";
 
 contract SynapseCCTPModuleTest is BaseCCTPTest {
     // 1M USDC
     uint256 public constant MAX_BURN_AMOUNT = 10**6 * 10**6;
     string public constant SYMBOL_USDC = "CCTP.MockC";
+    // Fake different values for bridging
+    address public constant TO = address(10);
+    uint256 public constant AMOUNT = 1 ether;
+    uint256 public constant MSG_VALUE = 2 ether;
+    // Fake values for SwapQuery
+    address public constant TOKEN_OUT = address(20);
+    uint256 public constant MIN_AMOUNT_OUT = 30;
+    uint256 public constant DEADLINE = 40;
+    // Fake values for DefaultParams
+    address public constant POOL = address(50);
+    uint8 public constant TOKEN_INDEX_FROM = 60;
+    uint8 public constant TOKEN_INDEX_TO = 70;
 
     SynapseCCTP public synapseCCTP;
     address public token;
@@ -25,6 +37,8 @@ contract SynapseCCTPModuleTest is BaseCCTPTest {
 
     SynapseCCTPModule public module;
     DelegateCaller public delegateCaller;
+
+    bool public attachEther;
 
     function setUp() public virtual override {
         super.setUp();
@@ -37,6 +51,9 @@ contract SynapseCCTPModuleTest is BaseCCTPTest {
 
         delegateCaller = new DelegateCaller();
         module = new SynapseCCTPModule(address(synapseCCTP));
+
+        deal(address(this), MSG_VALUE);
+        cctpSetups[DOMAIN_ETH].mintBurnToken.mintPublic(address(delegateCaller), AMOUNT);
     }
 
     function setBurnLimitPerMessage(uint32 domain) public {
@@ -48,6 +65,123 @@ contract SynapseCCTPModuleTest is BaseCCTPTest {
 
     function testConstructor() public {
         assertEq(module.synapseCCTP(), address(synapseCCTP));
+    }
+
+    // ══════════════════════════════════════════════ TESTS: BRIDGING ══════════════════════════════════════════════════
+
+    function testDelegateBridgeRevertsWhenDirectCall() public {
+        SwapQuery memory emptyQuery;
+        vm.expectRevert("Not a delegate call");
+        module.delegateBridge(address(0), 0, address(0), 0, emptyQuery);
+    }
+
+    function verifyTokenBalance() public virtual {
+        assertEq(MockERC20(token).balanceOf(address(delegateCaller)), 0);
+        assertEq(MockERC20(token).balanceOf(address(synapseCCTP)), 0);
+    }
+
+    function performDelegateCall(bytes memory payload) public {
+        if (attachEther) {
+            delegateCaller.performDelegateCall{value: MSG_VALUE}(address(module), payload);
+        } else {
+            delegateCaller.performDelegateCall(address(module), payload);
+        }
+    }
+
+    // Flow for all delegateBridge tests:
+    // - Tokens were minted to `delegateCaller` contract in setUp(), which is mock for SynapseRouterV2
+    // - `delegateCaller` issues a delegate call to `module.delegateBridge()` which should initiate the bridging
+    // module.delegateBridge(address to, uint256 chainId, address token, uint256 AMOUNT, SwapQuery memory destQuery)
+
+    function getModulePayload(bool isBaseRequest) public view returns (bytes memory payload) {
+        SwapQuery memory destQuery;
+        if (!isBaseRequest) {
+            destQuery = SwapQuery({
+                routerAdapter: address(delegateCaller),
+                tokenOut: TOKEN_OUT,
+                minAmountOut: MIN_AMOUNT_OUT,
+                deadline: DEADLINE,
+                rawParams: abi.encode(
+                    DefaultParams({
+                        action: Action.Swap,
+                        pool: POOL,
+                        tokenIndexFrom: TOKEN_INDEX_FROM,
+                        tokenIndexTo: TOKEN_INDEX_TO
+                    })
+                )
+            });
+        }
+        payload = abi.encodeWithSelector(module.delegateBridge.selector, TO, CHAINID_AVAX, token, AMOUNT, destQuery);
+    }
+
+    function expectSynapseCCTPEvent(bool isBaseRequest) public {
+        uint32 requestVersion = isBaseRequest ? RequestLib.REQUEST_BASE : RequestLib.REQUEST_SWAP;
+        bytes memory swapParams = isBaseRequest
+            ? bytes("")
+            : RequestLib.formatSwapParams({
+                tokenIndexFrom: TOKEN_INDEX_FROM,
+                tokenIndexTo: TOKEN_INDEX_TO,
+                deadline: DEADLINE,
+                minAmountOut: MIN_AMOUNT_OUT
+            });
+        uint64 nonce = cctpSetups[DOMAIN_ETH].messageTransmitter.nextAvailableNonce();
+        bytes memory expectedRequest = RequestLib.formatRequest({
+            requestVersion: requestVersion,
+            baseRequest: RequestLib.formatBaseRequest({
+                originDomain: DOMAIN_ETH,
+                nonce: nonce,
+                originBurnToken: token,
+                amount: AMOUNT,
+                recipient: TO
+            }),
+            swapParams: swapParams
+        });
+        bytes32 expectedRequestID = getExpectedrequestID({
+            originDomain: DOMAIN_ETH,
+            destinationDomain: DOMAIN_AVAX,
+            finalRecipient: TO,
+            originBurnToken: token,
+            amount: AMOUNT,
+            requestVersion: requestVersion,
+            swapParams: swapParams
+        });
+        vm.expectEmit(address(synapseCCTP));
+        emit CircleRequestSent({
+            chainId: CHAINID_AVAX,
+            sender: msg.sender,
+            nonce: nonce,
+            token: token,
+            amount: AMOUNT,
+            requestVersion: requestVersion,
+            formattedRequest: expectedRequest,
+            requestID: expectedRequestID
+        });
+    }
+
+    function testDelegateBridgeBaseRequest() public {
+        bytes memory payload = getModulePayload({isBaseRequest: true});
+        expectSynapseCCTPEvent({isBaseRequest: true});
+        performDelegateCall(payload);
+        verifyTokenBalance();
+    }
+
+    function testDelegateBridgeSwapRequest() public {
+        bytes memory payload = getModulePayload({isBaseRequest: false});
+        expectSynapseCCTPEvent({isBaseRequest: false});
+        performDelegateCall(payload);
+        verifyTokenBalance();
+    }
+
+    // ═══════════════════════════════════ TESTS: BRIDGING (WITH ETHER ATTACHED) ═══════════════════════════════════════
+
+    function testDelegateBridgeBaseRequestWithEther() public {
+        attachEther = true;
+        testDelegateBridgeBaseRequest();
+    }
+
+    function testDelegateBridgeSwapRequestWithEther() public {
+        attachEther = true;
+        testDelegateBridgeSwapRequest();
     }
 
     // ═══════════════════════════════════════════════ TESTS: VIEWS ════════════════════════════════════════════════════
