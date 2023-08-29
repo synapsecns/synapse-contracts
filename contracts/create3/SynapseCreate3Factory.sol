a091fc20898b251a312eec50e7827d65c3842737
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-08-29 17:03:25 +0100

    Add `safeCreate3` implementation

--- contracts/create3/SynapseCreate3Factory.sol
+++ contracts/create3/SynapseCreate3Factory.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.17;
 import {Create3Lib} from "./libs/Create3.sol";
 import {ISynapseCreate3Factory} from "./interfaces/ISynapseCreate3Factory.sol";
 
+import {Address} from "@openzeppelin/contracts-4.5.0/utils/Address.sol";
+
 /// @title Synapse Create3 Contract Factory
 /// @author Synapse contributors
 /// @author Modified from 0age (https://github.com/0age/metamorphic/blob/master/contracts/ImmutableCreate2Factory.sol)
@@ -11,7 +13,13 @@ import {ISynapseCreate3Factory} from "./interfaces/ISynapseCreate3Factory.sol";
 /// doesn't depend on the creation code (aka EIP-3171, aka "CREATE3").
 /// Every deployer has access to their unique address deployment space of 2**96 addresses.
 contract SynapseCreate3Factory is ISynapseCreate3Factory {
+    using Address for address;
+
+    error SynapseCreate3Factory__InitCallFailed();
     error SynapseCreate3Factory__UnauthorizedDeployer(address deployer, address authorized);
+
+    /// @dev keccak256("SynapseCreate3Factory__InitCallFailed()")[:4]
+    bytes private constant _INIT_CALL_FAILED_SELECTOR = hex"ac2e37b3";
 
     /// @dev Modifier to check that the first 20 bytes of the salt are equal to the caller's address.
     /// This is used to prevent unauthorized deploys.
@@ -31,7 +39,14 @@ contract SynapseCreate3Factory is ISynapseCreate3Factory {
         bytes32 salt,
         bytes memory creationCode,
         bytes memory initData
-    ) external payable containsCaller(salt) returns (address deployedAt) {}
+    ) external payable containsCaller(salt) returns (address deployedAt) {
+        // Deploy a contract using create3 library, forwarding all msg.value
+        deployedAt = Create3Lib.create3(salt, creationCode, msg.value);
+        // Perform initialization call if needed
+        if (initData.length != 0) {
+            deployedAt.functionCall(initData, string(_INIT_CALL_FAILED_SELECTOR));
+        }
+    }
 
     /// @inheritdoc ISynapseCreate3Factory
     function predictAddress(bytes32 salt) external view returns (address) {
