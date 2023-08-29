e117a9a8412e7cc7e8905aa3c5566f62b5acf839
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-08-29 17:22:59 +0100

    Add tests with salt reusing

--- test/create3/SynapseCreate3Factory.t.sol
+++ test/create3/SynapseCreate3Factory.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import {SynapseCreate3Factory} from "../../contracts/create3/SynapseCreate3Factory.sol";
+import {Create3Lib, SynapseCreate3Factory} from "../../contracts/create3/SynapseCreate3Factory.sol";
 import {InitializableContract} from "./mocks/InitializableContract.sol";
 import {RevertingContract} from "./mocks/RevertingContract.sol";
 
@@ -24,8 +24,8 @@ contract SynapseCreate3FactoryTest is Test {
         revertingReference = address(new RevertingContract());
     }
 
-    function testSafeCreate3DeploysContract(bytes12 shortSalt) public returns (address deployedAddress) {
-        bytes32 salt = constructSalt(deployer, shortSalt);
+    function testSafeCreate3DeploysContract(bytes12 saltSuffix) public returns (address deployedAddress) {
+        bytes32 salt = constructSalt(deployer, saltSuffix);
         address predictedAddress = factory.predictAddress(salt);
         // Deploy contract, use msgValue and initData, which are both empty by default
         vm.prank(deployer);
@@ -38,32 +38,32 @@ contract SynapseCreate3FactoryTest is Test {
         assertEq(deployedAddress.code, initializableReference.code);
     }
 
-    function testSafeCreate3DeploysContractWithEther(bytes12 shortSalt, uint256 value)
+    function testSafeCreate3DeploysContractWithEther(bytes12 saltSuffix, uint256 value)
         public
         returns (address deployedAddress)
     {
         deal(deployer, value);
         msgValue = value;
-        deployedAddress = testSafeCreate3DeploysContract(shortSalt);
+        deployedAddress = testSafeCreate3DeploysContract(saltSuffix);
         assertEq(address(deployedAddress).balance, value);
     }
 
-    function testSafeCreate3DeploysContractWithInitData(bytes12 shortSalt, uint256 argValue)
+    function testSafeCreate3DeploysContractWithInitData(bytes12 saltSuffix, uint256 argValue)
         public
         returns (address deployedAddress)
     {
         initData = abi.encodeWithSelector(InitializableContract.setValue.selector, argValue);
-        deployedAddress = testSafeCreate3DeploysContract(shortSalt);
+        deployedAddress = testSafeCreate3DeploysContract(saltSuffix);
         assertEq(InitializableContract(deployedAddress).value(), argValue);
     }
 
     function testSafeCreate3DeploysContractWithEtherAndInitData(
-        bytes12 shortSalt,
+        bytes12 saltSuffix,
         uint256 value,
         uint256 argValue
     ) public {
         initData = abi.encodeWithSelector(InitializableContract.setValue.selector, argValue);
-        address deployedAddress = testSafeCreate3DeploysContractWithEther(shortSalt, value);
+        address deployedAddress = testSafeCreate3DeploysContractWithEther(saltSuffix, value);
         assertEq(InitializableContract(deployedAddress).value(), argValue);
     }
 
@@ -83,8 +83,8 @@ contract SynapseCreate3FactoryTest is Test {
         factory.safeCreate3(salt, type(InitializableContract).creationCode, "");
     }
 
-    function testSafeCreate3RevertsWhenSaltHasSingleBitSwitched(bytes12 shortSalt) public {
-        bytes32 correctSalt = constructSalt(deployer, shortSalt);
+    function testSafeCreate3RevertsWhenSaltHasSingleBitSwitched(bytes12 saltSuffix) public {
+        bytes32 correctSalt = constructSalt(deployer, saltSuffix);
         bytes32 one = bytes32(uint256(1));
         // Iterate over all 160 highest bits of the salt
         for (uint256 i = 0; i < 160; ++i) {
@@ -101,6 +101,29 @@ contract SynapseCreate3FactoryTest is Test {
             vm.prank(deployer);
             factory.safeCreate3(incorrectSalt, type(InitializableContract).creationCode, "");
         }
+    }
+
+    // ═════════════════════════════════════════════ TESTS: SALT REUSE ═════════════════════════════════════════════════
+
+    function testSafeCreate3RevertsWhenSaltReused(bytes12 saltSuffix) public {
+        bytes32 salt = constructSalt(deployer, saltSuffix);
+        // Deploy contract with the salt
+        vm.prank(deployer);
+        address occupied = factory.safeCreate3(salt, type(InitializableContract).creationCode, "");
+        // Try to deploy another contract with the same salt
+        vm.expectRevert(abi.encodeWithSelector(Create3Lib.Create3__DeploymentAlreadyExists.selector, occupied));
+        vm.prank(deployer);
+        factory.safeCreate3(salt, type(RevertingContract).creationCode, "");
+    }
+
+    function testSafeCreate3AnotherDeployerUsedSaltSuffix(bytes12 saltSuffix) public {
+        // Deploy contract using another deployer, but the same salt suffix
+        address anotherDeployer = makeAddr("Another deployer");
+        bytes32 anotherSalt = constructSalt(anotherDeployer, saltSuffix);
+        vm.prank(anotherDeployer);
+        factory.safeCreate3(anotherSalt, type(InitializableContract).creationCode, "");
+        // Should be able to use the same suffix with the original deployer
+        testSafeCreate3DeploysContract(saltSuffix);
     }
 
     // ══════════════════════════════════════════════ TESTS: REVERTS ═══════════════════════════════════════════════════
@@ -140,9 +163,9 @@ contract SynapseCreate3FactoryTest is Test {
 
     // ══════════════════════════════════════════════════ HELPERS ══════════════════════════════════════════════════════
 
-    function constructSalt(address caller, bytes12 shortSalt) public pure returns (bytes32 salt) {
-        // Lowest 12 bytes are the "short salt"
-        salt = bytes32(uint256(uint96(shortSalt)));
+    function constructSalt(address caller, bytes12 saltSuffix) public pure returns (bytes32 salt) {
+        // Lowest 12 bytes are the "salt suffix"
+        salt = bytes32(uint256(uint96(saltSuffix)));
         // Highest 20 bytes are the caller
         salt |= bytes32(uint256(uint160(caller)) << 96);
     }
