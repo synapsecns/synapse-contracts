f0b5554c73d8d1f6eafd1a70a2768637c373fd22
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-08-30 11:51:07 +0100

    Chore: refactor tests a bit

--- test/create3/SynapseCreate3Factory.t.sol
+++ test/create3/SynapseCreate3Factory.t.sol
@@ -14,9 +14,6 @@ contract SynapseCreate3FactoryTest is Test {
     address public initializableReference;
     address public revertingReference;
 
-    uint256 public msgValue;
-    bytes public initData;
-
     function setUp() public {
         factory = new SynapseCreate3Factory();
         deployer = makeAddr("Deployer");
@@ -24,36 +21,48 @@ contract SynapseCreate3FactoryTest is Test {
         revertingReference = address(new RevertingContract());
     }
 
-    function testSafeCreate3DeploysContract(bytes12 saltSuffix) public returns (address deployedAddress) {
-        bytes32 salt = constructSalt(deployer, saltSuffix);
-        address predictedAddress = factory.predictAddress(salt);
-        // Deploy contract, use msgValue and initData, which are both empty by default
+    function getSaltAndAddress(bytes12 saltSuffix) public view returns (bytes32 salt, address predictedAddress) {
+        salt = constructSalt(deployer, saltSuffix);
+        predictedAddress = factory.predictAddress(salt);
+    }
+
+    function safeCreate3(
+        bytes32 salt,
+        bytes memory creationCode,
+        bytes memory initData,
+        uint256 msgValue
+    ) public returns (address deployedAddress) {
+        deal(deployer, msgValue);
         vm.prank(deployer);
-        deployedAddress = factory.safeCreate3{value: msgValue}(
-            salt,
-            type(InitializableContract).creationCode,
-            initData
-        );
+        return factory.safeCreate3{value: msgValue}(salt, creationCode, initData);
+    }
+
+    function testSafeCreate3DeploysContract(bytes12 saltSuffix) public {
+        (bytes32 salt, address predictedAddress) = getSaltAndAddress(saltSuffix);
+        address deployedAddress = safeCreate3(salt, type(InitializableContract).creationCode, "", 0);
+        // Checks
         assertEq(predictedAddress, deployedAddress);
+        assertEq(address(deployedAddress).balance, 0);
         assertEq(deployedAddress.code, initializableReference.code);
     }
 
-    function testSafeCreate3DeploysContractWithEther(bytes12 saltSuffix, uint256 value)
-        public
-        returns (address deployedAddress)
-    {
-        deal(deployer, value);
-        msgValue = value;
-        deployedAddress = testSafeCreate3DeploysContract(saltSuffix);
+    function testSafeCreate3DeploysContractWithEther(bytes12 saltSuffix, uint256 value) public {
+        (bytes32 salt, address predictedAddress) = getSaltAndAddress(saltSuffix);
+        address deployedAddress = safeCreate3(salt, type(InitializableContract).creationCode, "", value);
+        // Checks
+        assertEq(predictedAddress, deployedAddress);
         assertEq(address(deployedAddress).balance, value);
+        assertEq(deployedAddress.code, initializableReference.code);
     }
 
-    function testSafeCreate3DeploysContractWithInitData(bytes12 saltSuffix, uint256 argValue)
-        public
-        returns (address deployedAddress)
-    {
-        initData = abi.encodeWithSelector(InitializableContract.setValue.selector, argValue);
-        deployedAddress = testSafeCreate3DeploysContract(saltSuffix);
+    function testSafeCreate3DeploysContractWithInitData(bytes12 saltSuffix, uint256 argValue) public {
+        bytes memory initData = abi.encodeWithSelector(InitializableContract.setValue.selector, argValue);
+        (bytes32 salt, address predictedAddress) = getSaltAndAddress(saltSuffix);
+        address deployedAddress = safeCreate3(salt, type(InitializableContract).creationCode, initData, 0);
+        // Checks
+        assertEq(predictedAddress, deployedAddress);
+        assertEq(address(deployedAddress).balance, 0);
+        assertEq(deployedAddress.code, initializableReference.code);
         assertEq(InitializableContract(deployedAddress).value(), argValue);
     }
 
@@ -62,8 +71,13 @@ contract SynapseCreate3FactoryTest is Test {
         uint256 value,
         uint256 argValue
     ) public {
-        initData = abi.encodeWithSelector(InitializableContract.setValue.selector, argValue);
-        address deployedAddress = testSafeCreate3DeploysContractWithEther(saltSuffix, value);
+        bytes memory initData = abi.encodeWithSelector(InitializableContract.setValue.selector, argValue);
+        (bytes32 salt, address predictedAddress) = getSaltAndAddress(saltSuffix);
+        address deployedAddress = safeCreate3(salt, type(InitializableContract).creationCode, initData, value);
+        // Checks
+        assertEq(predictedAddress, deployedAddress);
+        assertEq(address(deployedAddress).balance, value);
+        assertEq(deployedAddress.code, initializableReference.code);
         assertEq(InitializableContract(deployedAddress).value(), argValue);
     }
 
@@ -79,8 +93,7 @@ contract SynapseCreate3FactoryTest is Test {
                 address(bytes20(salt))
             )
         );
-        vm.prank(deployer);
-        factory.safeCreate3(salt, type(InitializableContract).creationCode, "");
+        safeCreate3(salt, type(InitializableContract).creationCode, "", 0);
     }
 
     function testSafeCreate3RevertsWhenSaltHasSingleBitSwitched(bytes12 saltSuffix) public {
@@ -98,8 +111,7 @@ contract SynapseCreate3FactoryTest is Test {
                     address(bytes20(incorrectSalt))
                 )
             );
-            vm.prank(deployer);
-            factory.safeCreate3(incorrectSalt, type(InitializableContract).creationCode, "");
+            safeCreate3(incorrectSalt, type(InitializableContract).creationCode, "", 0);
         }
     }
 
@@ -112,8 +124,7 @@ contract SynapseCreate3FactoryTest is Test {
         address occupied = factory.safeCreate3(salt, type(InitializableContract).creationCode, "");
         // Try to deploy another contract with the same salt
         vm.expectRevert(abi.encodeWithSelector(Create3Lib.Create3__DeploymentAlreadyExists.selector, occupied));
-        vm.prank(deployer);
-        factory.safeCreate3(salt, type(RevertingContract).creationCode, "");
+        safeCreate3(salt, type(RevertingContract).creationCode, "", 0);
     }
 
     function testSafeCreate3AnotherDeployerUsedSaltSuffix(bytes12 saltSuffix) public {
@@ -129,53 +140,46 @@ contract SynapseCreate3FactoryTest is Test {
     // ══════════════════════════════════════════════ TESTS: REVERTS ═══════════════════════════════════════════════════
 
     function testSafeCreate3RevertsWhenConstructorRevertsSilently() public {
-        deal(deployer, 1);
         bytes32 salt = constructSalt(deployer, 0);
         vm.expectRevert(Create3Lib.Create3__DeploymentFailed.selector);
         // Make the deployment fail y providing Ether to non-payable constructor
-        vm.prank(deployer);
-        factory.safeCreate3{value: 1}(salt, type(RevertingContract).creationCode, "");
+        safeCreate3(salt, type(RevertingContract).creationCode, "", 1);
     }
 
     // Revert message is NOT bubbled up
     function testSafeCreate3RevertsWhenConstructorRevertsWithMessage() public {
         bytes32 salt = constructSalt(deployer, 0);
         vm.expectRevert(Create3Lib.Create3__DeploymentFailed.selector);
-        vm.prank(deployer);
-        factory.safeCreate3(salt, type(RevertingConstructorContract).creationCode, "");
+        safeCreate3(salt, type(RevertingConstructorContract).creationCode, "", 0);
     }
 
     function testSafeCreate3RevertsBubbleErrorWhenInitCallRevertsWithCustomError() public {
         bytes32 salt = constructSalt(deployer, 0);
-        initData = abi.encodeWithSelector(RevertingContract.revertWithNoArgError.selector);
+        bytes memory initData = abi.encodeWithSelector(RevertingContract.revertWithNoArgError.selector);
         vm.expectRevert(RevertingContract.NoArgError.selector);
-        vm.prank(deployer);
-        factory.safeCreate3(salt, type(RevertingContract).creationCode, initData);
+        safeCreate3(salt, type(RevertingContract).creationCode, initData, 0);
     }
 
     function testSafeCreate3RevertsBubbleErrorWhenInitCallRevertsWithCustomErrorWithArgs() public {
         bytes32 salt = constructSalt(deployer, 0);
         uint256 argValue = 42;
-        initData = abi.encodeWithSelector(RevertingContract.revertWithOneArgError.selector, argValue);
+        bytes memory initData = abi.encodeWithSelector(RevertingContract.revertWithOneArgError.selector, argValue);
         vm.expectRevert(abi.encodeWithSelector(RevertingContract.OneArgError.selector, argValue));
-        vm.prank(deployer);
-        factory.safeCreate3(salt, type(RevertingContract).creationCode, initData);
+        safeCreate3(salt, type(RevertingContract).creationCode, initData, 0);
     }
 
     function testSafeCreate3RevertsBubbleErrorWhenInitCallRevertsWithMessage() public {
         bytes32 salt = constructSalt(deployer, 0);
-        initData = abi.encodeWithSelector(RevertingContract.revertWithMessage.selector);
+        bytes memory initData = abi.encodeWithSelector(RevertingContract.revertWithMessage.selector);
         vm.expectRevert("Revert: GM");
-        vm.prank(deployer);
-        factory.safeCreate3(salt, type(RevertingContract).creationCode, initData);
+        safeCreate3(salt, type(RevertingContract).creationCode, initData, 0);
     }
 
     function testSafeCreate3RevertsWhenInitCallRevertsSilently() public {
         bytes32 salt = constructSalt(deployer, 0);
-        initData = abi.encodeWithSelector(RevertingContract.revertNoReason.selector);
+        bytes memory initData = abi.encodeWithSelector(RevertingContract.revertNoReason.selector);
         vm.expectRevert(SynapseCreate3Factory.SynapseCreate3Factory__InitCallFailed.selector);
-        vm.prank(deployer);
-        factory.safeCreate3(salt, type(RevertingContract).creationCode, initData);
+        safeCreate3(salt, type(RevertingContract).creationCode, initData, 0);
     }
 
     // ══════════════════════════════════════════════════ HELPERS ══════════════════════════════════════════════════════
