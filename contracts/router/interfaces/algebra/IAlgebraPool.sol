06812a4784bff1393b7765059baf1969ac39fbae
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-10-25 21:16:58 +0100

    Chore: remove `globalState` function that ended up unused

--- contracts/router/interfaces/algebra/IAlgebraPool.sol
+++ contracts/router/interfaces/algebra/IAlgebraPool.sol
@@ -2,19 +2,6 @@
 pragma solidity 0.8.17;
 
 interface IAlgebraPool {
-    function globalState()
-        external
-        view
-        returns (
-            uint160,
-            int24,
-            uint16 fee,
-            uint16,
-            uint8,
-            uint8,
-            bool
-        );
-
     function token0() external view returns (address);
 
     function token1() external view returns (address);
