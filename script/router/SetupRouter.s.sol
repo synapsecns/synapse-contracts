2a4450947420b3e0435b2e09bc423685e8175afa
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-11-03 19:03:13 +0000

    Move bridge interface to a separate file

--- script/router/SetupRouter.s.sol
+++ script/router/SetupRouter.s.sol
@@ -11,9 +11,7 @@ import {SwapQuoter, Pool} from "../../contracts/bridge/router/SwapQuoter.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
-interface IBridge {
-    function WETH_ADDRESS() external view returns (address);
-}
+import {IBridge} from "./interfaces/IBridge.sol";
 
 // solhint-disable no-console
 contract SetupRouterScript is DeployScript {
