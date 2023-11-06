bb364168a4168b07cb081b9aff879dc42607ec93
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-11-06 13:13:10 +0000

    Add "run if deployed option" for the integration tests

--- test/utils/IntegrationTest.sol
+++ test/utils/IntegrationTest.sol
@@ -8,4 +8,9 @@ interface IntegrationTest {
     /// @notice Returns the name of the contract to be tested.
     /// @dev The integration test will be skipped if the contract has already been deployed.
     function contractName() external view returns (string memory);
+
+    /// @notice Whether to run the integration test, if the tested contract is already deployed.
+    /// @dev This is set to false by default. Could be overridden to true to enable the CI workflow
+    /// for the deployed contract that has the config updated.
+    function runIfDeployed() external view returns (bool);
 }
