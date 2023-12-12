895270c97065688bb7799bb1b88fcfacc507b0a5
χ² <88190723+ChiTimesChi@users.noreply.github.com>
2023-12-12 11:21:41 +0000

    Linked pools for usdc: deploy on Mainnet, adjust on Avalanche (#308)
    * Add preliminary LinkedPool config for Mainnet USDC
    
    * Add integration tests for the new config
    
    * Regenerate LinkedPool config using a new module
    
    * PIn a block after the module deployment
    
    * Use multi-line labels to make the viz less thicc
    
    * Regenerate new config viz
    
    * Deploy LinkedPool for Mainnet USDC
    
    * Update the LinkedPool config on AVAX
    
    * [REVERT IN PROD] Enable CI workflow for AVAX USDC LinkedPool
    
    * Unpin linked pool config tests from a specific block number
    
    * Revert "[REVERT IN PROD] Enable CI workflow for AVAX USDC LinkedPool"
    
    This reverts commit 58301730583c65c21c113b53d8a2ae80d3ced573.
    
    * [REVERT IN PROD] Enable CI workflow for all LinkedPools
    
    * make bad quotes logging more generic
    
    * Chore: improve float logging
    
    * Log trades with slippage >= 1%
    
    * Log only bad quotes to improve readability
    
    * Chore: make logs a bit more readable
    
    * Disallow block pinning in the test template for linked pools
    
    * Revert "[REVERT IN PROD] Enable CI workflow for all LinkedPools"
    
    This reverts commit 10eaef2bdf4a346128212c5f990bf3e3a9f41b4d.
    
    * Add more TraderJoe pools to Avax LinkedPool
    
    * [REVERT IN PROD] enable CI workflow AVAX LinkedPools
    
    * Revert "[REVERT IN PROD] enable CI workflow AVAX LinkedPools"
    
    This reverts commit f231cb2effdb6b12a7a3366338009d023fe2e661.
    
    * Make Optimism vix less thicc
    
    * More external pools to the USDC LinkedPool config
    
    * [REVERT IN PROD] enable CI workflow for Opt USDC LinkedPool
    
    * Duplicate the setup
    
    * Revert "[REVERT IN PROD] enable CI workflow for Opt USDC LinkedPool"
    
    This reverts commit 2479cc889628905a18452e371192141367e1562d.

--- test/router/linkedPool/LinkedPoolConfig.Integration.Base.USDC.t.sol
+++ test/router/linkedPool/LinkedPoolConfig.Integration.Base.USDC.t.sol
@@ -4,16 +4,11 @@ pragma solidity 0.8.17;
 import {LinkedPoolConfigIntegrationTest} from "./LinkedPoolConfigIntegration.sol";
 
 contract LinkedPoolConfigUSDCBaseTestFork is LinkedPoolConfigIntegrationTest {
-    // 2023-11-04
-    uint256 public constant BASE_BLOCK_NUMBER = 6150000;
-
     /// @notice Test swaps worth 10_000 USDC
     uint256 public constant SWAP_VALUE = 10_000;
 
     /// @notice Used pools have accurate quoting functions, so should have no delta
     uint256 public constant MAX_PERCENT_DELTA = 0;
 
-    constructor()
-        LinkedPoolConfigIntegrationTest("base", BASE_BLOCK_NUMBER, "CCTP.USDC", SWAP_VALUE, MAX_PERCENT_DELTA)
-    {}
+    constructor() LinkedPoolConfigIntegrationTest("base", "CCTP.USDC", SWAP_VALUE, MAX_PERCENT_DELTA) {}
 }
