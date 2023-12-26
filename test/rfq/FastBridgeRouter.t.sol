236108f7ddcaafb3340b09c32e8815775fe711b7
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-12-26 19:36:06 +0000

    Setup tokens, pool

--- test/rfq/FastBridgeRouter.t.sol
+++ test/rfq/FastBridgeRouter.t.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.17;
 import {FastBridgeRouter} from "../../contracts/rfq/FastBridgeRouter.sol";
 
 import {MockFastBridge} from "../mocks/MockFastBridge.sol";
+import {MockERC20} from "../mocks/MockERC20.sol";
+import {MockDefaultPool} from "../mocks/MockDefaultPool.sol";
 
 import {Test} from "forge-std/Test.sol";
 
@@ -12,10 +14,26 @@ contract FastBridgeRouterTest is Test {
     MockFastBridge public fastBridge;
     address owner;
 
+    MockERC20 public token0;
+    MockERC20 public token1;
+
+    MockDefaultPool public pool;
+
     function setUp() public {
         owner = makeAddr("Owner");
         fastBridge = new MockFastBridge();
         router = new FastBridgeRouter(address(fastBridge), owner);
+
+        token0 = new MockERC20("T0", 18);
+        token1 = new MockERC20("T1", 18);
+
+        address[] memory tokens = new address[](2);
+        tokens[0] = address(token0);
+        tokens[1] = address(token1);
+        pool = new MockDefaultPool(tokens);
+        // Mint some tokens to the pool
+        token0.mint(address(pool), 100 ether);
+        token1.mint(address(pool), 120 ether);
     }
 
     function test_constructor() public {
@@ -23,7 +41,11 @@ contract FastBridgeRouterTest is Test {
         assertEq(router.owner(), owner);
     }
 
-    function test_bridge() public {
+    function test_bridge_withoutOriginSwap() public {
+        // TODO
+    }
+
+    function test_bridge_withOriginSwap() public {
         // TODO
     }
 
