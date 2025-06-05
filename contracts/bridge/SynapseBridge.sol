727a795bb302c2376f30594378504f06e079dca5
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2025-06-05 18:00:35 +0100

    refactor: separate constants vs storage

--- contracts/bridge/SynapseBridge.sol
+++ contracts/bridge/SynapseBridge.sol
@@ -26,11 +26,12 @@ contract SynapseBridge is Initializable, AccessControlUpgradeable, ReentrancyGua
     bytes32 public constant NODEGROUP_ROLE = keccak256("NODEGROUP_ROLE");
     bytes32 public constant GOVERNANCE_ROLE = keccak256("GOVERNANCE_ROLE");
 
+    uint256 public constant bridgeVersion = 8;
+    uint256 public constant chainGasAmount = 0;
+
     mapping(address => uint256) private fees;
 
     uint256 public startBlockNumber;
-    uint256 public constant bridgeVersion = 8;
-    uint256 public constant chainGasAmount = 0;
     /// @dev This is a variable taking the storage slot of deprecated chainGasAmount to prevent storage gap
     uint256 private _deprecatedChainGasAmount;
     address payable public WETH_ADDRESS;
