9bb3cbb4edfbaf407d583bb437afbf347806746e
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2025-06-05 15:27:04 +0100

    feat: scaffold the migrator contract

---
+++ contracts/migrator/SynapseMigrator.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {ISynapseMigrator} from "./interfaces/ISynapseMigrator.sol";
+import {ISynapseMigratorErrors} from "./interfaces/ISynapseMigratorErrors.sol";
+
+import {Ownable} from "@openzeppelin/contracts-4.5.0/access/Ownable.sol";
+
+contract SynapseMigrator is Ownable, ISynapseMigrator, ISynapseMigratorErrors {
+    event Migrated(address indexed user, address indexed oldToken, uint256 amount);
+    event TokenPairAdded(address indexed oldToken, address indexed newToken);
+
+    /// @inheritdoc ISynapseMigrator
+    function addTokenPair(address oldToken, address newToken) external onlyOwner {
+        // TODO: implement
+    }
+
+    /// @inheritdoc ISynapseMigrator
+    function migrate(address oldToken, uint256 amount) external {
+        // TODO: implement
+    }
+
+    /// @inheritdoc ISynapseMigrator
+    function getTokenPair(address oldToken) external view returns (address newToken) {
+        // TODO: implement
+    }
+
+    /// @inheritdoc ISynapseMigrator
+    function previewMigrate(address oldToken, uint256 amount) external view returns (uint256 newAmount) {
+        // TODO: implement
+    }
+}
---
+++ contracts/migrator/interfaces/ISynapseMigrator.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface ISynapseMigrator {
+    /// @notice Allows the contract owner to add a token pair to the migrator.
+    /// Users will be able to migrate from the old token to the new token using 1:1 ratio,
+    /// taking token decimals into account.
+    /// @dev Will revert in the following cases:
+    /// - Either of the tokens is the zero address.
+    /// - Token addresses are the same.
+    /// - The token pair is already added for the old token.
+    function addTokenPair(address oldToken, address newToken) external;
+
+    /// @notice Migrates the given amount of old tokens to new tokens.
+    /// Old tokens will be taken from the user and burned. New tokens will be transferred to the user.
+    /// @dev Will revert in the following cases:
+    /// - Zero address or amount is supplied.
+    /// - The token pair is not added for the old token.
+    /// - Contract does not have enough balance of the new token.
+    function migrate(address oldToken, uint256 amount) external;
+
+    /// @notice Returns the new token for the given old token.
+    /// @dev Will return the zero address if the token pair is not added for the old token.
+    function getTokenPair(address oldToken) external view returns (address newToken);
+
+    /// @notice Returns the amount of new tokens that will be received for the given amount of old tokens.
+    /// @dev Will return 0 if the token pair is not added for the old token.
+    function previewMigrate(address oldToken, uint256 amount) external view returns (uint256 newAmount);
+}
